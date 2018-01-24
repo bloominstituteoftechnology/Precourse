1ee38077b8079efbef74382698647eab17d3f304
Daniel Plott <Daniel.P.Plott@gmail.com>
2018-01-24 00:33:27 -0600

    finished homework

--- Lesson2-Intro-to-JS/homework/homework.js
+++ Lesson2-Intro-to-JS/homework/homework.js
@@ -61,10 +61,10 @@ function areEqual(x, y) {
   // return true if x and y are the same
   // otherwise return false
   if (x === y){
-  	return true;
+	return true;
 }
 
-  	return false;
+	return false;
 }
 
 function areSameLength(str1, str2) {
@@ -72,7 +72,7 @@ function areSameLength(str1, str2) {
   // otherwise return false
  
   if (str1.length === str2.length){
-  	return true;
+	return true;
 }
 	else
     return false;
@@ -82,22 +82,22 @@ function lessThanNinety(num) {
   // return true if the function argument: num , is less than ninety
   // otherwise return false
   if (num < 90){
-  	return true;
+	return true;
   }
 
   else
-  	return false;
+	return false;
 }
 
 function greaterThanFifty(num) {
   // return true if num is greater than fifty
   // otherwise return false
   if (num > 50){
-  	return true;
+	return true;
   }
 
   else
-  	return false;
+	return false;
 }
 
 function getRemainder(x, y) {
@@ -110,22 +110,22 @@ function isEven(num) {
   // return true if num is even
   // otherwise return false
   if ((num % 2) === 0){
-  	return true;
+	return true;
   }
 
   else
-  	return false;
+	return false;
 }
 
 function isOdd(num) {
   // return true if num is false
   // otherwise return false
   if ((num % 2 === 1)){
-  	return true;
+	return true;
   }
 
   else
-  	return false;
+	return false;
 }
 function square(num) {
   // square num and return the new value
@@ -136,46 +136,46 @@ function square(num) {
 
 function cube(num) {
   // cube num and return the new value
-  let newcube = num ^ 3;
+  let newcube = num * num * num;
   return newcube;
 }
 
 function raiseToPower(num, exponent) {
   // raise num to whatever power is passed in as exponent
-  let rtp = num ^ exponent;
+  let rtp = Math.pow(num, exponent);
   return rtp;
 }
 
 function roundNumber(num) {
   // round num and return it
-  let newrn = Math.floor(num);
+  let newrn = Math.round(num);
   return newrn;
 }
 
 function roundUp(num) {
   // round num up and return it
-  let newru = Math.ceiling(num);
+  let newru = Math.ceil(num);
   return newru;
 }
 
 function addExclamationPoint(str) {
   // add an exclamation point to the end of str and return the new string
   // 'hello world' -> 'hello world!'
-  let newstr = str + "!";
+  let newstr = str + '!';
   return newstr;
 }
 
 function combineNames(firstName, lastName) {
   // return firstName and lastName combined as one string and separated by a space.
   // 'Lambda', 'School' -> 'Lambda School'
-  let combnam = firstName + " " + lastName;
+  let combnam = firstName + ' ' + lastName;
   return combnam;
 }
 
 function getGreeting(name) {
   // Take the name string and concatenate other strings onto it so it takes the following form:
   // 'Sam' -> 'Hello Sam!'
-  let getgreet = 'Hello ' + name + "!";
+  let getgreet = 'Hello ' + name + '!';
   return getgreet;
 }
 
