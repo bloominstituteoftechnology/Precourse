9d3b165565203e2f9a6914477e327ef5de78a08f
Lukas Kaspar <lukas.schauffele@gmail.com>
2018-01-25 14:49:11 +1100

    isPrime -minor change

--- Lesson3-JS-II/homework/homework.js
+++ Lesson3-JS-II/homework/homework.js
@@ -67,11 +67,10 @@ function isPrime(num) {
   // hint: a prime number is only evenly divisible by itself and 1
   // hint2: you can solve this using a for loop
   // note: 0 and 1 are NOT considered prime numbers
-  if (num === 0) return false;
   for (let i = 2; i < num; i++) {
     if (num % 2 === 0) return false;
   }
-  return num !== 1;
+  return num !== 1 && num !== 0;
 }
 
 function returnFirst(arr) {
