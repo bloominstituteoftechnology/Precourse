15a49f57f949502b1e6a90ab77f88e634c6fca41
Alex Garcia <bolso72@gmail.com>
2018-01-25 17:06:17 -0500

    Lesson2 Homework

--- Lesson2-Intro-to-JS/homework/README.md
+++ Lesson2-Intro-to-JS/homework/README.md
@@ -2,7 +2,9 @@
 
 ## Instructions
 ---
-1. Feynman Writing Prompts - In a seprate text file that you create, write out explanations of the following concepts like you are explaining it to a 12 year old.  Doing this will help you quickly discover any holes in your understanding.  Ask your questions on Slack.
+1. Feynman Writing Prompts - In a seprate text file that you create, write out explanations
+of the following concepts like you are explaining it to a 12 year old.  Doing this will help 
+you quickly discover any holes in your understanding.  Ask your questions on Slack.
 		
 	* Variables
 	* Strings
@@ -13,7 +15,9 @@
 
 2. Install Node and NPM.  NPM comes packaged with Node. https://nodejs.org/en/download/
 
-3. From the top level of your `Precourse` folder, run `npm run test:JSI` to run the automated tests.  At first all of the tests will be broken.  You will fill out the functions in `homework.js` to make the tests pass.
+3. From the top level of your `Precourse` folder, run `npm run test:JSI` to run the automated tests. 
+At first all of the tests will be broken.  You will fill out the functions in `homework.js`
+to make the tests pass.
 
 
 #### Congratulations on finishing Homework #1!
--- Lesson2-Intro-to-JS/homework/homework.js
+++ Lesson2-Intro-to-JS/homework/homework.js
@@ -2,22 +2,22 @@
 //In these first 6 questions, replace `null` with the answer
 
 //create a string variable, it can contain anything
-let newString = null ;
+let newString = 'I am learning web development';
 
 //create a number variable, it an be any number
-let newNum = null ;
+let newNum = 75;
 
 //create a boolean variable
-let newBool = null ;
+let newBool = true;
 
 //solve the following math problem
-let newSubtract = 10 - null === 5;
+let newSubtract = 10 - 5 === 5;
 
 //Solve the following math problem
-let newMultiply = 10 * null === 40 ;
+let newMultiply = 10 * 4 === 40;
 
 //Solve the following math problem:
-let newModulo = 21 % 5 === null ;
+let newModulo = 21 % 5 === 1;
 
 
 
@@ -25,115 +25,183 @@ let newModulo = 21 % 5 === null ;
 //Make sure you use return when the prompt asks you to.
 //hint: console.log() will NOT work. 
 //Do not change any of the function names
-
 function returnString(str) {
   //simply return the string provided: str
+  return str;
 }
 
 function add(x, y) {
   // x and y are numbers
   // add x and y together and return the value
   // code here
+  return x + y;
 }
 
 function subtract(x, y) {
   // subtract y from x and return the value
   // code here
+  return x - y;
 }
 
 function multiply(x, y) {
   // multiply x by y and return the value
   // code here
+  const product = x * y;
+  return product;
 }
 
 function divide(x, y) {
   // divide x by y and return the value
   // code here
+  const quotient = x / y;
+  return quotient;
 }
 
 function areEqual(x, y) {
   // return true if x and y are the same
   // otherwise return false
   // code here
+  if (x === y) {
+  return true;
+  }
+  else {
+    return false;
+  }
+
 }
 
 function areSameLength(str1, str2) {
   // return true if the two strings have the same length
   // otherwise return false
   // code here
+  if (str1.length === str2.length) {
+  return true;
+  }
+  else {
+  return false;
+  }
+
+
 }
 
 function lessThanNinety(num) {
   // return true if the function argument: num , is less than ninety
   // otherwise return false
   // code here
+  if (num < 90) {
+  return true;
+  }
+  else {
+  return false;
+  }
+
 }
 
 function greaterThanFifty(num) {
   // return true if num is greater than fifty
   // otherwise return false
   // code here
+  if (num > 50) {
+  return true;
+  }
+  else {
+  return false;
+  }
+
 }
 
 function getRemainder(x, y) {
   // return the remainder from dividing x by y
   // code here
+  const remainder = x % y;
+  return remainder;
+
 }
 
 function isEven(num) {
   // return true if num is even
   // otherwise return false
   // code here
+  if (num % 2 == 0) {
+  return true;
+  }
+  else {
+  return false;
+  }
 }
 
 function isOdd(num) {
   // return true if num is false
   // otherwise return false
   // code here
+
+  if (num % 2 == 1) {
+  return true;
+  }
+  else {
+  return false;
+  }
 }
 
 function square(num) {
   // square num and return the new value
   // hint: NOT square root!
   // code here
+  const squaredNum = num * num;
+  return squaredNum;
 }
 
 function cube(num) {
   // cube num and return the new value
   // code here
+  const cubedNum =Math. pow(num,3);
+  return cubedNum;
 }
 
 function raiseToPower(num, exponent) {
   // raise num to whatever power is passed in as exponent
   // code here
+  return Math.pow(num, exponent);
 }
 
 function roundNumber(num) {
   // round num and return it
   // code here
+  const roundedNum = Math.round(num);
+  return roundedNum;
 }
 
 function roundUp(num) {
   // round num up and return it
   // code here
+  const roundedUp = Math.ceil(num);
+  return roundedUp;
 }
 
 function addExclamationPoint(str) {
   // add an exclamation point to the end of str and return the new string
   // 'hello world' -> 'hello world!'
   // code here
+  const exclPoint = str + '!';
+  return exclPoint; 
 }
 
 function combineNames(firstName, lastName) {
   // return firstName and lastName combined as one string and separated by a space.
   // 'Lambda', 'School' -> 'Lambda School'
   // code here
+  let fullName = firstName + ' ' +  lastName;
+  return fullName;
+
 }
 
 function getGreeting(name) {
   // Take the name string and concatenate other strings onto it so it takes the following form:
   // 'Sam' -> 'Hello Sam!'
   // code here
+  let greeting = 'Hello ' + name + '!';
+  return greeting;
+
 }
 
 // The next three questions will have you implement math area formulas. 
@@ -142,11 +210,16 @@ function getGreeting(name) {
 function getRectangleArea(length, width) {
   // return the area of the rectangle by using length and width
   // code here
+  let areaOfRectangle = length * width;
+  return areaOfRectangle;
+
 }
 
 function getTriangleArea(base, height) {
   // return the area of the triangle by using base and height
   // code here
+  let areaOfTriangle = 1 / 2 * base * height;
+  return areaOfTriangle;
 }
 
 // Do not modify code below this line.
