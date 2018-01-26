7c7803926b46be911a4a9dbe2fb2b5c883fbca2e
astro11 <aquila.strother@asu.edu>
2018-01-26 17:25:57 -0500

    updated homework.js file

--- Lesson4-JS-III/homework/homework.js
+++ Lesson4-JS-III/homework/homework.js
@@ -5,12 +5,22 @@ function makeCat(name, age) {
   // add an age property to the object with the value set to the age argument
   // add a method called meow that returns the string 'Meow!'
   // return the object
+  let cat = {
+    name: name,
+    age: age,
+    meow: function() {
+      return 'Meow!';
+    }    
+  };
+  return cat;
 }
+
 
 function addProperty(object, property) {
   // add the property to the object with a value of null
   // return the object
   // note: the property name is NOT 'property'.  The name is the value of the argument called property (a string)
+  property.null = 'property';
 }
 
 function invokeMethod(object, method) {
