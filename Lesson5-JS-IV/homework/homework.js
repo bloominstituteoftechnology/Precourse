24a287303d80961a69cd38935b372e8df508ddd7
DragonRydrz <DragonRydrz@me.com>
2018-01-26 23:28:37 -0500

    saving...

--- Lesson5-JS-IV/homework/homework.js
+++ Lesson5-JS-IV/homework/homework.js
@@ -8,23 +8,37 @@ function multiplyArguments() {
 
 function invokeCallback(cb) {
   // invoke cb
+  cb();
 }
 
 function sumArray(numbers, cb) {
   // sum up all of the integers in the numbers array
   // pass the result to cb
   // no return is necessary
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  cb(sum);
 }
 
 function forEach(arr, cb) {
   // iterate over arr and pass its values to cb one by one
   // hint: you will be invoking cb multiple times (once for each value in the array)
+  for (let i = 0; i < arr.length; i++) {
+    cb(arr[i]);
+  }
 }
 
 function map(arr, cb) {
   // create a new array
   // iterate over each value in arr, pass it to cb, then place the value returned from cb into the new arr
   // the new array should be the same length as the array argument
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArray.push(cb(arr[i]));
+  }
+  return newArray;
 }
 
 function createUserClass() {
@@ -39,6 +53,7 @@ function createUserClass() {
 function addPrototypeMethod(Constructor) {
   // add a method to the constructor's prototype
   // the method should be called 'sayHi' and should return the string 'Hello World!'
+ 
 }
 
 // !! This is the end of the homework exercises !!
