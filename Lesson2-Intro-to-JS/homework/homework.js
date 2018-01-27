45cc6ef36c8adc453c07d99742ae2b7987a7e0e1
Cesar Mejia Leiva <cmejia@gmail.com>
2018-01-27 07:20:13 -0800

    changed back to single quotations per AirBnB style guide

--- Lesson2-Intro-to-JS/homework/homework.js
+++ Lesson2-Intro-to-JS/homework/homework.js
@@ -2,7 +2,7 @@
 //In these first 6 questions, replace `null` with the answer
 
 //create a string variable, it can contain anything
-let newString = "This is a string";
+let newString = 'This is a string';
 
 //create a number variable, it an be any number
 let newNum = 42; // The answer to the question.
