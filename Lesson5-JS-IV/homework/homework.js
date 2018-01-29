a90705ea90fbf526158afefb614230746a8bfb84
dm <mostdarryl@gmail.com>
2018-01-29 16:52:05 -0500

    lesson 5 hw

--- Lesson5-JS-IV/homework/homework.js
+++ Lesson5-JS-IV/homework/homework.js
@@ -4,27 +4,60 @@ function multiplyArguments() {
   // use the arguments keyword to multiply all of the arguments together and return the product
   // if no arguments are passed in return 0
   // if one argument is passed in just return it
+
+	let product = arguments.length > 0? 1: 0;
+
+	for(let i = 0; i < arguments.length; i++){
+		product *= arguments[i];
+	}
+	return product;
+
+
+
 }
 
 function invokeCallback(cb) {
   // invoke cb
+
+  cb();
 }
 
 function sumArray(numbers, cb) {
   // sum up all of the integers in the numbers array
   // pass the result to cb
   // no return is necessary
+
+  let sum = 0;
+  
+  for(let i = 0; i < numbers.length; i++){
+	  sum += numbers[i];
+  }
+  cb(sum);
 }
 
 function forEach(arr, cb) {
   // iterate over arr and pass its values to cb one by one
   // hint: you will be invoking cb multiple times (once for each value in the array)
+
+
+	arr.forEach(function(item){
+		cb(item);
+	});
+
 }
 
 function map(arr, cb) {
   // create a new array
   // iterate over each value in arr, pass it to cb, then place the value returned from cb into the new arr
   // the new array should be the same length as the array argument
+
+
+	let newArr = [];
+
+	arr.forEach(function(item,index){
+		newArr.push(cb(item));
+		});
+	return newArr;
 }
 
 function createUserClass() {
@@ -34,11 +67,33 @@ function createUserClass() {
   // the constructor should have a method 'sayHi' on its prototype that returns the string 'Hello, my name is {{name}}'
   // {{name}} should be the name set on each instance
   // return the constructor
+
+
+  let User = function(options){
+	this.username = options.username;
+	this.name = options.name;
+	this.email = options.email;
+	this.password = options.password;
+
+	User.prototype.sayHi = function(){
+		return "Hello, my name is " + this.username;
+	}
+
+
+
+  }
+  return User;
 }
 
 function addPrototypeMethod(Constructor) {
   // add a method to the constructor's prototype
   // the method should be called 'sayHi' and should return the string 'Hello World!'
+
+
+  Constructor.prototype.sayHi = function(){
+	  return "Hello World!";
+  }
+
 }
 
 // !! This is the end of the homework exercises !!
@@ -59,6 +114,17 @@ function addReverseString() {
   // name this method reverse
   // hint:
   // you will need to use 'this' inside of reverse
+
+  String.prototype.reverse = function(){
+	  reversedString = "";
+	  for (let i = this.length - 1; i >= 0; i--){
+		  reversedString += this[i];
+	  }
+	  return reversedString;
+
+  }
+
+
 }
 
 function nFactorial(n) {
@@ -66,6 +132,11 @@ function nFactorial(n) {
   // solve this recursively
   // example:
   // the factorial of 3 is 6 (3 * 2 * 1)
+  if( n == 1){
+	  return 1;
+  }
+
+  return n * nFactorial(n-1);
 }
 
 function cacheFunction(cb) {
@@ -80,6 +151,9 @@ function cacheFunction(cb) {
   // if the function you return is invoked with 5 it would pass 5 to cb(5) and return 25
   // if the function you return is invoked again with 5 it will look on an object in the closure scope
   // and return 25 directly and will not invoke cb again
+	  
+
+
 }
 
 // Do not modify code below this line.
