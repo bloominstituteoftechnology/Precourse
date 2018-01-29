241ac263b8faa5633abde47008a6dd4f6690ec11
AVIgyra <vanisackerandrea@yahoo.com>
2018-01-29 10:26:05 +0100

    finished homework

--- Lesson3-JS-II/homework/homework.js
+++ Lesson3-JS-II/homework/homework.js
@@ -3,6 +3,9 @@
 function getBiggest(x, y) {
   // x and y are integers.  Return the larger integer
   // if they are the same return either one
+  if (x>y){ return x;
+} else if (x === y) {return x ||y;
+ } else {return y;}
 }
 
 function greeting(language) {
@@ -11,15 +14,27 @@ function greeting(language) {
   // language: 'English' -> 'Hello!'
   // language: 'Spanish' -> 'Hola!'
   // if language is undefined return 'Hello!'
+  if (language === 'German'){
+  return ('Guten Tag!');
+  }
+  else if (language === 'English'){
+    return ('Hello!');
+}
+  else if (language === 'Spanish'){
+    return ('Hola!');
+}
+  else {return ('Hello!');}
 }
 
 function isTenOrFive(num) {
   // return true if num is 10 or 5
   // otherwise return false
+  return num === 10 ||num ===5;
 }
 
 function isInRange(num) {
   // return true if num is less than 50 and greater than 20
+  return 20<num && num<50;
 }
 
 function isInteger(num) {
@@ -29,6 +44,10 @@ function isInteger(num) {
   // -10 -> true
   // otherwise return false
   // hint: you can solve this using Math.floor
+  if (Math.floor(num)===num) {
+    return true;
+  } else return false;
+
 }
 
 function fizzBuzz(num) {
@@ -36,6 +55,10 @@ function fizzBuzz(num) {
   // if num is divisible by 5 return 'buzz'
   // if num is divisible by 3 & 5 return 'fizzbuzz'
   // otherwise return num
+  if (num % 3===0 && num % 5===0) {return 'fizzbuzz';
+}  else if (num % 3===0){return 'fizz';
+} else if (num % 5===0){return 'buzz';
+} else return num;
 }
 
 function isPrime(num) {
@@ -44,62 +67,103 @@ function isPrime(num) {
   // hint: a prime number is only evenly divisible by itself and 1
   // hint2: you can solve this using a for loop
   // note: 0 and 1 are NOT considered prime numbers
+  for (let i=2; i<num; i++)
+  if (num%i===0){
+    return false;}
+    return num !==1 && num !==0;
 }
 
 function returnFirst(arr) {
   // return the first item from the array
+  return arr[0];
 }
 
 function returnLast(arr) {
   // return the last item of the array
+  return arr[arr.length-1];
 }
 
 function getArrayLength(arr) {
   // return the length of the array
+  return arr.length;
 }
 
 function incrementByOne(arr) {
   // arr is an array of integers  
   // increase each integer by one
   // return the array
+  for (let i=0; i<arr.length; i++){
+    arr[i]+=1;}
+    return arr;
 }
 
 function addItemToArray(arr, item) {
   // add the item to the end of the array
   // return the array
+  arr.push(item);
+  return arr;
 }
 
 function addItemToFront(arr, item) {
   // add the item to the front of the array
   // return the array
   // hint: use the array method .unshift
+  arr.unshift(item);
+  return arr;
 }
 
 function wordsToSentence(words) {
   // words is an array of strings
   // return a string that is all of the words concatenated together
   // spaces need to be between each word
-  // example: ['Hello', 'world!'] -> 'Hello world!'
+  // example: ['Hello', 'world!'] -> 'Hello world!'$
+  let s='';
+  for (let i=0; i<words.length; i++) {
+    s+=words[i];
+    if (words.legnth>1 || i<words.length-1) {
+      s+= ' ';
+    }
+  }
+
+
+return s;
 }
 
 function contains(arr, item) {
   // check to see if item is inside of arr
   // return true if it is, otherwise return false
+  for (let i=0; i<arr.length; i++) {
+    if (arr[i]===item)  return true;
+}
+return false;
 }
 
 function addNumbers(numbers) {
   // numbers is an array of integers.
   // add all of the integers and return the value
+  let s=0;
+  for (let i=0; i<numbers.length; i++){
+    s=s+numbers[i];
+  }
+  return s;
 }
 
 function averageTestScore(testScores) {
   // testScores is an array.  Iterate over testScores and compute the average.
   // return the average
+  let a=0;
+  for (let i=0; i<testScores.length; i++){
+    a=a+testScores[i];
+  }
+  return a/testScores.length;
 }
 
 function largestNumber(numbers) {
   // numbers is an array of integers
   // return the largest integer
+  for (let i=0; i<numbers.length; i++){
+    if (numbers[i]>numbers[i-1]){return numbers [i];}
+  }
 }
 
 // Do not modify code below this line.
