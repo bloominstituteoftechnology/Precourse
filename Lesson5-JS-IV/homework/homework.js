4379426a4755d148bdf0363a4db9ecefb293dda8
SmokeySmoke <35714295+SmokeySmoke@users.noreply.github.com>
2018-01-30 21:16:50 -0500

    Finished lesson 5 homework.

--- Lesson5-JS-IV/homework/homework.js
+++ Lesson5-JS-IV/homework/homework.js
@@ -58,20 +58,17 @@ function createUserClass() {
   // the constructor should have a method 'sayHi' on its prototype that returns the string 'Hello, my name is {{name}}'
   // {{name}} should be the name set on each instance
   // return the constructor
-	class User {
-		options(user, nam, mail, psswd) {
-			this.username = user;
-			this.name = nam;
-			this.email = mail;
-			this.password = psswd;
+	return class User {
+		constructor(options) {
+			this.username = options.username;
+			this.name = options.name;
+			this.email = options.email;
+			this.password = options.password;
 		}
 		sayHi() {
 			return 'Hello, my name is ' + this.name;
 		}
-	}
-
-	return User;
-
+	};
 }
 
 function addPrototypeMethod(Constructor) {
