e76c20221f07b8da319d6c393e15beb2b0fdff72
Johnathan Huggett <johnathanhuggett@Johnathans-MacBook-Pro.local>
2018-01-31 09:25:18 -0500

    all test pass, lesson5

--- Lesson5-JS-IV/homework/homework.js
+++ Lesson5-JS-IV/homework/homework.js
@@ -50,15 +50,31 @@ function map(arr, cb) {
 }
 
 function createUserClass() {
+  class User {
+    constructor(options) {
+      this.username = options.username;
+      this.password = options.password;
+      this.email = options.email;
+      this.name = options.name;      
+    }
+    sayHi(){
+      return `Hello, my name is ${this.name}.`;
+    }
+  } return User;
+}
+  
   // create a class constructor called User
   // it should accept an options object with username, name, email, and password properties
   // in the constructor set the username, name, email, and password properties
   // the constructor should have a method 'sayHi' on its prototype that returns the string 'Hello, my name is {{name}}'
   // {{name}} should be the name set on each instance
   // return the constructor
-}
+
 
 function addPrototypeMethod(Constructor) {
+  Constructor.prototype.sayHi = function() {
+    return 'Hello World!';
+    };
   // add a method to the constructor's prototype
   // the method should be called 'sayHi' and should return the string 'Hello World!'
 }
