0cbe9937aec52b5c5490a6472387cd81e15aafe2
tylar <tylarpiersonbusiness@gmail.com>
2018-02-08 13:43:33 -0800

    filled out the problems

--- Lesson5-JS-II/homework/homework.js
+++ Lesson5-JS-II/homework/homework.js
@@ -4,11 +4,20 @@ function multiplyArguments() {
   // use the arguments keyword to multiply all of the arguments together and return the product
   // if no arguments are passed in return 0
   // if one argument is passed in just return it
-}
+  let sum = 0;
+
+        for (let i = 0; i < arguments.length; i++) {
+            sum = sum + arguments[i];
+        }
+
+        return sum;
+    }
+
 
 function getBiggest(x, y) {
   // x and y are integers.  Return the larger integer
   // if they are the same return either one
+
 }
 
 function greeting(language) {
@@ -17,16 +26,35 @@ function greeting(language) {
   // language: 'English' -> 'Hello!'
   // language: 'Spanish' -> 'Hola!'
   // if language is undefined return 'Hello!'
-}
+  let lang = ['German', 'English', 'Spanish'];
+  if(language === 'German'){
+    return 'Guten Tag!';
+  } else if(language === 'English') {
+    return 'Hello!';
+  } else if(language === 'Spanish') {
+    return 'Hola!';
+  } else {
+    return 'Hello!';
+  }
 
 function isTenOrFive(num) {
   // return true if num is 10 or 5
   // otherwise return false
+  if(num ===5 || num === 10) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function isInRange(num) {
   // return true if num is less than 50 and greater than 20
   // otherwise return false
+  if(num < 50 && num >20) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function isInteger(num) {
@@ -43,6 +71,15 @@ function fizzBuzz(num) {
   // if num is divisible by 5 return 'buzz'
   // if num is divisible by 3 & 5 return 'fizzbuzz'
   // otherwise return num
+  if(num % 3 === 0){
+    return 'fizz';
+  } else if(num % 5 === 0) {
+    return 'buzz';
+  } else if(num % 3 === 0 && num % 5 === 0){
+    return 'fizzbuzz';
+  } else {
+    return num;
+  }
 }
 
 function isPrime(num) {
