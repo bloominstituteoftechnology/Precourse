71575fbec386ec85162bb741e8155c145a9b1c65
Pedro Remedios <pedro.remedios@gmail.com>
2018-02-09 19:59:42 +0000

    Homework Lesson 4 fix quotes

--- Lesson4-JS-I/homework/homework.js
+++ Lesson4-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 //In these first 6 questions, replace `null` with the answer
 
 //create a string variable, it can contain anything
-const newString = "Pedro";
+const newString = 'Pedro';
 
 //create a number variable, it an be any number
 const newNum = 1974;
@@ -143,14 +143,14 @@ function combineNames(firstName, lastName) {
   // return firstName and lastName combined as one string and separated by a space.
   // 'Lambda', 'School' -> 'Lambda School'
   // code here
-  return firstName + " " + lastName;
+  return firstName + ' ' + lastName;
 }
 
 function getGreeting(name) {
   // Take the name string and concatenate other strings onto it so it takes the following form:
   // 'Sam' -> 'Hello Sam!'
   // code here
-  return "Hello " + name + "!";
+  return 'Hello ' + name + '!';
 }
 
 // The next three questions will have you implement math area formulas.
