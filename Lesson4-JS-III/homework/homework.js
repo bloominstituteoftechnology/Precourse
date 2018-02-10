95805308f9fa33082d8c698a9cb387637ad1a997
Abraham Bueno <abrambueno1992@gmail.com>
2018-02-09 22:51:42 -0600

    Lesson 4 completed:Objects, Properties, Methods, for loops, dot notation, bracket notation.

--- Lesson4-JS-III/homework/homework.js
+++ Lesson4-JS-III/homework/homework.js
@@ -5,61 +5,114 @@ function makeCat(name, age) {
   // add an age property to the object with the value set to the age argument
   // add a method called meow that returns the string 'Meow!'
   // return the object
+  const mkCat = {
+    age: age,
+    name: name,
+    meow() {
+      return 'Meow!';
+    },
+  };
+  return mkCat;
 }
 
 function addProperty(object, property) {
   // add the property to the object with a value of null
   // return the object
   // note: the property name is NOT 'property'.  The name is the value of the argument called property (a string)
+ 
+  object[property ] = null;
+  return object;
+  
 }
 
 function invokeMethod(object, method) {
   // method is a string that contains the name of a method on the object
   // invoke this method
   // nothing needs to be returned
+ // invokeMethod['method'] ;
+ object[method]();
 }
 
 function multiplyMysteryNumberByFive(mysteryNumberObject) {
   // mysteryNumberObject has a property called mysteryNumber
   // multiply the mysteryNumber property by 5 and return the product
+  let mb5 = mysteryNumberObject.mysteryNumber*5 ;
+  return mb5 ;
 }
 
 function deleteProperty(object, property) {
   // remove the property from the object
   // return the object
+  delete object[property] ;
+  return object ;
 }
 
 function newUser(name, email, password) {
   // create a new object with properties matching the arguments passed in.
   // return the new object
+
+  const iob = {
+    name: name,
+    email: email,
+    password: password
+  } ;
+  return iob;
 }
 
 function hasEmail(user) {
   // return true if the user has a value for the property 'email'
   // otherwise return false
+  for (let key in user) {
+
+    if ( user[key].length > 0 && key === 'email') {
+  return true ;
+    }
+  } return false ;
+  
 }
 
 function hasProperty(object, property) {
   // return true if the object has the value of the property argument
   // property is a string
   // otherwise return false
+  for (let key in object) {
+    if (property[key] === typeof string || key === property) {
+      return true;
+    }
+  } 
+  return false;
 }
 
 function verifyPassword(user, password) {
   // check to see if the provided password matches the password property on the user object
   // return true if they match
   // otherwise return false
+  for (let key in user) {
+    if (key === 'password' && user['password'] === password) {
+      return true ;
+    }
+  }
+  return false;
 }
 
 function updatePassword(user, newPassword) {
   // replace the existing password on the user object with the value of newPassword
   // return the object
+  for (let key in user) {
+    if (key === 'password') {
+      user.password = newPassword ;
+    }
+  }
+  return user ;
 }
 
 function addFriend(user, newFriend) {
   // user has a property called friends that is an array
   // add newFriend to the end of the friends array
   // return the user object
+  user.friends.push(newFriend) ;
+  return user ;
+
 }
 
 function setUsersToPremium(users) {
@@ -67,6 +120,11 @@ function setUsersToPremium(users) {
   // each user object has the property 'isPremium'
   // set each user's isPremium property to true
   // return the users array
+  for (let i = 0 ; users.length > i ; i++) {
+    users[i].isPremium = true;
+    
+  } 
+  return users;
 }
 
 function sumUserPostLikes(user) {
@@ -75,9 +133,24 @@ function sumUserPostLikes(user) {
   // each post object has an integer property called 'likes'
   // sum together the likes from all the post objects
   // return the sum
+  let netSum = 0;
+  for (let i = 0 ; user.posts.length > i ; i++) {
+    netSum += user.posts[i].likes ;
+  
+  }
+  return netSum ;
+
 }
 
 function addCalculateDiscountPriceMethod(storeItem) {
+ storeItem.calculateDiscountPrice = function() {
+    const damount = this['price'] * this['discountPercentage'];
+    return storeItem.price - damount;
+    
+};
+return storeItem ;
+
+//return storeItem.calculateDiscountPriceS ;
   // add a method to the storeItem object called 'calculateDiscountPrice'
   // this method should multiply the storeItem's 'price' and 'discountPercentage' to get the discount
   // the method then subtracts the discount from the price and returns the discounted price
@@ -85,8 +158,9 @@ function addCalculateDiscountPriceMethod(storeItem) {
   // price -> 20
   // discountPercentage -> .2
   // discountPrice = 20 - (20 * .2)
-}
 
+
+}
 // Do not modify code below this line.
 // --------------------------------
 
@@ -104,5 +178,5 @@ module.exports = {
   addFriend: addFriend,
   setUsersToPremium: setUsersToPremium,
   sumUserPostLikes: sumUserPostLikes,
-  addCalculateDiscountPriceMethod: addCalculateDiscountPriceMethod
+  addCalculateDiscountPriceMethod: addCalculateDiscountPriceMethod,
 };
