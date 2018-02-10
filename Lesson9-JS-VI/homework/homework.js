9b6e379f47bb6a8ab48350c4dcfb7ee38e7639c2
islaybears <marcndanny@gmail.com>
2018-02-09 20:57:50 -0600

    homework 9

--- Lesson9-JS-VI/homework/homework.js
+++ Lesson9-JS-VI/homework/homework.js
@@ -2,23 +2,35 @@
 
 function invokeCallback(cb) {
   // invoke cb
+  cb();
 }
 
 function sumArray(numbers, cb) {
   // sum up all of the integers in the numbers array
   // pass the result to cb
   // no return is necessary
+  cb(numbers.reduce(function(curr, next){
+    return curr + next;
+  }));
 }
 
 function forEach(arr, cb) {
   // iterate over arr and pass its values to cb one by one
   // hint: you will be invoking cb multiple times (once for each value in the array)
+  for(let i=0; i<arr.length; i++){
+    cb(arr[i]);
+  }
 }
 
 function map(arr, cb) {
   // create a new array
   // iterate over each value in arr, pass it to cb, then place the value returned from cb into the new arr
   // the new array should be the same length as the array argument
+  let arr2 = [];
+  for(let i=0; i<arr.length; i++){
+    arr2.push(cb(arr[i]));
+  }
+  return arr2;
 }
 
 // Do not modify code below this line.
