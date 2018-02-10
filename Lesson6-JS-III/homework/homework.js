31a3f30f8aceb3a6aa4b5b158f6357629eff757d
islaybears <marcndanny@gmail.com>
2018-02-09 20:02:44 -0600

    homework 6

--- Lesson6-JS-III/homework/homework.js
+++ Lesson6-JS-III/homework/homework.js
@@ -2,31 +2,41 @@
 
 function returnFirst(arr) {
   // return the first item from the array
+  return arr[0];
 }
 
 function returnLast(arr) {
   // return the last item of the array
+  return arr[arr.length-1];
 }
 
 function getArrayLength(arr) {
   // return the length of the array
+  return arr.length;
 }
 
 function incrementByOne(arr) {
-  // arr is an array of integers  
+  // arr is an array of integers
   // increase each integer by one
   // return the array
+  return arr.map(function(curr){
+    return curr + 1;
+  });
 }
 
 function addItemToArray(arr, item) {
   // add the item to the end of the array
   // return the array
+  arr.push(item);
+  return arr;
 }
 
 function addItemToFront(arr, item) {
   // add the item to the front of the array
   // return the array
   // hint: use the array method .unshift
+  arr.unshift(item);
+  return arr;
 }
 
 function wordsToSentence(words) {
@@ -34,26 +44,42 @@ function wordsToSentence(words) {
   // return a string that is all of the words concatenated together
   // spaces need to be between each word
   // example: ['Hello', 'world!'] -> 'Hello world!'
+  return words.join(' ');
 }
 
 function contains(arr, item) {
   // check to see if item is inside of arr
   // return true if it is, otherwise return false
+  return arr.indexOf(item) !== -1;
 }
 
 function addNumbers(numbers) {
   // numbers is an array of integers.
   // add all of the integers and return the value
+  return numbers.reduce(function(curr, next){
+    return curr + next;
+  });
 }
 
 function averageTestScore(testScores) {
   // testScores is an array.  Iterate over testScores and compute the average.
   // return the average
+  let scoreNumber = testScores.length;
+
+  let total = testScores.reduce(function(curr, next){
+    return curr + next;
+  });
+
+  return total / scoreNumber;
 }
 
 function largestNumber(numbers) {
   // numbers is an array of integers
   // return the largest integer
+  numbers = numbers.sort(function(a, b){
+    return b - a;
+  });
+  return numbers[0];
 }
 
 // Do not modify code below this line.
