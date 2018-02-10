009a113cf23b52978c489ed1791c47780eec59cd
islaybears <marcndanny@gmail.com>
2018-02-09 19:40:24 -0600

    homework 5

--- Lesson5-JS-II/homework/homework.js
+++ Lesson5-JS-II/homework/homework.js
@@ -1,14 +1,24 @@
 // Do not change any of the function names
 
 function multiplyArguments() {
+  if(arguments.length === 0) return 0;
+
   // use the arguments keyword to multiply all of the arguments together and return the product
   // if no arguments are passed in return 0
   // if one argument is passed in just return it
+  let initVal = 1;
+
+  for(let i=0; i<arguments.length; i++){
+    initVal *= arguments[i];
+  }
+
+  return initVal;
 }
 
 function getBiggest(x, y) {
   // x and y are integers.  Return the larger integer
   // if they are the same return either one
+  return x > y ? x : y;
 }
 
 function greeting(language) {
@@ -17,16 +27,28 @@ function greeting(language) {
   // language: 'English' -> 'Hello!'
   // language: 'Spanish' -> 'Hola!'
   // if language is undefined return 'Hello!'
+  switch(language){
+    case 'German':
+      return 'Guten Tag!';
+    case 'English':
+      return 'Hello!';
+    case 'Spanish':
+      return 'Hola!';
+    default:
+      return 'Hello!';
+  }
 }
 
 function isTenOrFive(num) {
   // return true if num is 10 or 5
   // otherwise return false
+  return num === 5 || num === 10;
 }
 
 function isInRange(num) {
   // return true if num is less than 50 and greater than 20
   // otherwise return false
+  return num < 50 && num > 20;
 }
 
 function isInteger(num) {
@@ -36,6 +58,7 @@ function isInteger(num) {
   // -10 -> true
   // otherwise return false
   // hint: you can solve this using Math.floor
+  return num % 1 === 0;
 }
 
 function fizzBuzz(num) {
@@ -43,14 +66,29 @@ function fizzBuzz(num) {
   // if num is divisible by 5 return 'buzz'
   // if num is divisible by 3 & 5 return 'fizzbuzz'
   // otherwise return num
+  let str = '';
+
+  if(num % 3 === 0)
+    str += 'fizz';
+  if(num % 5 === 0)
+    str += 'buzz';
+
+  return str.length > 0 ? str : num;
 }
 
 function isPrime(num) {
+  if(num < 2)
+    return false;
   // return true if num is prime.
   // otherwise return false
   // hint: a prime number is only evenly divisible by itself and 1
   // hint2: you can solve this using a for loop
   // note: 0 and 1 are NOT considered prime numbers
+  for(let i=2; i<num; i++){
+    if(num % i === 0)
+      return false;
+  }
+  return true;
 }
 
 
