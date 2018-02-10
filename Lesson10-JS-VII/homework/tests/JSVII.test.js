2775417c9406ceff4e2ad94b78423b43914ecc23
islaybears <marcndanny@gmail.com>
2018-02-09 21:34:10 -0600

    homework 10

--- Lesson10-JS-VII/homework/tests/JSVII.test.js
+++ Lesson10-JS-VII/homework/tests/JSVII.test.js
@@ -2,13 +2,13 @@ var exercises = require('../homework');
 
 describe('counter', () => {
 	it('should return a function', () => {
-		expect(typeof closureMethods.counter()).toBe('function');
+		expect(typeof exercises.counter()).toBe('function');
 	});
 	it('should return 1 when the returned function is invoked', () => {
-		expect(closureMethods.counter()()).toBe(1);
+		expect(exercises.counter()()).toBe(1);
 	});
 	it('should increment and return the number each time the function is invoked', () => {
-		const counterFunction = closureMethods.counter();
+		const counterFunction = exercises.counter();
 		expect(counterFunction()).toBe(1);
 		expect(counterFunction()).toBe(2);
 		expect(counterFunction()).toBe(3);
