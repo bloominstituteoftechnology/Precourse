5d66eea6dec9bacaaf767ae80d35647d55d892d1
tylar <tylarpiersonbusiness@gmail.com>
2018-02-12 12:51:26 -0800

    added code to isPrime

--- Lesson5-JS-II/homework/homework.js
+++ Lesson5-JS-II/homework/homework.js
@@ -95,6 +95,11 @@ function isPrime(num) {
   // hint: a prime number is only evenly divisible by itself and 1
   // hint2: you can solve this using a for loop
   // note: 0 and 1 are NOT considered prime numbers
+    if(num % 1 === 0) {
+      return num;
+    } else {
+      return false;
+    } 
 }
 
 
