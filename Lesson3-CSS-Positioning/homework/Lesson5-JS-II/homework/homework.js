89ed259605f573fe182188df6a9f5dff8b5db2c1
krisjnbri <kristian.briscoe@pronotmail.com>
2018-02-13 14:08:01 -0500

    Finished lesson 4 JS

---
+++ Lesson3-CSS-Positioning/homework/Lesson5-JS-II/homework/homework.js
@@ -0,0 +1,70 @@
+// Do not change any of the function names
+
+function multiplyArguments() {
+  // use the arguments keyword to multiply all of the arguments together and return the product
+  // if no arguments are passed in return 0
+  // if one argument is passed in just return it
+}
+
+function getBiggest(x, y) {
+  // x and y are integers.  Return the larger integer
+  // if they are the same return either one
+}
+
+function greeting(language) {
+  // return a greeting for three different languages:
+  // language: 'German' -> 'Guten Tag!'
+  // language: 'English' -> 'Hello!'
+  // language: 'Spanish' -> 'Hola!'
+  // if language is undefined return 'Hello!'
+}
+
+function isTenOrFive(num) {
+  // return true if num is 10 or 5
+  // otherwise return false
+}
+
+function isInRange(num) {
+  // return true if num is less than 50 and greater than 20
+  // otherwise return false
+}
+
+function isInteger(num) {
+  // return true if num is an integer
+  // 0.8 -> false
+  // 1 -> true
+  // -10 -> true
+  // otherwise return false
+  // hint: you can solve this using Math.floor
+}
+
+function fizzBuzz(num) {
+  // if num is divisible by 3 return 'fizz'
+  // if num is divisible by 5 return 'buzz'
+  // if num is divisible by 3 & 5 return 'fizzbuzz'
+  // otherwise return num
+}
+
+function isPrime(num) {
+  // return true if num is prime.
+  // otherwise return false
+  // hint: a prime number is only evenly divisible by itself and 1
+  // hint2: you can solve this using a for loop
+  // note: 0 and 1 are NOT considered prime numbers
+}
+
+
+
+// Do not modify code below this line.
+// --------------------------------
+
+module.exports = {
+  multiplyArguments: multiplyArguments,
+  getBiggest: getBiggest,
+  greeting: greeting,
+  isTenOrFive: isTenOrFive,
+  isInRange: isInRange,
+  isInteger: isInteger,
+  fizzBuzz: fizzBuzz,
+  isPrime: isPrime,
+};
---
+++ Lesson3-CSS-Positioning/homework/Lesson6-JS-III/homework/README.md
@@ -0,0 +1,12 @@
+# Homework #JSIII
+
+## Instructions
+---
+1. Feynman Writing Prompts - Write out explanations of the following concepts like you are explaining it to a 12 year old.  Doing this will help you quickly discover any holes in your understanding.  Ask your questions on Slack.
+		
+	* Arrays
+
+2. 3. From the top level of your `Precourse` folder, run `npm test JSIII` to run the automated tests. You will fill out the functions in `homework.js` to make the tests pass.
+
+
+For more information about Lambda School's six month CS program visit: https://lambdaschool.com
