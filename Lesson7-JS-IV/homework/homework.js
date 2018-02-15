ce37838817cc8b7034c92948d55569fe7ea6967c
Andy <andy.hyon@gmail.com>
2018-02-14 22:09:26 -0800

    started hw7

--- Lesson7-JS-IV/homework/homework.js
+++ Lesson7-JS-IV/homework/homework.js
@@ -5,6 +5,12 @@ function makeCat(name, age) {
   // add an age property to the object with the value set to the age argument
   // add a method called meow that returns the string 'Meow!'
   // return the object
+  const catObject = {
+    name,
+    age,
+    meow: function() {return 'Meow!';}
+  };
+  return catObject;
 }
 
 function addProperty(object, property) {
@@ -17,6 +23,7 @@ function invokeMethod(object, method) {
   // method is a string that contains the name of a method on the object
   // invoke this method
   // nothing needs to be returned
+
 }
 
 function multiplyMysteryNumberByFive(mysteryNumberObject) {
@@ -82,7 +89,7 @@ function addCalculateDiscountPriceMethod(storeItem) {
   // this method should multiply the storeItem's 'price' and 'discountPercentage' to get the discount
   // the method then subtracts the discount from the price and returns the discounted price
   // return storeItem at the end of the function
-  // example: 
+  // example:
   // price -> 20
   // discountPercentage -> .2
   // discountPrice = 20 - (20 * .2)
