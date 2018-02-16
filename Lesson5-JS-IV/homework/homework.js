12b26b68ac31f046d03da10e47cf7dafbc16a7a6
tommycam <tommycam67@gmail.com>
2018-02-16 09:12:51 -0600

    finished homework 4

--- Lesson5-JS-IV/homework/homework.js
+++ Lesson5-JS-IV/homework/homework.js
@@ -4,27 +4,51 @@ function multiplyArguments() {
   // use the arguments keyword to multiply all of the arguments together and return the product
   // if no arguments are passed in return 0
   // if one argument is passed in just return it
+  let product = 1;
+  if (arguments.length == 0) {
+    return 0;
+  } else if (arguments.length == 1) {
+    return arguments[0];
+  } else {
+    for (let i = 0; i < arguments.length; i++) {
+      product *= arguments[i];
+    }
+    return product;
+  }
 }
 
 function invokeCallback(cb) {
   // invoke cb
+  cb();
 }
 
 function sumArray(numbers, cb) {
   // sum up all of the integers in the numbers array
   // pass the result to cb
   // no return is necessary
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  cb(sum);
 }
 
 function forEach(arr, cb) {
   // iterate over arr and pass its values to cb one by one
   // hint: you will be invoking cb multiple times (once for each value in the array)
+  for (let i = 0; i < arr.length; i++) {
+    cb(arr[i]);
+  }
 }
 
 function map(arr, cb) {
   // create a new array
   // iterate over each value in arr, pass it to cb, then place the value returned from cb into the new arr
   // the new array should be the same length as the array argument
+  let newArr = arr.map(function(item) {
+    return cb(item);
+  });
+  return newArr;
 }
 
 function createUserClass() {
@@ -34,11 +58,28 @@ function createUserClass() {
   // the constructor should have a method 'sayHi' on its prototype that returns the string 'Hello, my name is {{name}}'
   // {{name}} should be the name set on each instance
   // return the constructor
+  class User {
+
+    constructor(options) {
+      this.name = options.name;
+      this.username = options.username;
+      this.email = options.email;
+      this.password = options.password;
+    }
+
+    sayHi() {
+      return 'Hello, my name is ' + this.name;
+    }
+  }
+  return User;
 }
 
 function addPrototypeMethod(Constructor) {
   // add a method to the constructor's prototype
   // the method should be called 'sayHi' and should return the string 'Hello World!'
+  Constructor.prototype.sayHi = function() {
+    return 'Hello World!';
+  };
 }
 
 // !! This is the end of the homework exercises !!
@@ -47,10 +88,10 @@ function addPrototypeMethod(Constructor) {
 
 
 
-// !! ------------------ !! 
+// !! ------------------ !!
 /* The following exercises are extra credit, they are not required as part of the homework. In order to solve these problems you will need
-   to independantly study the concepts of Closure, and Recursion. There are links in the main README file to strt you on your journey. 
-   If you want to attempt these exercises, you will need to access the test.js file in the tests folder, and uncomment the tests pertaining 
+   to independantly study the concepts of Closure, and Recursion. There are links in the main README file to strt you on your journey.
+   If you want to attempt these exercises, you will need to access the test.js file in the tests folder, and uncomment the tests pertaining
    to the exercise you are attempting. Good luck! */
 
 
@@ -59,6 +100,14 @@ function addReverseString() {
   // name this method reverse
   // hint:
   // you will need to use 'this' inside of reverse
+  String.prototype.reverse = function() {
+    let revString = '';
+    for (let i = this.length - 1; i >= 0; i--) {
+      revString += this[i];
+    }
+    return revString;
+  };
+
 }
 
 function nFactorial(n) {
@@ -66,6 +115,11 @@ function nFactorial(n) {
   // solve this recursively
   // example:
   // the factorial of 3 is 6 (3 * 2 * 1)
+  let factorial = 1;
+  for (let i = 1; i <= n; i++) {
+    factorial *= i;
+  }
+  return factorial;
 }
 
 function cacheFunction(cb) {
@@ -80,6 +134,15 @@ function cacheFunction(cb) {
   // if the function you return is invoked with 5 it would pass 5 to cb(5) and return 25
   // if the function you return is invoked again with 5 it will look on an object in the closure scope
   // and return 25 directly and will not invoke cb again
+  let cache = {};
+
+  let cbCache = function(arg) {
+    if (!cache.hasOwnProperty(arg)) {
+      cache[arg] = cb(arg);
+    }
+    return cache[arg];
+  };
+  return cbCache;
 }
 
 // Do not modify code below this line.
