3c779c35a6966d7833ca20268d01751dfff8241f
Perry Tran <perrytran@Perrys-MacBook-Pro.local>
2018-02-16 11:46:23 -0800

    Started on Lesson 5 Homework

--- Lesson5-JS-II/homework/homework.js
+++ Lesson5-JS-II/homework/homework.js
@@ -4,12 +4,26 @@ function multiplyArguments() {
   // use the arguments keyword to multiply all of the arguments together and return the product
   // if no arguments are passed in return 0
   // if one argument is passed in just return it
+  if(arguments === 0) {
+    return 0;
+  }
+  else if (arguments === 1) {
+    return arguments;
+  }
 }
 
 function getBiggest(x, y) {
   // x and y are integers.  Return the larger integer
   // if they are the same return either one
+  if(x > y) {
+    return x;
+} else if (y > x) {
+    return y;
+  } else if (x === y) {
+    return x || y;
+  }
 }
+
 
 function greeting(language) {
   // return a greeting for three different languages:
@@ -17,11 +31,29 @@ function greeting(language) {
   // language: 'English' -> 'Hello!'
   // language: 'Spanish' -> 'Hola!'
   // if language is undefined return 'Hello!'
+  if (language === 'German') {
+    return 'Guten Tag!';
+  }
+  else if (language === 'English') {
+    return 'Hello!';
+  }
+  else if (language === 'Spanish') {
+    return 'Hola!';
+  }
+  else {
+    return 'Hello!';
+  }
 }
 
 function isTenOrFive(num) {
   // return true if num is 10 or 5
   // otherwise return false
+  if(num === 10 || 5) {
+    return true;
+  } 
+  else if (num !== 10 || 5) {
+    return false;
+  }
 }
 
 function isInRange(num) {
