731aaa7b1db235d0df88c5a151477589e0df5cc6
Rafal <rafalkamka@gmail.com>
2018-02-21 00:06:02 +0100

    Zadanko 6 zrobione

--- Lesson6-JS-III/homework/homework.js
+++ Lesson6-JS-III/homework/homework.js
@@ -2,31 +2,42 @@
 
 function returnFirst(arr) {
   // return the first item from the array
+    return arr[0];
 }
 
 function returnLast(arr) {
   // return the last item of the array
+    return arr[arr.length - 1];
 }
 
 function getArrayLength(arr) {
   // return the length of the array
+    return arr.length;
 }
 
 function incrementByOne(arr) {
   // arr is an array of integers  
   // increase each integer by one
   // return the array
+    for(var i = 0; i < arr.length; i++){
+        arr[i] += 1;
+    }
+    return arr;
 }
 
 function addItemToArray(arr, item) {
   // add the item to the end of the array
   // return the array
+    arr.push(item);
+    return arr;
 }
 
 function addItemToFront(arr, item) {
   // add the item to the front of the array
   // return the array
   // hint: use the array method .unshift
+    arr.unshift(item);
+    return arr;
 }
 
 function wordsToSentence(words) {
@@ -34,26 +45,83 @@ function wordsToSentence(words) {
   // return a string that is all of the words concatenated together
   // spaces need to be between each word
   // example: ['Hello', 'world!'] -> 'Hello world!'
+    var result = '';
+    for( var i = 0; i < words.length; i++){
+        if(words[i] == words[words.length - 1]){
+        result += words[i];    
+        }
+        else if(words[i] != words[words.length - 1]){
+        result += words[i] + ' ';
+        }
+        
+    }
+    return result;
 }
 
 function contains(arr, item) {
   // check to see if item is inside of arr
   // return true if it is, otherwise return false
+    var rozsadzacz = false;
+    for(var i = 0; i < arr.length; i++){
+        /*if(item == arr[i]){
+            rozsadzacz = true;
+            return rozsadzacz;
+        }
+        else if(item == ''){
+            rozsadzacz = false;
+        }
+        else{
+            rozsadzacz = false;
+        }*/
+        if(item !== arr[i] || arr.length < 1 || arr == undefined){
+            rozsadzacz = false;
+        }
+        else if(arr == []){
+            rozsadzacz = false;
+        }
+        else{
+            rozsadzacz = true;
+            return rozsadzacz;
+        }
+        
+    }
+    return rozsadzacz;
 }
 
 function addNumbers(numbers) {
   // numbers is an array of integers.
   // add all of the integers and return the value
+    var result = 0;
+    for( var i = 0; i < numbers.length; i++){
+        result += numbers[i];
+    }
+    return result;
 }
 
 function averageTestScore(testScores) {
   // testScores is an array.  Iterate over testScores and compute the average.
   // return the average
+    if(Array.isArray(testScores)){
+        var result = 0;
+        for(var i = 0; i < testScores.length; i++){
+            result += testScores[i];
+        }
+        var average = result / i;
+        return average;
+        
+    }
 }
 
 function largestNumber(numbers) {
   // numbers is an array of integers
   // return the largest integer
+    var theBiggest = 0;
+    for(var i = 0; i < numbers.length; i++){
+        if(numbers[i] > theBiggest){
+            theBiggest = numbers[i];
+        }
+    }
+    return theBiggest;
 }
 
 // Do not modify code below this line.
