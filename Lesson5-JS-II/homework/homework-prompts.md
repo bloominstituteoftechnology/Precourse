9eda83ec2518941250965272ece0a4987fbd15cf
bsmart <brittany.smart@portlandoregon.gov>
2018-02-20 13:52:08 -0800

    Lesson5 HW

--- Lesson5-JS-II/homework/homework-prompts.md
+++ Lesson5-JS-II/homework/homework-prompts.md
@@ -4,7 +4,11 @@
 1. Feynman Writing Prompts - Write out explanations of the following concepts like you are explaining it to a 12 year old.  Doing this will help you quickly discover any holes in your understanding.  Ask your questions on Slack.
 
 	* `for`
+        - Runs a code block so long as what is inside the () is TRUE.
 	* `&&`, `||`, `!`
+        **&&** the code runs if both satements are TRUE
+        **||** code runs if at least one statement must be True
+        **!** will flip the statements truthiness to the opposite value. (!true) === not true or false
 
 2. From the top level of your `Precourse` folder, run `npm test JSII` to run the automated tests. You will fill out the functions in `homework.js` to make the tests pass.
 
--- Lesson5-JS-II/homework/homework.js
+++ Lesson5-JS-II/homework/homework.js
@@ -8,6 +8,11 @@ function multiplyArguments() {
 
 function getBiggest(x, y) {
   // x and y are integers.  Return the larger integer
+  if(x > y) {
+    return x;
+  } else {
+    return y;
+  }
   // if they are the same return either one
 }
 
@@ -17,16 +22,35 @@ function greeting(language) {
   // language: 'English' -> 'Hello!'
   // language: 'Spanish' -> 'Hola!'
   // if language is undefined return 'Hello!'
+  if(language === 'German') {
+    return 'Guten Tag!';
+  } else if (language === 'English') {
+    return 'Hello!';
+  } else if (language === 'Spanish') {
+    return 'Hola!';
+  } else {
+    return 'Hello!';
+  }
 }
 
 function isTenOrFive(num) {
   // return true if num is 10 or 5
   // otherwise return false
+  if(num === 10 || num === 5) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function isInRange(num) {
   // return true if num is less than 50 and greater than 20
   // otherwise return false
+  if(num < 50 && num > 20) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function isInteger(num) {
@@ -36,6 +60,11 @@ function isInteger(num) {
   // -10 -> true
   // otherwise return false
   // hint: you can solve this using Math.floor
+  if(Math.floor(num) !== 0 || num === 0){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function fizzBuzz(num) {
@@ -43,6 +72,15 @@ function fizzBuzz(num) {
   // if num is divisible by 5 return 'buzz'
   // if num is divisible by 3 & 5 return 'fizzbuzz'
   // otherwise return num
+  if(num%3 === 0  && num%5 === 0) {
+    return 'fizzbuzz';
+  } else if(num%3 === 0) {
+    return 'fizz';
+  } else if(num%5 === 0) {
+    return 'buzz';
+  } else {
+    return num;
+  }
 }
 
 function isPrime(num) {
@@ -51,6 +89,14 @@ function isPrime(num) {
   // hint: a prime number is only evenly divisible by itself and 1
   // hint2: you can solve this using a for loop
   // note: 0 and 1 are NOT considered prime numbers
+
+
+    for (var i = 2; i < num; i++) { // i will always be less than the parameter so the condition below will never allow parameter to be divisible by itself ex. (7 % 7 = 0) which would return true
+      if (num % i === 0) return false; // when parameter is divisible by i, it's not a prime number so return false
+    }
+    return num > 1;
+
+
 }
 
 
