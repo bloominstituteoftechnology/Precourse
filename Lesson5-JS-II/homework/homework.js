b4f4f36b9b15d97d5492e3ab49f0b39a557fafbe
bsmart <brittany.smart@portlandoregon.gov>
2018-02-21 14:17:07 -0800

    L6 HW

--- Lesson5-JS-II/homework/homework.js
+++ Lesson5-JS-II/homework/homework.js
@@ -90,13 +90,10 @@ function isPrime(num) {
   // hint2: you can solve this using a for loop
   // note: 0 and 1 are NOT considered prime numbers
 
-
-    for (var i = 2; i < num; i++) { // i will always be less than the parameter so the condition below will never allow parameter to be divisible by itself ex. (7 % 7 = 0) which would return true
-      if (num % i === 0) return false; // when parameter is divisible by i, it's not a prime number so return false
-    }
-    return num > 1;
-
-
+  for (let i = 2; i < num; i++) { // i will always be less than the parameter so the condition below will never allow parameter to be divisible by itself ex. (7 % 7 = 0) which would return true
+    if (num % i === 0) return false; // when parameter is divisible by i, it's not a prime number so return false
+  }
+  return num > 1; // otherwise it's a prime number so return true (it also must be greater than 1, reason for the n > 1 instead of true)
 }
 
 
--- Lesson6-JS-III/homework/README.md
+++ Lesson6-JS-III/homework/README.md
@@ -3,8 +3,8 @@
 ## Instructions
 ---
 1. Feynman Writing Prompts - Write out explanations of the following concepts like you are explaining it to a 12 year old.  Doing this will help you quickly discover any holes in your understanding.  Ask your questions on Slack.
-		
-	* Arrays
+
+	* Arrays - a bucket that can hold multiple types of items (data types). Each item has a number, the first one starts with zero. You can use these numbers to access specific items in the array.
 
 2. 3. From the top level of your `Precourse` folder, run `npm test JSIII` to run the automated tests. You will fill out the functions in `homework.js` to make the tests pass.
 
