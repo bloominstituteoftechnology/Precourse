bac62bc5cde5cface24f120ee67e1f1ea612eb74
Derrick Mei <derrick.mei.168@gmail.com>
2018-02-22 02:33:40 -0600

    Derricks Lesson 5 Homework

--- Lesson5-JS-II/homework/homework.js
+++ Lesson5-JS-II/homework/homework.js
@@ -1,17 +1,35 @@
 // Do not change any of the function names
 
 function multiplyArguments() {
+  var x=1;
+  if (arguments.length===0){
+    return 0;
+    }
+  else {
+    for (let i=0; i<arguments.length; i++){
+      x *= arguments[i];
+    }
+    return x;
+  }
   // use the arguments keyword to multiply all of the arguments together and return the product
   // if no arguments are passed in return 0
   // if one argument is passed in just return it
 }
 
 function getBiggest(x, y) {
+  if (x>=y){
+    return x;}
+  else return y;
   // x and y are integers.  Return the larger integer
   // if they are the same return either one
 }
 
 function greeting(language) {
+  var polyglot = {German: 'Guten Tag!', Mandarin: 'Ni Hao!', Spanish: 'Hola!'};
+  if (!(language in polyglot)){
+    return 'Hello!';
+  }
+  else return polyglot[language];
   // return a greeting for three different languages:
   // language: 'German' -> 'Guten Tag!'
   // language: 'Mandarin' -> 'Ni Hao!'
@@ -20,16 +38,19 @@ function greeting(language) {
 }
 
 function isTenOrFive(num) {
+  return num === 10 || num === 5;
   // return true if num is 10 or 5
   // otherwise return false
 }
 
 function isInRange(num) {
+  return num < 50 && num > 20;
   // return true if num is less than 50 and greater than 20
   // otherwise return false
 }
 
 function isInteger(num) {
+  return num === Math.round(num);
   // return true if num is an integer
   // 0.8 -> false
   // 1 -> true
@@ -39,6 +60,10 @@ function isInteger(num) {
 }
 
 function fizzBuzz(num) {
+  if (num%15===0){return 'fizzbuzz';}
+  else if (num%3===0){return 'fizz';}
+  else if (num%5===0){return 'buzz';}
+  else return num;
   // if num is divisible by 3 return 'fizz'
   // if num is divisible by 5 return 'buzz'
   // if num is divisible by 3 & 5 return 'fizzbuzz'
@@ -46,6 +71,9 @@ function fizzBuzz(num) {
 }
 
 function isPrime(num) {
+  for(var i = 2; i < num; i++) {
+    if(num % i === 0)  {return false;}}
+    return num > 1; 
   // return true if num is prime.
   // otherwise return false
   // hint: a prime number is only evenly divisible by itself and 1
