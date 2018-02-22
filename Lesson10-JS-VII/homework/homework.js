cf3ab6324ad7af1a70db8559f221d8035a5211a7
Justin Hong <jhong2009@gmail.com>
2018-02-21 19:30:34 -0800

    upload lesson 10 homework

--- Lesson10-JS-VII/homework/homework.js
+++ Lesson10-JS-VII/homework/homework.js
@@ -5,6 +5,11 @@ function counter() {
   // Example: const newCounter = counter();
   // newCounter(); // 1
   // newCounter(); // 2
+
+  var counter = 1;
+  return function() {
+  	return counter++;
+  }
 }
 
 function cacheFunction(cb) {
@@ -18,6 +23,16 @@ function cacheFunction(cb) {
   // if the function you return is invoked with 5 it would pass 5 to cb(5) and return 25
   // if the function you return is invoked again with 5 it will look on an object in the closure scope
   // and return 25 directly and will not invoke cb again
+  
+  let cache = {};
+  return function (x) {
+    if (cache.hasOwnProperty(x)) {
+      return cache[x];
+    } else {
+      cache[x] = cb(x);
+      return cache[x];
+    }
+  }
 }
 
 // Do not modify code below this line.
