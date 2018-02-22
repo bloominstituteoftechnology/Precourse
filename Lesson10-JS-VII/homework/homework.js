001c1f03c1be4ccc3e863b9e121e79919c668818
Kyle Kauzlarich <kauzlarich.kyle@gmail.com>
2018-02-21 23:33:15 -0800

    currently at last question

--- Lesson10-JS-VII/homework/homework.js
+++ Lesson10-JS-VII/homework/homework.js
@@ -1,10 +1,13 @@
 // Do not change any of the function names
-
 function counter() {
   // Return a function that when invoked increments and returns a counter variable.
   // Example: const newCounter = counter();
   // newCounter(); // 1
   // newCounter(); // 2
+  let newCounter = 0;
+  return function counter() {
+    return newCounter += 1;
+  }
 }
 
 function cacheFunction(cb) {
@@ -18,6 +21,9 @@ function cacheFunction(cb) {
   // if the function you return is invoked with 5 it would pass 5 to cb(5) and return 25
   // if the function you return is invoked again with 5 it will look on an object in the closure scope
   // and return 25 directly and will not invoke cb again
+  return function cacheFunction(cb) {
+   return cb * 2;
+  }
 }
 
 // Do not modify code below this line.
