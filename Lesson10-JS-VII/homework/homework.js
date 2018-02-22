cb3069a8b62d7e188192cf25a647886663c4a966
Kyle Kauzlarich <kauzlarich.kyle@gmail.com>
2018-02-21 23:56:18 -0800

    lesson finished

--- Lesson10-JS-VII/homework/homework.js
+++ Lesson10-JS-VII/homework/homework.js
@@ -21,9 +21,16 @@ function cacheFunction(cb) {
   // if the function you return is invoked with 5 it would pass 5 to cb(5) and return 25
   // if the function you return is invoked again with 5 it will look on an object in the closure scope
   // and return 25 directly and will not invoke cb again
-  return function cacheFunction(cb) {
-   return cb * 2;
-  }
+    let obj = {};
+    return function(x) {
+      if (obj.hasOwnProperty(x)) {
+        return obj[x];
+      } else {
+        let result = cb(x);
+        obj[x] = result;
+        return result;
+      }
+    }
 }
 
 // Do not modify code below this line.
