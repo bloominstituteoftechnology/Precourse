de0a845660d4faf0c7deb1eeb1b03026e21c4afc
Alex Martin <atonymartin20@yahoo.com>
2018-02-25 09:26:51 -0600

    Lesson 5 Completed

--- Lesson5-JS-IV/homework/homework.js
+++ Lesson5-JS-IV/homework/homework.js
@@ -4,27 +4,68 @@ function multiplyArguments() {
   // use the arguments keyword to multiply all of the arguments together and return the product
   // if no arguments are passed in return 0
   // if one argument is passed in just return it
+  if (arguments.length === 0){
+    return 0;
+  } else if (arguments.length === 1) {
+    return arguments[0];
+  }
+  else {
+    let product = 1;
+    for (let i = 0; i < arguments.length; i++) {
+      product *= arguments[i];
+    }
+    return product;
+  }
 }
 
 function invokeCallback(cb) {
   // invoke cb
+  cb ();
 }
 
 function sumArray(numbers, cb) {
   // sum up all of the integers in the numbers array
   // pass the result to cb
   // no return is necessary
+ /* let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  cb(sum);
+  */
+  let sum = 0;
+  numbers.forEach(function(int) {
+    sum += int;
+  });
+  cb(sum);
 }
 
 function forEach(arr, cb) {
   // iterate over arr and pass its values to cb one by one
   // hint: you will be invoking cb multiple times (once for each value in the array)
+  /*for (let i = 0; i < arr.length; i++) {
+    cb(arr[i]);
+  } */
+  arr.forEach(function(value) {
+    cb(value);
+  });
 }
 
 function map(arr, cb) {
   // create a new array
   // iterate over each value in arr, pass it to cb, then place the value returned from cb into the new arr
   // the new array should be the same length as the array argument
+/*  let arr2 = [];
+  for (let i = 0; i < arr.length; i++) {
+    cb(arr[i]);
+    arr2.push(cb(arr[i]));
+  }
+  return arr2; */
+  const arr2 = [];
+  arr.map(function(value, index) {
+    arr2[index] = cb(value);
+  });
+  return arr2;
 }
 
 function createUserClass() {
@@ -34,11 +75,26 @@ function createUserClass() {
   // the constructor should have a method 'sayHi' on its prototype that returns the string 'Hello, my name is {{name}}'
   // {{name}} should be the name set on each instance
   // return the constructor
+  class User {
+      constructor(object) {
+    this.username = object.username;
+    this.name = object.name;
+    this.email = object.email;
+    this.password = object.password;
+    }
+    sayHi() {
+      return 'Hello, my name is {{name}}';
+    }
+  }
+  return User;
 }
 
 function addPrototypeMethod(Constructor) {
   // add a method to the constructor's prototype
   // the method should be called 'sayHi' and should return the string 'Hello World!'
+  Constructor.prototype.sayHi = function() {
+    return 'Hello World!';
+    };
 }
 
 // !! This is the end of the homework exercises !!
@@ -54,21 +110,21 @@ function addPrototypeMethod(Constructor) {
    to the exercise you are attempting. Good luck! */
 
 
-function addReverseString() {
+//function addReverseString(Constructor) {
   // add a method to the string constructor's prototype that returns a reversed copy of the string
   // name this method reverse
   // hint:
   // you will need to use 'this' inside of reverse
-}
+//}
 
-function nFactorial(n) {
+//function nFactorial(n) {
   // return the factorial for n
   // solve this recursively
   // example:
   // the factorial of 3 is 6 (3 * 2 * 1)
-}
+//}
 
-function cacheFunction(cb) {
+//function cacheFunction(cb) {
   // Extra Credit
   // use closure to create a cache for the cb function
   // the function that you return should accept a single argument and invoke cb with that argument
@@ -80,7 +136,7 @@ function cacheFunction(cb) {
   // if the function you return is invoked with 5 it would pass 5 to cb(5) and return 25
   // if the function you return is invoked again with 5 it will look on an object in the closure scope
   // and return 25 directly and will not invoke cb again
-}
+//}
 
 // Do not modify code below this line.
 // --------------------------------
@@ -93,7 +149,7 @@ module.exports = {
   map: map,
   createUserClass: createUserClass,
   addPrototypeMethod: addPrototypeMethod,
-  addReverseString: addReverseString,
+ /* addReverseString: addReverseString,
   nFactorial: nFactorial,
-  cacheFunction: cacheFunction
+  cacheFunction: cacheFunction*/
 };
