24e205cce1f7d5ca3636c61747271a4a411787a9
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-02-26 09:06:07 -0800

    commited out multiplyArguments code

--- Lesson5-JS-II/homework/homework.js
+++ Lesson5-JS-II/homework/homework.js
@@ -4,14 +4,14 @@ function multiplyArguments() {
   // use the arguments keyword to multiply all of the arguments together and return the product
   // if no arguments are passed in return 0
   // if one argument is passed in just return it
-  let element = 1;
-  if(arguments.length === 0) {
-    return 0;
-    for(let i = 0; i < arguments.length; i++) {
-      element = element * arguments[i];
-    }
-  }
-  return element;
+  // let element = 1;
+  // if(arguments.length === 0) {
+  //   return 0;
+  //   for(let i = 0; i < arguments.length; i++) {
+  //     element = element * arguments[i];
+  //   }
+  // }
+  // return element;
 }
 
 function getBiggest(x, y) {
