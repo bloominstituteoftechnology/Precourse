e7f2041aaa7bbb9750e13475eac198f72c0889ed
Paul Fang <paulfang@pauls-mbp.home>
2018-02-27 19:33:32 -0800

    hw4

--- Lesson5-JS-IV/homework/homework.js
+++ Lesson5-JS-IV/homework/homework.js
@@ -4,27 +4,38 @@ function multiplyArguments() {
   // use the arguments keyword to multiply all of the arguments together and return the product
   // if no arguments are passed in return 0
   // if one argument is passed in just return it
+  if (arguments.length === 0) return 0;
+  let retVal = arguments[0];
+
+  for (let i = 1; i < arguments.length; i++) {
+    retVal *= arguments[i];
+  }
+  return retVal;
 }
 
 function invokeCallback(cb) {
   // invoke cb
+  cb();
 }
 
 function sumArray(numbers, cb) {
   // sum up all of the integers in the numbers array
   // pass the result to cb
   // no return is necessary
+  cb(numbers.reduce((memo, val) => memo += val));
 }
 
 function forEach(arr, cb) {
   // iterate over arr and pass its values to cb one by one
   // hint: you will be invoking cb multiple times (once for each value in the array)
+  arr.forEach(cb);
 }
 
 function map(arr, cb) {
   // create a new array
   // iterate over each value in arr, pass it to cb, then place the value returned from cb into the new arr
   // the new array should be the same length as the array argument
+  return arr.map(cb);
 }
 
 function createUserClass() {
@@ -34,11 +45,24 @@ function createUserClass() {
   // the constructor should have a method 'sayHi' on its prototype that returns the string 'Hello, my name is {{name}}'
   // {{name}} should be the name set on each instance
   // return the constructor
+  class User {
+    constructor({ username, name, email, password }) {
+      this.username = username;
+      this.name = name;
+      this.email = email;
+      this.password = password;
+    }
+    sayHi() {
+      return 'Hello, my name is ' + name;
+    }
+  }
+  return User;
 }
 
 function addPrototypeMethod(Constructor) {
   // add a method to the constructor's prototype
   // the method should be called 'sayHi' and should return the string 'Hello World!'
+  Constructor.prototype.sayHi = () => 'Hello World!';
 }
 
 // !! This is the end of the homework exercises !!
