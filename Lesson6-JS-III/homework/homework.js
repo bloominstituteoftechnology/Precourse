70cebcc5f800e7156fabe8d33218b92ee59553f0
BUDDHAPLEX <ericandrade@gmail.com>
2018-02-28 15:21:31 -0500

    Adding Lesson 6 HW for EA

--- Lesson6-JS-III/homework/homework.js
+++ Lesson6-JS-III/homework/homework.js
@@ -2,58 +2,79 @@
 
 function returnFirst(arr) {
   // return the first item from the array
+  return arr[0];
 }
 
 function returnLast(arr) {
   // return the last item of the array
+  return arr[arr.length -1];
 }
 
 function getArrayLength(arr) {
   // return the length of the array
+  return arr.length;
 }
 
 function incrementByOne(arr) {
-  // arr is an array of integers  
+  // arr is an array of integers
   // increase each integer by one
   // return the array
+  for(let i = 0; i < arr.length; i++) {
+    arr[i] ++;}
+    return arr;
 }
 
 function addItemToArray(arr, item) {
   // add the item to the end of the array
   // return the array
+  arr.push(item);
+  return arr;
 }
 
 function addItemToFront(arr, item) {
   // add the item to the front of the array
   // return the array
   // hint: use the array method .unshift
+  arr.unshift(item);
+  return arr;
 }
 
 function wordsToSentence(words) {
   // words is an array of strings
   // return a string that is all of the words concatenated together
   // spaces need to be between each word
-  // example: ['Hello', 'world!'] -> 'Hello world!'
+  // example: ['Hello', 'world!'] -> 'Hello world
+  return words.join(' ');
 }
 
 function contains(arr, item) {
   // check to see if item is inside of arr
   // return true if it is, otherwise return false
+  return(arr.includes(item));
 }
 
 function addNumbers(numbers) {
   // numbers is an array of integers.
   // add all of the integers and return the value
+  return numbers.reduce((a,b) => a + b, 0);
 }
 
 function averageTestScore(testScores) {
   // testScores is an array.  Iterate over testScores and compute the average.
   // return the average
+  return (testScores.reduce((a,b) => a + b, 0))/testScores.length;
 }
 
 function largestNumber(numbers) {
   // numbers is an array of integers
   // return the largest integer
+  let largeNum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    if (largeNum < numbers[i]) {
+      largeNum = numbers[i];
+    }
+  }
+  return largeNum;
 }
 
 // Do not modify code below this line.
