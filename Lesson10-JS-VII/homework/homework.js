854ed0b13b18c3f4072552effe81661a61453d1a
cgoez <goez2001@gmail.com>
2018-02-27 18:56:44 -0800

    Lessons 8,9,10,11

--- Lesson10-JS-VII/homework/homework.js
+++ Lesson10-JS-VII/homework/homework.js
@@ -5,7 +5,10 @@ function counter() {
   // Example: const newCounter = counter();
   // newCounter(); // 1
   // newCounter(); // 2
+  let count = 0;
+  return () => ++count;
 }
+
 
 function cacheFunction(cb) {
   // use closure to create a cache for the cb function
@@ -18,6 +21,16 @@ function cacheFunction(cb) {
   // if the function you return is invoked with 5 it would pass 5 to cb(5) and return 25
   // if the function you return is invoked again with 5 it will look on an object in the closure scope
   // and return 25 directly and will not invoke cb again
+  let cache = {};
+  return (arg) => {
+    if (!cache.hasOwnProperty(arg)) {
+      cache[arg] = cb(arg);
+      return cache[arg];
+    }
+    else {
+     return cache[arg];
+    }
+  };
 }
 
 // Do not modify code below this line.
