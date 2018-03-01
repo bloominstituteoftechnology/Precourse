a00d4013d957c79d6147def9a2146d2171da1e2c
nickbrenn <nbren@tutanota.com>
2018-03-01 13:16:07 -0500

    Finished homework 11

--- Lesson11-JS-VIII/homework/homework.js
+++ Lesson11-JS-VIII/homework/homework.js
@@ -6,11 +6,22 @@ function nFactorial(n) {
   // return the factorial for n
   // example:
   // the factorial of 3 is 6 (3 * 2 * 1)
+  if(n === 1){
+    return 1;
+  } else{
+    return n*nFactorial(n-1);
+  }
 }
 
 function nFibonacci(n) {
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  let fibmynacc = [1, 1, 2];
+  for(i = 2; fibmynacc.length < n + 1; i + fibmynacc[i++]){
+    i = fibmynacc[fibmynacc.length - 1] + fibmynacc[fibmynacc.length - 2];
+    fibmynacc.push(i);
+  }
+  return fibmynacc[n];
 }
 
 // Do not modify code below this line.
