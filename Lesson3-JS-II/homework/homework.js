58d436a5b917c6a0c3f3a64861751646592e2a3e
Debra <devra29@yahoo.com>
2018-02-28 20:53:34 -0700

    updates to LambdaSchool homework files

--- Lesson3-JS-II/homework/homework.js
+++ Lesson3-JS-II/homework/homework.js
@@ -2,37 +2,73 @@
 
 function getBiggest(x, y) {
   // x and y are integers.  Return the larger integer
+   if (x != y) {
+    return Math.max(x,y);
+   } else {
+    return (x||y);
+  }
   // if they are the same return either one
 }
 
 function greeting(language) {
   // return a greeting for three different languages:
-  // language: 'German' -> 'Guten Tag!'
-  // language: 'English' -> 'Hello!'
-  // language: 'Spanish' -> 'Hola!'
-  // if language is undefined return 'Hello!'
+  // language: 'German' -> 'Guten Tag!
+  if (language ==='German') {
+    return 'Guten Tag!';
+  }
+  if (language ==='Spanish') {
+    return 'Hola!';
+  }else {
+    return 'Hello!';
+  }
 }
 
 function isTenOrFive(num) {
   // return true if num is 10 or 5
+  if (num === 10 || num === 5){
+  return true;
+} else {
+  return false;
   // otherwise return false
+}
 }
 
 function isInRange(num) {
-  // return true if num is less than 50 and greater than 20
+    if (num > 20 && num < 50){
+    return true;
+ }else { 
+  return false;
+}
 }
 
 function isInteger(num) {
-  // return true if num is an integer
+  // return true if num is an integenumr
+   if (Number.isInteger(num)) {
+    return true;
+   }else {
+    return false;
+   }
+ }
   // 0.8 -> false
   // 1 -> true
   // -10 -> true
   // otherwise return false
   // hint: you can solve this using Math.floor
-}
+
 
 function fizzBuzz(num) {
   // if num is divisible by 3 return 'fizz'
+  if (num % 3 === 0 && num % 5 === 0) {
+    return 'fizzbuzz';
+  }else if (num % 3 === 0) {
+    return 'fizz';
+  }
+  else if (num % 5 ===0) {
+    return 'buzz';
+  }else {
+      return num; 
+  }
+
   // if num is divisible by 5 return 'buzz'
   // if num is divisible by 3 & 5 return 'fizzbuzz'
   // otherwise return num
@@ -40,32 +76,47 @@ function fizzBuzz(num) {
 
 function isPrime(num) {
   // return true if num is prime.
-  // otherwise return false
-  // hint: a prime number is only evenly divisible by itself and 1
-  // hint2: you can solve this using a for loop
-  // note: 0 and 1 are NOT considered prime numbers
+if (num < 2){
+  return false;
 }
+for (var i = 2; i < num; i++) {
+  if (num % i === 0) {
+    return false; 
+  } 
+}
+return true;
+}
+  
 
 function returnFirst(arr) {
   // return the first item from the array
+ return(arr[0]);
 }
 
 function returnLast(arr) {
   // return the last item of the array
+  return(arr[arr.length - 1]);
 }
 
 function getArrayLength(arr) {
   // return the length of the array
+  return(arr.length);
 }
 
 function incrementByOne(arr) {
   // arr is an array of integers  
   // increase each integer by one
+  for (var i = 0; i < arr.length; i++){
+    arr[i] = arr[i] + 1;
+  }
   // return the array
+  return arr;
 }
 
 function addItemToArray(arr, item) {
   // add the item to the end of the array
+  arr.push(item);
+    return arr;
   // return the array
 }
 
@@ -73,33 +124,59 @@ function addItemToFront(arr, item) {
   // add the item to the front of the array
   // return the array
   // hint: use the array method .unshift
+  arr.unshift(item);
+  return(arr);
+// expected output: Array [item, 1, 2, 3]
 }
 
 function wordsToSentence(words) {
   // words is an array of strings
+  
   // return a string that is all of the words concatenated together
+  return(words.join(' '));
   // spaces need to be between each word
   // example: ['Hello', 'world!'] -> 'Hello world!'
 }
 
 function contains(arr, item) {
   // check to see if item is inside of arr
-  // return true if it is, otherwise return false
+
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === item) {
+            return true;
+        }
+    }
+
+    return false;
 }
+  // return true if it is, otherwise return false
+
 
 function addNumbers(numbers) {
   // numbers is an array of integers.
   // add all of the integers and return the value
+  let total = 0;
+  for (var i = 0; i < numbers.length; i++){
+     total += numbers[i];
+     }   
+      return total;
 }
 
 function averageTestScore(testScores) {
   // testScores is an array.  Iterate over testScores and compute the average.
   // return the average
+  let total = 0;
+  for (var i = 0; i < testScores.length; i++){
+   total += testScores[i]/testScores.length;
+  }
+  return Math.round(total);
 }
+
 
 function largestNumber(numbers) {
   // numbers is an array of integers
   // return the largest integer
+  return Math.max.apply(null, numbers);
 }
 
 // Do not modify code below this line.
--- Lesson4-JS-III/homework/homework.js
+++ Lesson4-JS-III/homework/homework.js
@@ -1,20 +1,26 @@
 // Do not change any of the function names
 
 function makeCat(name, age) {
-  // create a new object with a name property with the value set to the name argument
-  // add an age property to the object with the value set to the age argument
-  // add a method called meow that returns the string 'Meow!'
-  // return the object
+  return {
+    name: name,
+    age: age,
+    meow: function() {
+      return 'Meow!';
+    },
+  };
 }
 
 function addProperty(object, property) {
   // add the property to the object with a value of null
+  object[property] = null;
   // return the object
+  return object;
   // note: the property name is NOT 'property'.  The name is the value of the argument called property (a string)
 }
 
 function invokeMethod(object, method) {
   // method is a string that contains the name of a method on the object
+  object[method]();
   // invoke this method
   // nothing needs to be returned
 }
@@ -22,49 +28,75 @@ function invokeMethod(object, method) {
 function multiplyMysteryNumberByFive(mysteryNumberObject) {
   // mysteryNumberObject has a property called mysteryNumber
   // multiply the mysteryNumber property by 5 and return the product
+  return mysteryNumberObject.mysteryNumber * 5;
 }
 
 function deleteProperty(object, property) {
   // remove the property from the object
+  delete object[property];
   // return the object
+  return object;
 }
 
 function newUser(name, email, password) {
   // create a new object with properties matching the arguments passed in.
+  let user = {};
+  user.name = name;
+  user.email = email;
+  user.password = password;
   // return the new object
+  return user;
 }
 
 function hasEmail(user) {
+  if (user.email) {
+    return true;
+  } else return false;
   // return true if the user has a value for the property 'email'
   // otherwise return false
+  
 }
 
 function hasProperty(object, property) {
   // return true if the object has the value of the property argument
+  if (object.hasOwnProperty(property)) {
+    return true;
+  }else return false;
   // property is a string
   // otherwise return false
 }
 
 function verifyPassword(user, password) {
   // check to see if the provided password matches the password property on the user object
+  if (password === user.password) {
+    return true;
+  }else return false;
   // return true if they match
   // otherwise return false
 }
 
 function updatePassword(user, newPassword) {
   // replace the existing password on the user object with the value of newPassword
+  user.password = newPassword;
   // return the object
-}
+  return user;
+} 
 
 function addFriend(user, newFriend) {
   // user has a property called friends that is an array
+  user.friends.push(newFriend);
   // add newFriend to the end of the friends array
   // return the user object
+  return user;
 }
 
 function setUsersToPremium(users) {
   // users is an array of user objects.
   // each user object has the property 'isPremium'
+  for (var i = 0; i < users.length; i++) 
+    users[i].isPremium = true;
+    return users;
+  
   // set each user's isPremium property to true
   // return the users array
 }
@@ -75,16 +107,28 @@ function sumUserPostLikes(user) {
   // each post object has an integer property called 'likes'
   // sum together the likes from all the post objects
   // return the sum
+  let total = 0;
+  for (var i = 0; i < user.posts.length; i++) {
+    total += user.posts[i].likes;
+    
+}
+  return total;
 }
 
 function addCalculateDiscountPriceMethod(storeItem) {
   // add a method to the storeItem object called 'calculateDiscountPrice'
+  storeItem.calculateDiscountPrice = function() {
+     let discount = this.price - (this.price * this.discountPercentage);
+     return discount;
+  };
+    return storeItem;
   // this method should multiply the storeItem's 'price' and 'discountPercentage' to get the discount
   // the method then subtracts the discount from the price and returns the discounted price
   // example: 
   // price -> 20
   // discountPercentage -> .2
   // discountPrice = 20 - (20 * .2)
+
 }
 
 // Do not modify code below this line.
