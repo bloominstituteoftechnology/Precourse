6802f4512499a6b088c25a32d3d2ee1d3ad23c71
SyCoDe <sycode@gmail.com>
2018-03-02 10:35:32 -0700

    latest updates to all files

---
+++ Lesson9-JS-VI (1)/homework/homework.js
@@ -0,0 +1,43 @@
+// Do not change any of the function names
+
+function invokeCallback(cb) {
+  // invoke cb
+  cb();
+}
+
+function sumArray(numbers, cb) {
+  // sum up all of the integers in the numbers array
+  // pass the result to cb
+  // no return is necessary
+  cb(numbers.reduce((acc,cur) => acc + cur));
+}
+
+function forEach(arr, cb) {
+  // iterate over arr and pass its values to cb one by one
+  // hint: you will be invoking cb multiple times (once for each value in the array)
+  for (let i = 0; i < arr.length; i++){
+    cb(arr[i]);
+  }
+}
+
+function map(arr, cb) {
+  // create a new array
+  // iterate over each value in arr, pass it to cb, then place the value returned from cb into the new arr
+  // the new array should be the same length as the array argument
+  
+  var newArr = [];
+  for (let i=0; i<arr.length; i++){
+    newArr.push(cb(arr[i]));
+  }
+  return newArr; 
+}
+
+// Do not modify code below this line.
+// --------------------------------
+
+module.exports = {
+  invokeCallback,
+  sumArray,
+  forEach,
+  map,
+};
