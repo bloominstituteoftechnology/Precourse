d0341f48f1bbc41ab0baca2b000b2ba45144cef4
Cruise Brantley <cruise.brantley@gmail.com>
2018-03-03 20:57:35 -0600

    Finished Lesson11 for practice

--- Lesson11-JS-VIII/homework/homework.js
+++ Lesson11-JS-VIII/homework/homework.js
@@ -6,11 +6,32 @@ function nFactorial(n) {
   // return the factorial for n
   // example:
   // the factorial of 3 is 6 (3 * 2 * 1)
+  if(n > 1)
+    n = n * nFactorial(n-1);
+  return n;
 }
 
 function nFibonacci(n) {
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  let num1 = 1;
+  let num2 = 2;
+  let sum = 3;
+
+  if(n == 1)
+    return 1;
+  if(n == 2)
+    return 2;
+
+  function fibFunction(n){
+    sum = num1 + num2;
+    num1 = num2;
+    num2 = sum;
+    if(n > 3)
+      fibFunction(n-1);
+    return sum;
+  }
+  return fibFunction(n);
 }
 
 // Do not modify code below this line.
