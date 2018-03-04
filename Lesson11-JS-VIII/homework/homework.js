9a856696833cd7dea4b846f58a469bff5d1f7cd5
jaklundun <govi.escudero@gmail.com>
2018-03-03 20:05:52 -0800

    Completed Lesson 11 exercise 1 of 2.

--- Lesson11-JS-VIII/homework/homework.js
+++ Lesson11-JS-VIII/homework/homework.js
@@ -6,7 +6,16 @@ function nFactorial(n) {
   // return the factorial for n
   // example:
   // the factorial of 3 is 6 (3 * 2 * 1)
+ if (n === 0 ) {
+    return 1;
+  } else {
+    console.log(n);
+    return n * nFactorial(n - 1);
+    // 3 * 2 * 1
+  }
 }
+
+
 
 function nFibonacci(n) {
   // fibonacci sequence: 1 2 3 5 8 13 ...
