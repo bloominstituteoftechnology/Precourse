98f2c2cfbd45eec9fa7c71db8ec956f7b818d87e
Camilo Gutierrez <2017cg@gmail.com>
2018-03-04 22:17:55 -0500

    Lesson 6: Arrays Homework

--- Lesson6-JS-III/homework/README.md
+++ Lesson6-JS-III/homework/README.md
@@ -4,7 +4,7 @@
 ---
 1. Feynman Writing Prompts - Write out explanations of the following concepts like you are explaining it to a 12 year old.  Doing this will help you quickly discover any holes in your understanding.  Ask your questions on Slack.
 		
-	* Arrays
+	* Arrays - It allows you to store a vast amount of information in a single container. For example, think of a school and teachers. The school is a single container that holds all the teachers in one place. So you can pull any teacher out of school, instead of having multiple placeholders (variables) for all the teachers. So Arrays allows us to extract more information from a single container. 
 
 2. 3. From the top level of your `Precourse` folder, run `npm test JSIII.test.js` to run the automated tests. You will fill out the functions in `homework.js` to make the tests pass.
 
--- Lesson6-JS-III/homework/homework.js
+++ Lesson6-JS-III/homework/homework.js
@@ -2,31 +2,39 @@
 
 function returnFirst(arr) {
   // return the first item from the array
+ return arr[0]; 
 }
 
 function returnLast(arr) {
   // return the last item of the array
+  return arr[arr.length -1]; 
 }
 
 function getArrayLength(arr) {
   // return the length of the array
+ return arr.length; 
 }
 
 function incrementByOne(arr) {
   // arr is an array of integers  
   // increase each integer by one
   // return the array
+return arr.map((num) => {return num +1;}); 
 }
 
 function addItemToArray(arr, item) {
   // add the item to the end of the array
   // return the array
+  arr.push(item); 
+  return arr; 
 }
 
 function addItemToFront(arr, item) {
   // add the item to the front of the array
   // return the array
   // hint: use the array method .unshift
+  arr.unshift(item); 
+  return arr; 
 }
 
 function wordsToSentence(words) {
@@ -34,26 +42,51 @@ function wordsToSentence(words) {
   // return a string that is all of the words concatenated together
   // spaces need to be between each word
   // example: ['Hello', 'world!'] -> 'Hello world!'
+  let concatenated = ''; 
+  for (let i = 0; i < words.length; i++){
+    concatenated += words[i] + ' ' ; 
+  }
+  concatenated = concatenated.slice (0, -1);
+  return concatenated;
 }
 
 function contains(arr, item) {
   // check to see if item is inside of arr
   // return true if it is, otherwise return false
+if (arr.includes(item)) {
+  return true; 
+} else {
+  return false; 
+}
 }
 
 function addNumbers(numbers) {
   // numbers is an array of integers.
   // add all of the integers and return the value
+const sum = numbers.reduce((total, amount) => total + amount); 
+return sum; 
+
 }
 
 function averageTestScore(testScores) {
   // testScores is an array.  Iterate over testScores and compute the average.
   // return the average
+  let avg = 0; 
+  for (let i = 0; i <testScores.length; i++){
+    avg += testScores[i];
+  } avg /= testScores.length; 
+return avg; 
 }
 
 function largestNumber(numbers) {
   // numbers is an array of integers
   // return the largest integer
+let largest = 0; 
+for (let i = 0; i < numbers.length; i++){
+  if (largest < numbers[i]){
+    largest = numbers[i];
+  }
+} return largest; 
 }
 
 // Do not modify code below this line.
