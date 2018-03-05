121499787bb9ae3f7ae02282e6fe3b08a8e4e5f6
odax <nr10@albion.edu>
2018-03-04 22:20:00 -0500

    worked on lesson 11 and 12, could not get them to pass

--- Lesson11-JS-VIII/homework/homework.js
+++ Lesson11-JS-VIII/homework/homework.js
@@ -3,16 +3,28 @@
 // solve these recursively
 
 function nFactorial(n) {
-  // return the factorial for n
-  // example:
-  // the factorial of 3 is 6 (3 * 2 * 1)
+  if (n != 1) {
+    return n * nFactorial(n-1); 
+  }
+  else return 1;
 }
 
-function nFibonacci(n) {
+var cache = {};
+function nFibonacci(n) { 
+  if (n <= 1) {
+    return 1;
+    }
+  }
+  if (n in cache) {
+    return cache[n];
+  }
+  var value = nFibonacci(n - 1) + nFibonacci(n - 2);
+  cache[n] = value;
+  return value;
+}
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
-}
-
+ 
 // Do not modify code below this line.
 // --------------------------------
 
