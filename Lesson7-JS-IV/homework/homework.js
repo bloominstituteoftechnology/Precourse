2c7cf2a261c1eafa44a695fcdb005a9be2b34631
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-03-07 00:09:45 +0800

    Modified a file in the Lesson7-JS-IV/homework, homework.js

--- Lesson7-JS-IV/homework/homework.js
+++ Lesson7-JS-IV/homework/homework.js
@@ -5,13 +5,23 @@ function makeCat(name, age) {
   // add an age property to the object with the value set to the age argument
   // add a method called meow that returns the string 'Meow!'
   // return the object
-}
+  const newCat = {
+    name,
+    age,
+    meow: function() {
+      return('Meow!');
+    },
+  };
+  return newCat;
+ }  
 
 function addProperty(object, property) {
   // add the property to the object with a value of null
   // return the object
   // note: the property name is NOT 'property'.  The name is the value of the argument called property (a string)
-}
+  object[property] = null;
+  return object;
+ }
 
 function invokeMethod(object, method) {
   // method is a string that contains the name of a method on the object
