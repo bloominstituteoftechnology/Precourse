dc8f3857a8504ad9cc46bc1d4ad70e65c5b08ce6
shannonhealy <sahealy525@gmail.com>
2018-03-06 21:39:14 -0500

    Lesson 5 Homework

--- Lesson5-JS-II/homework/README.md
+++ Lesson5-JS-II/homework/README.md
@@ -7,6 +7,14 @@
 	* `for`
 	* `&&`, `||`, `!`
 
+	A for is a type of looping code. It basically runs "for as long as" the conditions you give it are being met.
+	
+	&& is the and operator. You can give an if statement multiple conditions to meet, but if you list them with && such as (this && this), both this and this need to be true.
+
+	|| is the or operator. Similar in functioning to the and operator in that you can use it to incorporate different conditions for the if statement to look to meet, but in this case either statement can return true for the code to run. For example (this || that) needs to be true, they dont both need to be met, just one.
+
+	! is the not operator. This one is a bit different from && and ||. Not basically flips around the boolean value that the condition will return. For example (!true) will return false because you are saying its (not true). As an expansion of that example if you write (!(10 === 10)) it will return false, because even though 10 normally equals 10, you are flipping that truth with the ! operator
+
 2. From the top level of your `Precourse` folder, run `npm test JSII.test.js` to run the automated tests. You will fill out the functions in `homework.js` to make the tests pass.
 
 For more information about Lambda School's six month CS program visit: https://lambdaschool.com
--- Lesson5-JS-II/homework/homework.js
+++ Lesson5-JS-II/homework/homework.js
@@ -4,11 +4,31 @@ function multiplyArguments() {
   // use the arguments keyword to multiply all of the arguments together and return the product
   // if no arguments are passed in return 0
   // if one argument is passed in just return it
+ if(arguments.length === 0){
+   return 0;
+ } else if(arguments.length === 1) {
+   return arguments[0];
+ } else {
+   var num = 1;
+   for(var count = 0; count < arguments.length; count++) {
+    num = num * arguments[count];
+   }
+   return num;
+  }
 }
 
 function getBiggest(x, y) {
   // x and y are integers.  Return the larger integer
   // if they are the same return either one
+  if(x === y) {
+    return x;
+  }
+  else if (x > y) {
+    return x;
+  }
+  else {
+    return y;
+  }
 }
 
 function greeting(language) {
@@ -17,16 +37,44 @@ function greeting(language) {
   // language: 'Mandarin' -> 'Ni Hao!'
   // language: 'Spanish' -> 'Hola!'
   // if language is undefined return 'Hello!'
+  if(language === 'German') {
+    var greeting = 'Guten Tag!';
+    return greeting;
+  }
+  else if(language === 'Mandarin') {
+    greeting = 'Ni Hao!';
+    return greeting;
+  }
+  else if(language === 'Spanish') {
+    greeting = 'Hola!';
+    return greeting;
+  }
+  else {
+    greeting = 'Hello!';
+    return greeting;
+  }
 }
 
 function isTenOrFive(num) {
   // return true if num is 10 or 5
   // otherwise return false
+  if (num === 10 || num === 5) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 function isInRange(num) {
   // return true if num is less than 50 and greater than 20
   // otherwise return false
+  if (num < 50 && num > 20) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 function isInteger(num) {
@@ -36,6 +84,13 @@ function isInteger(num) {
   // -10 -> true
   // otherwise return false
   // hint: you can solve this using Math.floor
+  var checkNumber = Math.floor(num);
+  if (num === checkNumber) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 function fizzBuzz(num) {
@@ -43,6 +98,22 @@ function fizzBuzz(num) {
   // if num is divisible by 5 return 'buzz'
   // if num is divisible by 3 & 5 return 'fizzbuzz'
   // otherwise return num
+  var message = ' ';
+  if (num % 3 === 0 && num % 5 === 0) {
+    message = 'fizzbuzz';
+    return message;
+  }
+  else if (num % 3 === 0) {
+    message = 'fizz';
+    return message;
+  }
+  else if (num % 5 === 0) {
+    message = 'buzz';
+    return message;
+  }
+  else {
+    return num;
+  }
 }
 
 function isPrime(num) {
@@ -51,8 +122,18 @@ function isPrime(num) {
   // hint: a prime number is only evenly divisible by itself and 1
   // hint2: you can solve this using a for loop
   // note: 0 and 1 are NOT considered prime numbers
+  if(num === 0 || num === 1) {
+    return false;
+  }
+  else {
+    for(var counter = 2; counter < num; counter++){
+      if(num % counter === 0){
+        return false;
+      }
+    }
+    return true;
+  }
 }
-
 
 
 // Do not modify code below this line.
