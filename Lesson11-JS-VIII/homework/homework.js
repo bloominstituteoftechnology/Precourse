9df9941aed6cf51237deb1c32651afc1aca21480
Anthony Fiedler <afiedler@ntdomain.associates>
2018-03-08 12:54:51 -0800

    Lesson 11 tests passed JS-VIII and Writing prompt at top of doc

--- Lesson11-JS-VIII/homework/homework.js
+++ Lesson11-JS-VIII/homework/homework.js
@@ -2,15 +2,26 @@
 
 // solve these recursively
 
+/*
+Recursion: Iteration using functions to call themselves from within the function, resulting in a loop.
+Similar to 'for' or 'while'. We always need a way to exit the loop, though.
+*/
+
 function nFactorial(n) {
   // return the factorial for n
   // example:
   // the factorial of 3 is 6 (3 * 2 * 1)
+  if(n === 0){
+    return 1;
+  }
+  return n * nFactorial(n-1);
 }
 
 function nFibonacci(n) {
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  if(n <= 1){return 1;}
+  return nFibonacci(n-1) + nFibonacci(n-2);
 }
 
 // Do not modify code below this line.
