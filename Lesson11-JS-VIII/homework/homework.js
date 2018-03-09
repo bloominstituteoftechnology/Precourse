c04d7929f79cddbbfafd0e672bcd6161d6e96ff9
april <martinezapril56@yahoo.com>
2018-03-09 08:50:51 -0500

    commit

--- Lesson11-JS-VIII/homework/homework.js
+++ Lesson11-JS-VIII/homework/homework.js
@@ -22,16 +22,12 @@ console.log(factorial(num));
 function nFibonacci(n) {
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
-  var array = [0, 1];
-
-  for (var i = 2; i <= n; i++) {
-    array.push(array[i - 1] + array[i - 2]);
-
-  }
-  return array[n];
+  return n < 1 ? 0
+        : n <= 2 ? 1
+        : fibonacci(n - 1) + fibonacci(n - 2);
 }
-var n = 9;
-var ans = fibonacci(n);
+
+console.log(fibonacci(4))
 
 // Do not modify code below this line.
 // --------------------------------
