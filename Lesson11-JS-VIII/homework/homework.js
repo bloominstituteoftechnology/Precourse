55aa7f13d0a0489486427fa944676cff25c7bb39
april <martinezapril56@yahoo.com>
2018-03-09 08:04:15 -0500

    commit

--- Lesson11-JS-VIII/homework/homework.js
+++ Lesson11-JS-VIII/homework/homework.js
@@ -7,11 +7,13 @@ function nFactorial(n) {
   // example:
   // the factorial of 3 is 6 (3 * 2 * 1)
  
-    if (n==0 || n==1){
-      return 1;
-    }
-    return factorial(n-1)*n;
-  } 
+  if (n === 0) {
+    return 1;
+  }
+  
+  
+  return n * factorial(n - 1);
+}
  
 
 
