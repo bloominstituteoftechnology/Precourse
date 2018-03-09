61864137818fc038cae88c0792ff1cbb6f5f1193
udykas <udykas@gmail.com>
2018-03-09 16:36:03 -0500

    Lesson 6 Complete

--- Lesson6-JS-III/homework/homework.js
+++ Lesson6-JS-III/homework/homework.js
@@ -1,35 +1,55 @@
 // Do not change any of the function names
 
 function returnFirst(arr) {
+  return arr[0];
   // return the first item from the array
 }
 
 function returnLast(arr) {
+  return arr[arr.length - 1];
   // return the last item of the array
 }
 
 function getArrayLength(arr) {
+  return arr.length;
   // return the length of the array
 }
 
 function incrementByOne(arr) {
-  // arr is an array of integers  
+  for (let i = 0; i < arr.length; i++){
+    arr[i] = arr[i] + 1;
+  }
+
+  return arr;
+  // arr is an array of integers
   // increase each integer by one
   // return the array
 }
 
 function addItemToArray(arr, item) {
+  arr.push(item);
+  return arr;
   // add the item to the end of the array
   // return the array
 }
 
 function addItemToFront(arr, item) {
+  arr.unshift(item);
+  return arr;
   // add the item to the front of the array
   // return the array
   // hint: use the array method .unshift
 }
 
 function wordsToSentence(words) {
+  let sentence = words[0];
+
+  for (let i = 1; i< words.length; i++) {
+    sentence = sentence + ' ' + words[i];
+  }
+
+  return sentence;
+
   // words is an array of strings
   // return a string that is all of the words concatenated together
   // spaces need to be between each word
@@ -37,21 +57,49 @@ function wordsToSentence(words) {
 }
 
 function contains(arr, item) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === item) {
+      return true;
+    }
+
+    return false;
+  }
   // check to see if item is inside of arr
   // return true if it is, otherwise return false
 }
 
 function addNumbers(numbers) {
+  let sum = 0;
+
+  for (let i = 0; i < numbers.length; i++) {
+    sum = sum + numbers[i];
+  }
+
+  return sum;
   // numbers is an array of integers.
   // add all of the integers and return the value
 }
 
 function averageTestScore(testScores) {
+  let sum = 0;
+
+  for (let i = 0; i < testScores.length; i++) {
+    sum = sum + testScores[i];
+  }
+
+  let average = sum / testScores.length;
+
+  return average;
   // testScores is an array.  Iterate over testScores and compute the average.
   // return the average
 }
 
 function largestNumber(numbers) {
+  for (let i =0; i<numbers.length; i++){
+    if (numbers[i] > numbers[i - 1]){
+      return numbers[i];
+    }
+  }
   // numbers is an array of integers
   // return the largest integer
 }
