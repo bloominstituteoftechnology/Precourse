27b163dda285f00a10bbfaa1d1c6f0afeb05d14a
april <martinezapril56@yahoo.com>
2018-03-08 21:00:45 -0500

    commit

--- Lesson7-JS-IV/homework/homework.js
+++ Lesson7-JS-IV/homework/homework.js
@@ -102,7 +102,7 @@ function addFriend(user, newFriend) {
   // user has a property called friends that is an array
   // add newFriend to the end of the friends array
   // return the user object
-  friend.push(newFriend);
+  friend.push('newFriend');
   return friend;
 }
 
