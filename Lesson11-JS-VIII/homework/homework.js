fb9f146befd204e5c537aec0bb06a1e5aae98a85
april <martinezapril56@yahoo.com>
2018-03-09 08:19:48 -0500

    commit

--- Lesson11-JS-VIII/homework/homework.js
+++ Lesson11-JS-VIII/homework/homework.js
@@ -7,32 +7,38 @@ function nFactorial(n) {
   // example:
   // the factorial of 3 is 6 (3 * 2 * 1)
  
-  if (n === 0) {
-    return 1;
-  }
-  
-  
-  return n * factorial(n - 1);
-}
+  if ( n === 1 ){ return 1; } 
+
+
+
+  return n * nFactorial( n - 1 );
+
+  } 
  
 
 
 function nFibonacci(n) {
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
-  function fibonacci(n) {
-    var array = [0, 1];
+  var array = [0, 1];
 
-    for (var i = 2; i <= n; i++) {
-        array.push(array[i - 1] + array[i - 2]);
-    }
 
-    return array[n];
+
+  for (var i = 2; i <= n; i++) {
+
+    array.push(array[i - 1] + array[i - 2]);
+
+  }
+
+
+
+  return array[n];
+
 }
+
 var n = 9;
-var ans = fibonacci(n);
-}
 
+var ans = fibonacci(n);
 
 // Do not modify code below this line.
 // --------------------------------
