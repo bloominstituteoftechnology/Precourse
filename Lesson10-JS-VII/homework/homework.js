28efdf9415eb773064ebf69a84217733c9ec87bd
Kamry Bowman <kamry.bowman@gmail.com>
2018-03-10 12:16:57 -0700

    Completed Lesson 10 homework and writing prompts.

--- Lesson10-JS-VII/homework/homework.js
+++ Lesson10-JS-VII/homework/homework.js
@@ -5,6 +5,11 @@ function counter() {
   // Example: const newCounter = counter();
   // newCounter(); // 1
   // newCounter(); // 2
+  let count = 0;
+  return function() {
+    count += 1;
+    return count;
+  };
 }
 
 function cacheFunction(cb) {
@@ -18,7 +23,43 @@ function cacheFunction(cb) {
   // if the function you return is invoked with 5 it would pass 5 to cb(5) and return 25
   // if the function you return is invoked again with 5 it will look on an object in the closure scope
   // and return 25 directly and will not invoke cb again
+  
+    //function to check if an argument has already been entered as an argument to callback function, and returns output if so, otherwise returns false
+    function checkCache (historicArr, test) {
+      let result = false;
+      historicArr.forEach(function(recordArr) {
+        if (recordArr[0] === test) {
+          result = recordArr[1];
+        }
+      });
+      return result;
+    }
+
+  //cache holds an array of prior calls of the cb function, which holds arrays of the argument and return output for each call respectively 
+  let cache = [];
+  
+  //function to be returned, that will contain cache closure
+  let cacherFunc = function (callbackInput) {
+    
+    //uses checkCache to search cache array for the cb's input, saves the result
+    let cacheSearchResult = checkCache(cache, callbackInput);
+
+    //if cache search found a prior cb call with the same argument, returns that cb output
+    if (cacheSearchResult !== false) {
+      return cacheSearchResult;
+    }
+
+    //If no prior matching cache search, calls callback function with input, and then adds the input and output to cache before returning the output
+    let callbackOutput = cb (callbackInput);
+
+    cache.push([callbackInput, callbackOutput]);
+    return callbackOutput;
+  };
+  return cacherFunc;
 }
+
+
+
 
 // Do not modify code below this line.
 // --------------------------------
