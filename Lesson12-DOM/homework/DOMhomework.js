63c9d697eda7fe952f1d69e6b6d415fd26474401
april <martinezapril56@yahoo.com>
2018-03-10 10:44:13 -0500

    commit

--- Lesson12-DOM/homework/DOMhomework.js
+++ Lesson12-DOM/homework/DOMhomework.js
@@ -20,7 +20,7 @@ spanNew.innerHTML += 'April Martinez';
           'complete' which should be set to false. Hint: use the 'this' keyword in the constructor function.
 */
 
-class ToDo () {
+function ToDo () {
   // code here
   function ToDo(description) {
     this.description = description;
