c94fa520725e0243a3990378c65bb74d59071e76
april <martinezapril56@yahoo.com>
2018-03-09 21:50:35 -0500

    commit

--- Lesson11-JS-VIII/homework/homework.js
+++ Lesson11-JS-VIII/homework/homework.js
@@ -24,20 +24,10 @@ function nFactorial(n) {
 function nFibonacci(n) {
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
-  if (n == 0 || n == 1)
+  if (n == 1 || n == 0)
   {
-      return 1;
+      return n * nFractorial(n-1);
   }
-   a = 0;
-   b = 1;
-
-  for ( i = 0; i < n; i++)
-  {
-      n = b + a;
-      a = b;
-      b = n;
-  }
-  return c;
 }
 // Do not modify code below this line.
 // --------------------------------
