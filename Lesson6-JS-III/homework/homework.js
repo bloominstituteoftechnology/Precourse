125b242d078d7f1a5af8d3283d80369211d493a9
Sharde DeLane <rhcprocker@yahoo.com>
2018-03-10 15:20:52 -0600

    homework.js

--- Lesson6-JS-III/homework/homework.js
+++ Lesson6-JS-III/homework/homework.js
@@ -2,20 +2,16 @@
 
 function returnFirst(arr) {
   // return the first item from the array
-  arr = ['Bob Ross', 'Jon Snow', 'Happy Trees'];
   return (arr[0]);
 }
 
 function returnLast(arr) {
-
-  arr = ['Bob Ross', 'Jon Snow', 'Happy Trees'];
   // return the last item of the array
- return (arr[3]);
+  return (arr[arr.length-1]);
 }
 
 function getArrayLength(arr) {
   // return the length of the array
-  arr = ['Bob Ross', 'Jon Snow', 'Happy Trees'];
   return (arr.length);
 }
 
@@ -23,25 +19,28 @@ function incrementByOne(arr) {
   // arr is an array of integers  
   // increase each integer by one
   // return the array
-  for(let i=0;i <arr;i++){
+  for(var i = 0; i < arr.length; i++){
+      arr[i]=arr[i]+1;
+  }
+    return arr;
+}
 
-  return (arr[i]);
-}
-}
+
+
 
 function addItemToArray(arr, item) {
   // add the item to the end of the array
   // return the array
-  addItemToArray.push(item);
-  return (addItemToArray);
+    arr.push(item);
+    return arr;
 }
 
 function addItemToFront(arr, item) {
   // add the item to the front of the array
   // return the array
   // hint: use the array method .unshift
-  addItemToFront.unshift(item);
-  return (addItemToFront);
+  arr.unshift(item);
+  return (arr);
 }
 
 function wordsToSentence(words) {
@@ -49,81 +48,84 @@ function wordsToSentence(words) {
   // return a string that is all of the words concatenated together
   // spaces need to be between each word
   // example: ['Hello', 'world!'] -> 'Hello world!'
-  words=['Hello','world!'];
-  return (words);
-  
+  var result = ' ';
+  for (var i = 0; i < words.length; i++){
+      result += words[i] + ' ';
+  }
+      return result.trim();
 
 }
 
 function contains(arr, item) {
   // check to see if item is inside of arr
   // return true if it is, otherwise return false
-  var isThisReal=item.length;
-
-  for(let i=0; i<isThisReal; i++)
-    if(item[i]===item){
-    return (true);
+    if(arr.includes(item)){
+       return true;
     }
-      else {
-       return (false);
+       else{
+         return false;
       }
 }
 
 function addNumbers(numbers) {
   // numbers is an array of integers.
   // add all of the integers and return the value
-  numbers =[1,2,3,4,5,6,7,8,9];
-  var sum =0;
-  for(let i=0; i < numbers.length;i++){
+  let sum = 0;
 
-    sum = sum + numbers.length[i];
-
-    return (sum);
+  for (let i = 0; i < numbers.length; i++) {
+      sum = sum + numbers[i];
   }
+
+      return sum;
 }
+
 
 
 function averageTestScore(testScores) {
   // testScores is an array.  Iterate over testScores and compute the average.
   // return the average
-
-
+       var total = 0;
+        for (let i = 0; i < testScores.length; i++){
+      
+                total = total + testScores[i];
+        }
+          return total/testScores.length;
 }
 
 function largestNumber(numbers) {
   // numbers is an array of integers
   // return the largest integer
-  if(numbers.length ===0){
-    return -1;
+  var largestNum = 0;
+  for(let i = 0; i < numbers.length; i++){
+      if(largestNum < numbers[i]){
+        largestNum = numbers[i];
+      }
   }
-  var max= numbers[0];
-  var maxNumber=0;
-  for(let i=1; i<numbers.length;i++){
-    if(numbers[i]>max){
-      maxNumber= i;
-      max= numbers[i];
-      return (maxNumber);
-    }
-  }
+        return largestNum;
 }
 
-function multiplyArguments(someRandomArray) {
+function multiplyArguments() {
   // use the arguments keyword to multiply all of the arguments together and return the product
   // if no arguments are passed in return 0
   // if one argument is passed in just return it
-  someRandomArray=[1,3,5,6];
-  var sum=1;
-  if(someRandomArray===null)
-  return 0;
-
-  else if(someRandomArray < 0 || someRandomArray > 0){
-      for(let i=0;i<someRandomArray.length;i++){
-      sum=sum+someRandomArray[i];
-     return (multiplyArguments(someRandomArray));
+    var product = 1;
+  
+    if (arguments.length === 0) {
+      
+         return 0;
+    } 
+        else if (arguments.length === 1) {
+    
+        return arguments[0];
       }
-  }
-
-}
+  
+        for (var i = 0; i < arguments.length; i++) {
+          
+          product *= arguments[i];
+        }
+            
+            return product;
+}	 
 
 // Do not modify code below this line.
 // --------------------------------
