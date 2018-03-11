0ee1d16bace6c44551db1b6b821c2982abb76b19
april <martinezapril56@yahoo.com>
2018-03-11 01:50:28 -0500

    commit

--- Lesson12-DOM/homework/DOMhomework.js
+++ Lesson12-DOM/homework/DOMhomework.js
@@ -102,7 +102,7 @@ function displayToDos() {
   // code here
   const toDoContainer = document.getElementById('#toDoContainer');
   toDoContainer.innerHTML = '';
- function buildToDos(toDos);
+ function buildToDos(toDos)
   for(let i = 0; i < hold.length; i++) {
   toDoContainer.appendChild(hold[i]);
 
