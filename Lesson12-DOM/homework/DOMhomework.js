20893e82c253baa4f2b697d7c617afbefb109e16
april <martinezapril56@yahoo.com>
2018-03-11 15:20:36 -0400

    commit

--- Lesson12-DOM/homework/DOMhomework.js
+++ Lesson12-DOM/homework/DOMhomework.js
@@ -80,7 +80,7 @@ function buildToDo(toDo, index) {
 
 function buildToDotoDo() {
   // code here
-  document.getElementById('#toDo').onclick = toDo;
+  document.getElementById('#toDo').onclick = 'toDo';
   return toDo.map(buildToDo);
 
 } 
