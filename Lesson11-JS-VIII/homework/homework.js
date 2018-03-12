926a96c3c1d2fe38f4330a52cc5a6406c491589f
april <martinezapril56@yahoo.com>
2018-03-12 13:39:28 -0400

    mom

--- Lesson11-JS-VIII/homework/homework.js
+++ Lesson11-JS-VIII/homework/homework.js
@@ -25,14 +25,17 @@ function nFibonacci(n) {
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
 
-  var fib = function(n){ 
-    var x = 0; var y = 1; if(n<=2){
-       return n-1; } for(var i = 0; i < n; i++){ 
-         var tempY = y; y = tempY + x; x = tempY; } 
-         return y;
+  if(n == 0) {
+
+    return 1;
+
+  } else { 
+
+    return n * nFactorial(n - 1);
+
+  }
+
 }
-  
-}  
   
    
 // Do not modify code below this line.
