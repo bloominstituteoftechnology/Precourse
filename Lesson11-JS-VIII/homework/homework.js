b88a55e677bd090bb3882ec9ef3fa4a7d1c8fba4
april <martinezapril56@yahoo.com>
2018-03-12 13:11:39 -0400

    commit

--- Lesson11-JS-VIII/homework/homework.js
+++ Lesson11-JS-VIII/homework/homework.js
@@ -8,17 +8,14 @@ function nFactorial(n) {
   // the factorial of 3 is 6 (3 * 2 * 1)
  //recursive
 
- function fibonacci(n) {
-  var array = [0, 1];
+ if (n == 0 || n == 1)
+    return 1;
+  if (f[n] > 0)
+    return f[n];
+  return f[n] = factorial(n-1) * n;
+} 
 
-  for (var i = 2; i <= n; i++) {
-      array.push(array[i - 1] + array[i - 2]);
-  }
-
-  return array[n];
-}
-
-}   
+  
 
     
 
@@ -26,11 +23,17 @@ function nFactorial(n) {
 function nFibonacci(n) {
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
-  if (n == 0 || n == 1)
-  {
-      return n * nFractorial(n-1);
+  function fibonacci(n) {
+    var array = [0, 1];
+  
+    for (var i = 2; i <= n; i++) {
+        array.push(array[i - 1] + array[i - 2]);
+    }
+  
+    return array[n];
   }
-}
+  
+  }   
 // Do not modify code below this line.
 // --------------------------------
 
