0ab659653bb8df278f377539f8a3d755612c2c73
april <martinezapril56@yahoo.com>
2018-03-12 18:17:25 -0400

    commit

--- Lesson12-DOM/homework/DOMhomework.js
+++ Lesson12-DOM/homework/DOMhomework.js
@@ -62,7 +62,7 @@ function buildToDo(toDo, index) {
   const toDoShell = document.createElement('div');
   toDoShell.className = 'toDoShell';
   const toDoText = document.createElement('span');
-  toDoText.innerHTML = toDo.description;
+  toDoText.innerHTML = toDoText.description;
   toDoText.id = index;
   toDoText.onclick = completeToDo;
   if(toDoText.complete === true) {
