ebf4362218b0d72a9f9df6d9fc7a3ae9ece1718d
EricMohr <emohr1977@yahoo.com>
2018-03-13 18:04:32 -0500

    updated homework

--- Lesson4-JS-I/homework/homework.js
+++ Lesson4-JS-I/homework/homework.js
@@ -63,11 +63,7 @@ function areEqual(x, y) {
 function areSameLength(str1, str2) {
   // return true if the two strings have the same length
   // otherwise return false
-   if (str1.length === str2.length) {
-    return true; }
-   else {
-    return false;
-    }
+   return str1.length === str2.length; 
 }
 
 function lessThanNinety(num) {
