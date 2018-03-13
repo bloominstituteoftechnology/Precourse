9e0f21dd8d47b8e86b9d990d5d69ea8d439740ff
april <martinezapril56@yahoo.com>
2018-03-13 11:10:25 -0400

    commit

--- Lesson12-DOM/homework/DOMhomework.js
+++ Lesson12-DOM/homework/DOMhomework.js
@@ -139,7 +139,7 @@ function displayToDos() {
 function addToDo() {
   // code here
    
-  let newItem = ToDo(document.querySelector('.ToDo').value = newToDo);
+  let newItem = ToDo(document.querySelector('.ToDo').value = []);
   toDoItems.push(ToDo);
   document.querySelector('.newToDo');
   displayToDos(toDos);
