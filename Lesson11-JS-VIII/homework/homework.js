bf7d984a79213fed43e8740adb1b7954ecb9d70b
udykas <udykas@gmail.com>
2018-03-13 14:11:04 -0400

    Lesson 11 complete!

--- Lesson11-JS-VIII/homework/homework.js
+++ Lesson11-JS-VIII/homework/homework.js
@@ -3,12 +3,35 @@
 // solve these recursively
 
 function nFactorial(n) {
+
+  let sum = 1;
+
+  function factorial(n){
+    if(n < 1) {
+      return sum;
+    }
+    sum = sum * n;
+    return factorial(n - 1);
+  }
+
+  return factorial(n);
+
   // return the factorial for n
   // example:
   // the factorial of 3 is 6 (3 * 2 * 1)
 }
 
 function nFibonacci(n) {
+
+  function fibonacci(n){
+    if (n <= 1){
+      return 1;
+    }
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+
+  return fibonacci(n);
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
 }
