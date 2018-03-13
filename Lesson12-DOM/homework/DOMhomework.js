21c679f967a6f8422cfd35a0110aef3f0d4645e3
april <martinezapril56@yahoo.com>
2018-03-13 01:05:23 -0400

    com

--- Lesson12-DOM/homework/DOMhomework.js
+++ Lesson12-DOM/homework/DOMhomework.js
@@ -138,7 +138,7 @@ function displayToDos() {
 function addToDo() {
   // code here
    
-  let newItem = ToDo(document.querySelector('.toDoItems').value);
+  let newItem = ToDo(document.querySelector('.toDoItems').value = description);
   toDoItems.push(ToDo);
   document.querySelector('.newToDo');
   displayToDos(toDos);
