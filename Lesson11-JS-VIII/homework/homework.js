f27f9844c531b26122e94ceadabb92398b0de86e
april <martinezapril56@yahoo.com>
2018-03-13 20:24:17 -0400

    commit

--- Lesson11-JS-VIII/homework/homework.js
+++ Lesson11-JS-VIII/homework/homework.js
@@ -24,8 +24,9 @@ function nFactorial(n) {
 function nFibonacci(n) {
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
-  if(n===0 || n===1) return 1;
-    return nFibonacci(n-2) + nFibonacci(n-1);
+  
+    if(n===0 || n===1) return 1;
+      return nFibonacci(n-2) + nFibonacci(n-1);
 
 }
 
