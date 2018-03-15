12f2639ba716e2dfda6b48d3632504b5cd7faabf
blowry619 <blowry619@gmail.com>
2018-03-15 19:25:08 -0400

    homework changes

--- Lesson5-JS-II/homework/homework.js
+++ Lesson5-JS-II/homework/homework.js
@@ -1,31 +1,47 @@
 // Do not change any of the function names
 
-function multiplyArguments() {
-  // use the arguments keyword to multiply all of the arguments together and return the product
-  // if no arguments are passed in return 0
-  // if one argument is passed in just return it
+function multiplyArguments(){
+
 }
 
 function getBiggest(x, y) {
-  // x and y are integers.  Return the larger integer
-  // if they are the same return either one
+    // x and y are integers.  Return the larger integer
+    // if they are the same return either one
 }
 
 function greeting(language) {
-  // return a greeting for three different languages:
+  if (language = 'German') {
+    return 'Gutem Tag!';
+  } else if (language = 'Mandarin') {
+    return 'Ni Hao!';
+  } else if (language = 'Spanish') {
+    return 'Hola!';
+  } else ()
+    return'Hello!';
+} // return a greeting for three different languages:
   // language: 'German' -> 'Guten Tag!'
   // language: 'Mandarin' -> 'Ni Hao!'
   // language: 'Spanish' -> 'Hola!'
   // if language is undefined return 'Hello!'
-}
+
 
 function isTenOrFive(num) {
   // return true if num is 10 or 5
-  // otherwise return false
+  if (num === 10 || num === 5 ) {
+    return'true';
+  } else {
+    return'false';
+  }
+ // otherwise return false
 }
 
 function isInRange(num) {
   // return true if num is less than 50 and greater than 20
+  if ( num < 50 && num > 20) {
+    return 'true';
+  } else {
+    return 'false';
+  }
   // otherwise return false
 }
 
@@ -40,6 +56,15 @@ function isInteger(num) {
 
 function fizzBuzz(num) {
   // if num is divisible by 3 return 'fizz'
+  if (num % 3 === 0){
+    return 'fizz';
+  } else if( num % 5 === 0){
+    return 'buzz';
+  } else if (num % 5 === 0 && num % 3 === 0)
+    return 'fizzbuzz';
+  } else () {
+    return 'null';
+  }
   // if num is divisible by 5 return 'buzz'
   // if num is divisible by 3 & 5 return 'fizzbuzz'
   // otherwise return num
