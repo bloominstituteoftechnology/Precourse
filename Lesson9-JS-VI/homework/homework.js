828f246d5dfdcae08bb2b1cbdbb72c60ebbeaa5e
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-03-20 20:36:13 +0800

    Added new file to the Lesson9-JS-VI/homework folder, namely the homework.js file.

--- Lesson9-JS-VI/homework/homework.js
+++ Lesson9-JS-VI/homework/homework.js
@@ -2,23 +2,35 @@
 
 function invokeCallback(cb) {
   // invoke cb
+  return cb();
 }
 
 function sumArray(numbers, cb) {
   // sum up all of the integers in the numbers array
   // pass the result to cb
   // no return is necessary
+  const sum = numbers.reduce (function (accum, item) {
+    return accum + item;
+  });
+  return cb(sum);
 }
 
 function forEach(arr, cb) {
   // iterate over arr and pass its values to cb one by one
   // hint: you will be invoking cb multiple times (once for each value in the array)
+  arr.forEach(function (item, index) {
+    cb(item);
+  });
 }
 
 function map(arr, cb) {
   // create a new array
   // iterate over each value in arr, pass it to cb, then place the value returned from cb into the new arr
   // the new array should be the same length as the array argument
+  arr.forEach(function (item){
+    cb(item);
+  });
+    return arr.map(cb);
 }
 
 // Do not modify code below this line.
