81160e2bcd60bc39e26d5b856d1a0b3d1d366a26
Bridget Lowry <blowry619@gmail.com>
2018-03-20 16:41:02 -0700

    completed wk6

--- Lesson6-JS-III/homework/homework.js
+++ Lesson6-JS-III/homework/homework.js
@@ -19,6 +19,10 @@ function incrementByOne(arr) {
   // arr is an array of integers  
   // increase each integer by one
   // return the array
+  for ( let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i] + 1;
+  }
+  return arr;
   }
 
 function addItemToArray(arr, item) {
@@ -41,16 +45,13 @@ function wordsToSentence(words) {
   // return a string that is all of the words concatenated together
   // spaces need to be between each word
   // example: ['Hello', 'world!'] -> 'Hello world!'
-  return words;
+  return words.join(' ');
 }
 
 function contains(arr, item) {
   // check to see if item is inside of arr
   // return true if it is, otherwise return false
-  let found = arr.find(item);
-  if (i = 0; i < arr.length; i++)
-    
-  }
+ return (arr.includes(item));
 }
 
 function addNumbers(numbers) {
@@ -79,12 +80,27 @@ function averageTestScore(testScores) {
 function largestNumber(numbers) {
   // numbers is an array of integers
   // return the largest integer
+for (let i = 0; i < numbers.length; i++) {  
+  return Math.max.apply(null, numbers);
+  }
 }
 
 function multiplyArguments() {
   // use the arguments keyword to multiply all of the arguments together and return the product
   // if no arguments are passed in return 0
   // if one argument is passed in just return it
+  let product = 1;
+  if (arguments.length === 0) {
+    product = 0;
+  } 
+  for (let i = 0; i < arguments.length; i++) {
+    if (arguments.length === 1) {
+      product = arguments[0];
+    } else {  
+    product = arguments[i] * product;
+    }
+  }
+  return product;
 }
 
 // Do not modify code below this line.
