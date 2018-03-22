dd930b96a076df2b9205435f5f9cb43c847be89d
diana-so <diana.south@gmx.com>
2018-03-21 23:51:05 -0400

    updated Lesson 6 homework.js after merge for March repo

--- Lesson6-JS-III/homework/homework.js
+++ Lesson6-JS-III/homework/homework.js
@@ -120,11 +120,25 @@ function largestNumber(numbers) {
   return max;
 }
 
+
 function multiplyArguments() {
   // use the arguments keyword to multiply all of the arguments together and return the product
   // if no arguments are passed in return 0
   // if one argument is passed in just return it
+  let product = 1;
+
+  // returns to 0 if no arguments are passed
+  if (arguments.length === 0) {
+      return 0;
+  }
+
+  for (let i = 0; i < arguments.length; i++) {
+    product = product * arguments[i];
+  }
+
+  return product;
 }
+
 
 // Do not modify code below this line.
 // --------------------------------
