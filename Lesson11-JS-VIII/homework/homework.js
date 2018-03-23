326644a4d65e3853c8788df2264bd8372693da09
den200x <delu@ucdavis.edu>
2018-03-22 23:04:17 -0700

    hw11

--- Lesson11-JS-VIII/homework/homework.js
+++ Lesson11-JS-VIII/homework/homework.js
@@ -3,12 +3,26 @@
 // solve these recursively
 
 function nFactorial(n) {
+  var fac = 1;
+  while (n !== 0 ){
+    fac = fac * n;
+    n = n - 1;
+  }
+  return fac;
   // return the factorial for n
   // example:
   // the factorial of 3 is 6 (3 * 2 * 1)
 }
 
 function nFibonacci(n) {
+  var a = 0, b = 1, c =1;
+  for (var i = 1; i <= n; i++){
+    c = a + b;
+    a = b;
+    b = c;
+  }
+
+  return c;
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
 }
