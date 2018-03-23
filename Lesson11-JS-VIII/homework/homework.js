ef76c3e7327d21022107fa8eb20fc0d1b8ab26f6
eddygonzalez9708 <edward.gonzalez9708@gmail.com>
2018-03-22 21:02:19 -0400

    modified homework.js and add JavaScript_Concepts_LS11.txt

--- Lesson11-JS-VIII/homework/homework.js
+++ Lesson11-JS-VIII/homework/homework.js
@@ -6,11 +6,47 @@ function nFactorial(n) {
   // return the factorial for n
   // example:
   // the factorial of 3 is 6 (3 * 2 * 1)
+
+  function factorialOfN(start, productSoFar) {
+
+    if (productSoFar === undefined) {
+      productSoFar = 1;
+    }
+
+    if (start >= 0 && start <= 1) {
+      return productSoFar;
+    }
+    else if (start < 0) {
+      return 'Invalid Input';
+    }
+
+    productSoFar = productSoFar * start;
+    return factorialOfN(start-1, productSoFar);
+  }
+
+  return factorialOfN(n);
 }
 
 function nFibonacci(n) {
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+
+  function fib(cont1, cont2, cont3, position) {
+
+    if (n <= 0 || typeof(n) != 'number') {
+      return 'Invalid Input';
+    }
+    else if (position === n) {
+      return cont1;
+    }
+
+    cont3 = cont1;
+    cont1 = cont1 + cont2;
+    position = position + 1;
+    return fib(cont1, cont3, cont3, position);
+  }
+
+  return fib(1,1,1,1);
 }
 
 // Do not modify code below this line.
