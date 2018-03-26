734dad36261818ed571627fd38e8367141249784
gjavaras <fr33k3r@yahoo.com>
2018-03-26 20:14:06 +0100

    -Created Feynman Writing Prompts -Solved Exercises

--- Lesson11-JS-VIII/homework/homework.js
+++ Lesson11-JS-VIII/homework/homework.js
@@ -6,11 +6,23 @@ function nFactorial(n) {
   // return the factorial for n
   // example:
   // the factorial of 3 is 6 (3 * 2 * 1)
+  if (n === 0) {
+    return 1;
+  }
+  return n * nFactorial(n-1);
 }
 
 function nFibonacci(n) {
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  if (n === 1) {
+    return 1;
+  }
+  if (n === 2) {
+    return 2;
+  }
+
+  return nFibonacci(n-2) + nFibonacci(n-1);
 }
 
 // Do not modify code below this line.
