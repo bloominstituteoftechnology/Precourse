d70e4db5ab61b6d312637f8b4dec06126baed780
German Gladkov <germangladkov@Germans-MacBook-Pro.local>
2018-03-26 17:25:24 -0700

    homework day 5

--- Lesson5-JS-II/homework/homework.js
+++ Lesson5-JS-II/homework/homework.js
@@ -3,9 +3,33 @@
 function getBiggest(x, y) {
   // x and y are integers.  Return the larger integer
   // if they are the same return either one
+  if (x > y) {
+    return x;
+  }
+  else if (y > x) {
+    return y;
+  }
+  else {return x;}
+
 }
 
 function greeting(language) {
+  if (language === 'German')
+  {
+    return 'Guten Tag!';
+  }
+  else if (language === 'Mandarin')
+  {
+    return 'Ni Hao!';
+  }
+  else if (language === 'Spanish')
+  {
+    return 'Hola!';
+  }
+  else 
+  {
+    return 'Hello!';
+  }
   // return a greeting for three different languages:
   // language: 'German' -> 'Guten Tag!'
   // language: 'Mandarin' -> 'Ni Hao!'
@@ -14,16 +38,30 @@ function greeting(language) {
 }
 
 function isTenOrFive(num) {
+  if (num === 10 || num === 5)
+  {
+    return true;
+  }
+  return false;
   // return true if num is 10 or 5
   // otherwise return false
 }
 
 function isInRange(num) {
+  if (num < 50 && num > 20)
+  {
+    return true;
+  }
+  return false;
   // return true if num is less than 50 and greater than 20
   // otherwise return false
 }
 
 function isInteger(num) {
+  if (Math.floor(num) === num)
+    return true;
+  else
+    return false;
   // return true if num is an integer
   // 0.8 -> false
   // 1 -> true
@@ -33,6 +71,13 @@ function isInteger(num) {
 }
 
 function fizzBuzz(num) {
+  if ( num % 3 === 0 && num % 5 === 0) 
+    return 'fizzbuzz';
+  else if (num % 3 === 0)
+    return 'fizz';
+  else if (num % 5 === 0)
+    return 'buzz';
+  else return num;
   // if num is divisible by 3 return 'fizz'
   // if num is divisible by 5 return 'buzz'
   // if num is divisible by 3 & 5 return 'fizzbuzz'
@@ -40,6 +85,17 @@ function fizzBuzz(num) {
 }
 
 function isPrime(num) {
+  if (num <= 1)
+{
+  return false;}
+  for (let i = 2; i * i <= num; i++)
+{
+  if (num % i === 0)
+  {
+    return false;
+  }
+  return true;
+}
   // return true if num is prime.
   // otherwise return false
   // hint: a prime number is only evenly divisible by itself and 1
