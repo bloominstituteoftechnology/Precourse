734311c55032023ae80c981bc48eb97b6b63cf61
Jajah N <srichaii@Jajahs-MacBook-Pro.local>
2018-03-26 22:33:23 -0400

    revised homework lesson 11

--- Lesson11-JS-VIII/homework/homework.js
+++ Lesson11-JS-VIII/homework/homework.js
@@ -15,13 +15,13 @@ function nFactorial(n) {
 function nFibonacci(n) {
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
-  if (n < 1) {
-    return 0;
-  } 
-  if ( n <= 2) {
-    return 1; 
+  if (n <= 1) {
+    return 1;
+  } else if (n === 2) {
+    return 2;
+  } else {
+    return nFibonacci(n - 1) + nFibonacci(n - 2);
   }
-  return nFibonacci(n - 1) + nFibonacci(n - 2);
 }
 
 // Do not modify code below this line.
