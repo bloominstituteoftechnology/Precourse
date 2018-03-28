cd0f0640d35d66980bcd89d8140ba6a075d83683
Rob <rob@users-MacBook-Air.local>
2018-03-28 17:11:53 -0500

    updated files

--- Lesson4-JS-I/homework/homework.js
+++ Lesson4-JS-I/homework/homework.js
@@ -1,149 +1,184 @@
 //In these first 6 questions, replace `null` with the answer
 
 //create a string variable, it can contain anything
-const newString = null ;
+const newString = "bloodhound";
 
 //create a number variable, it an be any number
-const newNum = null ;
+const newNum = 45;
 
 //create a boolean variable
-const newBool = null ;
+const newBool = "lovesBloodhounds" = true;
 
 //solve the following math problem
-const newSubtract = 10 - null === 5;
+const newSubtract = 10 - 5 == 5;
 
 //Solve the following math problem
-const newMultiply = 10 * null === 40 ;
+const newMultiply = 10 * 4 == 40 ;
 
 //Solve the following math problem:
-const newModulo = 21 % 5 === null ;
+const newModulo = 21 % 5 == 1 ;
 
 
 
-//In the next 22 problems you will compete the function. All of your code will go inside of the function braces. 
+//In the next 22 problems you will compete the function. All of your code will go inside of the function braces.
 //Make sure you use return when the prompt asks you to.
-//hint: console.log() will NOT work. 
+//hint: console.log() will NOT work.
 //Do not change any of the function names
 
-function returnString(str) {
-  //simply return the string provided: str
+function returnString(str);{
+  const strng = str;
+  return strng;
 }
+  //simply return the string provided: str
 
-function add(x, y) {
+
+
+function add(x, y) { const sum = x + y;
+  return sum;
   // x and y are numbers
   // add x and y together and return the value
   // code here
 }
 
-function subtract(x, y) {
+function subtract(x, y) { const difference = x-y;
+  return difference;
   // subtract y from x and return the value
   // code here
 }
 
-function multiply(x, y) {
+function multiply(x, y) { const product = x * y;
+  return product;
   // multiply x by y and return the value
   // code here
 }
 
-function divide(x, y) {
+function divide(x, y){ const solution = x / y;
+   return solution;
   // divide x by y and return the value
   // code here
 }
 
-function areEqual(x, y) {
+
+function areEqual(x, y){ if (x = y){
+  return true;
+}
+  return false;
+}
+
   // return true if x and y are the same
   // otherwise return false
   // code here
-}
+
 
 function areSameLength(str1, str2) {
+  if (str1.length == str2.length)
+  return true;
+  return false;
+}
   // return true if the two strings have the same length
   // otherwise return false
-  // code here
-}
+// code here
 
-function lessThanNinety(num) {
+
+function lessThanNinety(num) { if (num < 90 )
+  return true;
+
+  return false;
+}
   // return true if the function argument: num , is less than ninety
   // otherwise return false
-  // code here
-}
+
 
 function greaterThanFifty(num) {
   // return true if num is greater than fifty
   // otherwise return false
-  // code here
+  if (num > 50) { return true;
+  return false;// code here
+
 }
 
 function getRemainder(x, y) {
   // return the remainder from dividing x by y
-  // code here
+  const remainder = x % y;
+    return remainder;// code here
 }
 
-function isEven(num) {
+function isEven(num) { if (num % 2 === 0) return true;
+
   // return true if num is even
   // otherwise return false
   // code here
+  return false;
 }
 
 function isOdd(num) {
+
   // return true if num is odd
   // otherwise return false
-  // code here
+
+  if (num % 2 === 1) return true;// code here
+  return false;
+
 }
 
-function square(num) {
+function square(num) { return num.pow;
+
   // square num and return the new value
   // hint: NOT square root!
-  // code here
+
 }
 
-function cube(num) {
+function cube(num) { return (num.pow * num);
   // cube num and return the new value
   // code here
 }
 
-function raiseToPower(num, exponent) {
+function raiseToPower(num, exponent) { return Math.pow(num,exponent);
+
   // raise num to whatever power is passed in as exponent
   // code here
 }
 
 function roundNumber(num) {
   // round num and return it
-  // code here
+  return num.round;// code here
 }
 
-function roundUp(num) {
+function roundUp(num) { return num.ceil;
   // round num up and return it
   // code here
 }
 
-function addExclamationPoint(str) {
-  // add an exclamation point to the end of str and return the new string
+function addExclamationPoint(str) { const myExclamation = "str" + "!";
+return myExclamation;  // add an exclamation point to the end of str and return the new string
   // 'hello world' -> 'hello world!'
   // code here
 }
 
-function combineNames(firstName, lastName) {
-  // return firstName and lastName combined as one string and separated by a space.
+function combineNames(firstName, lastName) { const myName = "firstName" + " lasltName";
+return myName;  // return firstName and lastName combined as one string and separated by a space.
   // 'Lambda', 'School' -> 'Lambda School'
   // code here
 }
 
-function getGreeting(name) {
+function getGreeting(name) { const greet = "Hello" + " name";
+return greet;
   // Take the name string and concatenate other strings onto it so it takes the following form:
   // 'Sam' -> 'Hello Sam!'
   // code here
 }
 
-// The next three questions will have you implement math area formulas. 
+// The next three questions will have you implement math area formulas.
 // If you can't remember these area formulas then head over to Google.
- 
-function getRectangleArea(length, width) {
+
+function getRectangleArea(length, width) { const area = length * width;
+  return area;
   // return the area of the rectangle by using length and width
   // code here
 }
 
-function getTriangleArea(base, height) {
+function getTriangleArea(base, height) { const area = base * height;
+  return area;
   // return the area of the triangle by using base and height
   // code here
 }
@@ -180,4 +215,5 @@ module.exports = {
   getGreeting,
   getRectangleArea,
   getTriangleArea,
-};
+}
+}
--- Lesson5-JS-II/homework/homework.js
+++ Lesson5-JS-II/homework/homework.js
@@ -1,51 +1,104 @@
 // Do not change any of the function names
 
-function getBiggest(x, y) {
+function getBiggest(x, y) { return Math.max(x, y); {
+  if (x = y) console.log(x || y);
+}
   // x and y are integers.  Return the larger integer
   // if they are the same return either one
 }
 
-function greeting(language) {
+function greeting(language); {
+  if (language === 'German') {
+    return ('Guten Tag!');
+  } else if (language === 'Mandarin') {
+    return ('Ni Hao!');
+  } else if (language === 'Spanish') {
+    return ('Hola!');
+  } else 
+    return ('Hello!');
+  }
+
+
+ 
+
   // return a greeting for three different languages:
   // language: 'German' -> 'Guten Tag!'
   // language: 'Mandarin' -> 'Ni Hao!'
   // language: 'Spanish' -> 'Hola!'
   // if language is undefined return 'Hello!'
-}
 
-function isTenOrFive(num) {
+
+function isTenOrFive(num) { const moreFives = (10,5); { if (num = moreFives) return true;
+  else return false;
+}
   // return true if num is 10 or 5
   // otherwise return false
 }
 
-function isInRange(num) {
+function isInRange(num) { 
   // return true if num is less than 50 and greater than 20
   // otherwise return false
+  if (num > 20  && num < 50)
+  return true;
+else return false;
 }
 
-function isInteger(num) {
+function isInteger(num) { 
   // return true if num is an integer
   // 0.8 -> false
   // 1 -> true
   // -10 -> true
   // otherwise return false
   // hint: you can solve this using Math.floor
+  if (Math.floor = num)
+  return true;
+  else return false;
 }
 
-function fizzBuzz(num) {
+function fizzBuzz(num){ 
   // if num is divisible by 3 return 'fizz'
   // if num is divisible by 5 return 'buzz'
   // if num is divisible by 3 & 5 return 'fizzbuzz'
   // otherwise return num
+  if (num % 3 === 0 && 5 === 0 ){
+
+    return 'fizzbuzz';
+
+  } else if (num % 3 === 0){
+  
+    return 'fizz';
+  
+ } else if (num % 5 === 0){ 
+
+    return 'buzz';
+  }
+   else return num;
 }
 
-function isPrime(num) {
+
+
+function isPrime(num) { 
+
   // return true if num is prime.
   // otherwise return false
   // hint: a prime number is only evenly divisible by itself and 1
   // hint2: you can solve this using a for loop
   // note: 0 and 1 are NOT considered prime numbers
-}
+if (num === 0 || 1)
+  { return false;
+  }
+    else if (num === 2)
+  {
+    return true;
+
+  } else 
+  { for (let i = 2; i < num; x++)
+    { if (num % i ===0) 
+      return false;
+    }
+  }
+    return true;
+} 
 
 
 
@@ -60,4 +113,5 @@ module.exports = {
   isInteger,
   fizzBuzz,
   isPrime,
-};
+}
+
