0b09a297650ab51abe059910d21a2338918f16af
Usman Javed <ujaved931@gmail.com>
2018-03-29 17:31:58 +0500

    Finished Lesson 6 Homework

--- Lesson6-JS-III/homework/homework.js
+++ Lesson6-JS-III/homework/homework.js
@@ -1,35 +1,44 @@
 // Do not change any of the function names
 
 function returnFirst(arr) {
+  return arr[0];
   // return the first item from the array
 }
 
 function returnLast(arr) {
+  return arr[arr.length - 1];
   // return the last item of the array
 }
 
 function getArrayLength(arr) {
+  return arr.length;
   // return the length of the array
 }
 
 function incrementByOne(arr) {
+  return arr.map(num => num + 1);
   // arr is an array of integers  
   // increase each integer by one
   // return the array
 }
 
 function addItemToArray(arr, item) {
+  arr.push(item);
+  return arr;
   // add the item to the end of the array
   // return the array
 }
 
 function addItemToFront(arr, item) {
+  arr.unshift(item);
+  return arr;
   // add the item to the front of the array
   // return the array
   // hint: use the array method .unshift
 }
 
 function wordsToSentence(words) {
+  return words.join(' ');
   // words is an array of strings
   // return a string that is all of the words concatenated together
   // spaces need to be between each word
@@ -37,21 +46,31 @@ function wordsToSentence(words) {
 }
 
 function contains(arr, item) {
+  return arr.includes(item);
   // check to see if item is inside of arr
   // return true if it is, otherwise return false
 }
 
 function addNumbers(numbers) {
+  return numbers.reduce((sum, num) => sum + num,0);
   // numbers is an array of integers.
   // add all of the integers and return the value
 }
 
 function averageTestScore(testScores) {
+  if (testScores.length !== 0){
+    const addingScores = testScores.reduce((sum, num) => sum + num,0);
+    let avg = addingScores/testScores.length;
+    return avg;
+  }
+  else return testScores;
   // testScores is an array.  Iterate over testScores and compute the average.
   // return the average
 }
 
 function largestNumber(numbers) {
+  const largest = Math.max(...numbers);
+  return largest;
   // numbers is an array of integers
   // return the largest integer
 }
