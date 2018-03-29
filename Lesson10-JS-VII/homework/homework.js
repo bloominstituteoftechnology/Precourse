7c63ca1b653aafbad69c4e962b997604f5f42c75
jbierly1 <james.bierly@gmail.com>
2018-03-29 12:09:16 -0400

    finished lesson 10 hw and writing prompt

--- Lesson10-JS-VII/homework/homework.js
+++ Lesson10-JS-VII/homework/homework.js
@@ -1,6 +1,12 @@
 // Do not change any of the function names
 
 function counter() {
+  let counter=0;
+  return(function newCounter(){
+      counter++;
+      return(counter);
+    });
+
   // Return a function that when invoked increments and returns a counter variable.
   // Example: const newCounter = counter();
   // newCounter(); // 1
@@ -8,7 +14,18 @@ function counter() {
 }
 
 function cacheFunction(cb) {
-  // use closure to create a cache for the cb function
+let cache={};
+  return(function evaluateArg(arg){
+    if(arg in cache){
+      return(cache[arg]);
+    }else{
+    let result=cb(arg);
+    cache[arg]=result;
+    return(result);
+  }
+  });
+}
+ // use closure to create a cache for the cb function
   // the function that you return should accept a single argument and invoke cb with that argument
   // when the function you return is invoked with an argument it should save that argument and its result
   // when the function you return is called again with an argument that it has seen before it should not call cb
@@ -18,7 +35,6 @@ function cacheFunction(cb) {
   // if the function you return is invoked with 5 it would pass 5 to cb(5) and return 25
   // if the function you return is invoked again with 5 it will look on an object in the closure scope
   // and return 25 directly and will not invoke cb again
-}
 
 // Do not modify code below this line.
 // --------------------------------
