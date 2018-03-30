1fa50ffe08e52757e26e058319d301cd5c5e25c8
Alex Ager <alexager5028@Alexs-MacBook-Air.local>
2018-03-29 19:28:58 -0600

    This is the completed version of Precourse homework 7 on objects and methods.

--- Lesson7-JS-IV/homework/homework.js
+++ Lesson7-JS-IV/homework/homework.js
@@ -1,82 +1,164 @@
 // Do not change any of the function names
 
+// 1)
 function makeCat(name, age) {
   // create a new object with a name property with the value set to the name argument
   // add an age property to the object with the value set to the age argument
   // add a method called meow that returns the string 'Meow!'
   // return the object
+      const kitty = {
+          name: name,
+          age: age,
+          meow: function() {
+            return 'Meow!';
+          }
+
+        };
+        return kitty;
 }
 
+// 2)
 function addProperty(object, property) {
   // add the property to the object with a value of null
   // return the object
   // note: the property name is NOT 'property'.  The name is the value of the argument called property (a string)
+      object[property] = null;
+      return object;
 }
 
 function invokeMethod(object, method) {
   // method is a string that contains the name of a method on the object
   // invoke this method
   // nothing needs to be returned
+      object[method]();
+
 }
 
+// 4)
 function multiplyMysteryNumberByFive(mysteryNumberObject) {
   // mysteryNumberObject has a property called mysteryNumber
   // multiply the mysteryNumber property by 5 and return the product
+      let newNum = mysteryNumberObject.mysteryNumber * 5;
+      return newNum;
 }
 
+// 5)
 function deleteProperty(object, property) {
   // remove the property from the object
   // return the object
+        delete object[property];
+        return object;
+      
 }
 
+// 6)
 function newUser(name, email, password) {
   // create a new object with properties matching the arguments passed in.
   // return the new object
-}
+        const object = {
+            name: name,
+            email: email,
+            password,
 
+        };
+          return object;
+      }
+
+
+// 7)
 function hasEmail(user) {
   // return true if the user has a value for the property 'email'
   // otherwise return false
-}
+          if (user.email) {
+            return true;
+          }
+          else {
+            return false;
+          }
 
+          }
+
+// 8)
 function hasProperty(object, property) {
   // return true if the object has the value of the property argument
   // property is a string
   // otherwise return false
+         if (object.hasOwnProperty(property)) {
+           return true;
+         }  else  {
+           return false;
+         }
+         
 }
+      
 
+
+// 9)
 function verifyPassword(user, password) {
   // check to see if the provided password matches the password property on the user object
   // return true if they match
   // otherwise return false
+        if (user.password === password) {
+          return true;
+        }
+        else {
+          return false;
+        }
+        
 }
 
+// 10)
 function updatePassword(user, newPassword) {
   // replace the existing password on the user object with the value of newPassword
   // return the object
+          user.password = newPassword;
+          return user;
+
 }
 
+// 11)
 function addFriend(user, newFriend) {
   // user has a property called friends that is an array
   // add newFriend to the end of the friends array
   // return the user object
+        user.friends.push(newFriend);
+        return user;
+  
 }
 
+
+// 12)
 function setUsersToPremium(users) {
   // users is an array of user objects.
   // each user object has the property 'isPremium'
   // set each user's isPremium property to true
   // return the users array
+      for (let i = 0; i < users.length; i++) {
+            users[i].isPremium = true;
+      }
+      return users;
 }
 
+
+// 13)
 function sumUserPostLikes(user) {
   // user has an array property called 'posts'
   // posts is an array of post objects
   // each post object has an integer property called 'likes'
   // sum together the likes from all the post objects
   // return the sum
-}
+  let likeSum = 0;
+  user.posts.forEach(function(post) {
+    likeSum += post.likes;
+    return likeSum;
+  });
+  return likeSum;
 
+      }
+  // array.forEach(function(currentValue, index, arr), thisValue)
+
+
+// 14)
 function addCalculateDiscountPriceMethod(storeItem) {
   // add a method to the storeItem object called 'calculateDiscountPrice'
   // this method should multiply the storeItem's 'price' and 'discountPercentage' to get the discount
@@ -86,7 +168,12 @@ function addCalculateDiscountPriceMethod(storeItem) {
   // price -> 20
   // discountPercentage -> .2
   // discountPrice = 20 - (20 * .2)
-}
+       storeItem.calculateDiscountPrice = function() {
+            let discountPrice = storeItem.price - storeItem.price * storeItem.discountPercentage;
+            return discountPrice;
+       };
+       return storeItem;
+    }
 
 // Do not modify code below this line.
 // --------------------------------
