0e5914d111952d7ba10e8ee866dfaf171768ad77
Kyran McCann <kyran.mccann@gmail.com>
2018-03-30 19:38:56 -0700

    Completed Lesson 6 homework and added feynman prompts from lessons 5 and 6

--- Lesson6-JS-III/homework/homework.js
+++ Lesson6-JS-III/homework/homework.js
@@ -2,31 +2,42 @@
 
 function returnFirst(arr) {
   // return the first item from the array
+  return arr[0];
 }
 
 function returnLast(arr) {
   // return the last item of the array
+  return arr[arr.length - 1];
 }
 
 function getArrayLength(arr) {
   // return the length of the array
+  return arr.length;
 }
 
 function incrementByOne(arr) {
   // arr is an array of integers  
   // increase each integer by one
   // return the array
+  for (let i = 0; i < arr.length; i++){
+    arr[i] = arr[i] + 1;
+  }
+  return arr;
 }
 
 function addItemToArray(arr, item) {
   // add the item to the end of the array
   // return the array
+  arr.push(item); 
+  return arr;
 }
 
 function addItemToFront(arr, item) {
   // add the item to the front of the array
   // return the array
   // hint: use the array method .unshift
+  arr.unshift(item);
+  return arr;
 }
 
 function wordsToSentence(words) {
@@ -34,32 +45,71 @@ function wordsToSentence(words) {
   // return a string that is all of the words concatenated together
   // spaces need to be between each word
   // example: ['Hello', 'world!'] -> 'Hello world!'
+  let sentence = words.join(' ');
+  return sentence;
 }
 
 function contains(arr, item) {
   // check to see if item is inside of arr
   // return true if it is, otherwise return false
+  if (arr.length > 0){
+    for (let i = 0; i < arr.length; i++){
+      if (arr[i] === item){
+        return true; 
+      } else {
+        return false; 
+      }
+    } 
+  } else {
+    return false; 
+  }
 }
 
 function addNumbers(numbers) {
   // numbers is an array of integers.
   // add all of the integers and return the value
+  let sum = 0; 
+  for (let i = 0; i < numbers.length; i ++){
+    sum = sum + numbers[i]; 
+  }
+  return sum;
 }
 
 function averageTestScore(testScores) {
   // testScores is an array.  Iterate over testScores and compute the average.
   // return the average
+  let sum = 0;
+  for (let i = 0; i < testScores.length; i++){
+    sum = sum + testScores[i]; 
+  }
+  return sum / testScores.length; 
 }
 
 function largestNumber(numbers) {
   // numbers is an array of integers
   // return the largest integer
+  let biggest = 0; 
+  for (let i = 0; i < numbers.length; i++){
+    if (numbers[i] > biggest){
+      biggest = numbers[i]; 
+    } 
+  } return biggest; 
 }
 
 function multiplyArguments() {
   // use the arguments keyword to multiply all of the arguments together and return the product
   // if no arguments are passed in return 0
   // if one argument is passed in just return it
+  if (arguments.length > 0){
+    let product = 1; 
+    for (let i = 0; i < arguments.length; i++){
+      product = product * arguments[i];
+    }
+    return product; 
+  }
+  else { 
+    return 0;
+  }
 }
 
 // Do not modify code below this line.
