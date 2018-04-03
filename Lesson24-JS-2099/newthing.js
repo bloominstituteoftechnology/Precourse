41c28ee315df4f851b723fdb9efc0309f668b75c
Thundurus <marcus.moss@live.com>
2018-04-03 16:36:59 -0400

    Added a new folder, containing a functionless script.

---
+++ Lesson24-JS-2099/newthing.js
@@ -0,0 +1,28 @@
+ Volume in drive C has no label.
+ Volume Serial Number is D0BD-1A6D
+
+ Directory of C:\Users\XTC\Documents\LS_Precourse\Precourse
+
+04/03/2018  04:30 PM    <DIR>          .
+04/03/2018  04:30 PM    <DIR>          ..
+04/03/2018  04:26 PM               277 .eslintrc.js
+04/03/2018  04:26 PM               977 .gitignore
+04/03/2018  04:26 PM               188 .travis.yml
+04/03/2018  04:26 PM    <DIR>          Lesson01-Git
+04/03/2018  04:26 PM    <DIR>          Lesson02-HTML-CSS
+04/03/2018  04:26 PM    <DIR>          Lesson03-CSS-Positioning
+04/03/2018  04:26 PM    <DIR>          Lesson04-JS-I
+04/03/2018  04:26 PM    <DIR>          Lesson05-JS-II
+04/03/2018  04:26 PM    <DIR>          Lesson06-JS-III
+04/03/2018  04:26 PM    <DIR>          Lesson07-JS-IV
+04/03/2018  04:26 PM    <DIR>          Lesson08-JS-V
+04/03/2018  04:26 PM    <DIR>          Lesson09-JS-VI
+04/03/2018  04:26 PM    <DIR>          Lesson10-JS-VII
+04/03/2018  04:26 PM    <DIR>          Lesson11-JS-VIII
+04/03/2018  04:26 PM    <DIR>          Lesson12-DOM
+04/03/2018  04:30 PM    <DIR>          Lesson24-JS-2099
+04/03/2018  04:30 PM                 0 newthing.js
+04/03/2018  04:26 PM               904 package.json
+04/03/2018  04:26 PM             2,465 README.md
+               6 File(s)          4,811 bytes
+              15 Dir(s)  38,567,825,408 bytes free
