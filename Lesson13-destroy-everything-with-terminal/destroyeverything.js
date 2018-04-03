fbc0b49860c56879e8870a6ddf5ee8c850d72d4d
Ryan Justice <ryan.justice.19@hotmail.com>
2018-04-03 16:53:33 -0700

    Created new js file

---
+++ Lesson13-destroy-everything-with-terminal/destroyeverything.js
@@ -0,0 +1,12 @@
+ Volume in drive C has no label.
+ Volume Serial Number is A2D1-FFFF
+
+ Directory of C:\Users\ryanj\Documents\Lambda
+
+04/03/2018  04:44 PM    <DIR>          .
+04/03/2018  04:44 PM    <DIR>          ..
+04/03/2018  04:44 PM                 0 destroyeverything.js
+04/03/2018  04:42 PM    <DIR>          Lesson13-destroy-everything-with-terminal
+04/03/2018  04:40 PM    <DIR>          Precourse
+               1 File(s)              0 bytes
+               4 Dir(s)  261,594,148,864 bytes free
