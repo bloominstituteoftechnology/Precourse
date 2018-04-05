9658f91368d2378fb68a92bb773c60db3f7db4c0
tangled-tessellations <thepolitepanda@gmail.com>
2018-04-05 15:28:47 -0700

    Completed homework 8 and section 8 FeynmanDescription

--- Lesson8-JS-V/homework/homework.js
+++ Lesson8-JS-V/homework/homework.js
@@ -7,11 +7,26 @@ function createUserClass() {
   // the constructor should have a method 'sayHi' on its prototype that returns the string 'Hello, my name is {{name}}'
   // {{name}} should be the name set on each instance
   // return the class
+
+  const User = function (options) {
+    this.username = options.username;
+    this.name = options.name;
+    this.email = options.email;
+    this.password = options.password;
+    this.sayHi = function() {
+      return 'Hello, my name is ' + this.name;
+    };
+  };
+
+  return User;
 }
 
 function addPrototypeMethod(Constructor) {
   // add a method to the constructor's prototype
   // the method should be called 'sayHi' and should return the string 'Hello World!'
+  Constructor.prototype.sayHi = function() {
+    return 'Hello World!';
+  };
 }
 
 function addReverseString() {
@@ -19,6 +34,14 @@ function addReverseString() {
   // name this method reverse
   // hint:
   // you will need to use 'this' inside of reverse
+  String.prototype.reverse = function() {
+    let newString = '';
+    for (let i = 0; i < this.length; i++) {
+      let letterIndex = this.length - (i + 1);
+      newString += this[letterIndex];
+    }
+    return newString;
+  };
 }
 
 // Do not modify code below this line.
