feaa30fd3e68165a59a30372ce3e2aa831e40185
Adrian Adames <adrianadames@gmail.com>
2018-04-06 16:11:27 -0400

    submitted homework for Lesson 7

--- Lesson7-JS-IV/homework/homework.js
+++ Lesson7-JS-IV/homework/homework.js
@@ -5,61 +5,127 @@ function makeCat(name, age) {
   // add an age property to the object with the value set to the age argument
   // add a method called meow that returns the string 'Meow!'
   // return the object
+
+  const newObj = {
+    name: arguments[0], 
+    age: arguments[1], 
+    meow: function() {
+      return 'Meow!';
+    },
+  };
+  return newObj;
+  
 }
 
 function addProperty(object, property) {
   // add the property to the object with a value of null
   // return the object
   // note: the property name is NOT 'property'.  The name is the value of the argument called property (a string)
+
+  object[property] = null;
+
+  return object;
 }
 
 function invokeMethod(object, method) {
   // method is a string that contains the name of a method on the object
   // invoke this method
   // nothing needs to be returned
+  object[method]();
 }
 
 function multiplyMysteryNumberByFive(mysteryNumberObject) {
   // mysteryNumberObject has a property called mysteryNumber
   // multiply the mysteryNumber property by 5 and return the product
+  var x; 
+  x = mysteryNumberObject.mysteryNumber * 5;
+  return x;
+
 }
 
 function deleteProperty(object, property) {
   // remove the property from the object
   // return the object
+  delete object[property];
+  return object;
+
 }
 
 function newUser(name, email, password) {
   // create a new object with properties matching the arguments passed in.
   // return the new object
+  var newObject;
+  newObject = {name: arguments[0], email: arguments[1], password: arguments[2]};
+  return newObject;
 }
 
 function hasEmail(user) {
   // return true if the user has a value for the property 'email'
   // otherwise return false
+  var check;
+  
+  for (let key in user) {
+    if (key === 'email' && user[key] !== null) {
+      check =true;
+    }
+  }
+  if (check === true) {
+    return true;
+  }
+  else return false;
 }
+
+
+
 
 function hasProperty(object, property) {
   // return true if the object has the value of the property argument
   // property is a string
   // otherwise return false
+  var check; 
+
+  for (let key in object) {
+    if (key === property) {
+      check = true;
+    }
+    if (check === true) {
+      return true;
+    }
+    else return false;
+  }
+
 }
 
 function verifyPassword(user, password) {
   // check to see if the provided password matches the password property on the user object
   // return true if they match
   // otherwise return false
+
+  if (user.password === password) {
+    return true;
+  }
+  else return false;
+  
 }
 
 function updatePassword(user, newPassword) {
   // replace the existing password on the user object with the value of newPassword
   // return the object
+  user.password = newPassword;
+  return user;
 }
 
 function addFriend(user, newFriend) {
   // user has a property called friends that is an array
   // add newFriend to the end of the friends array
   // return the user object
+  var x; 
+
+  x = user.friends;
+  x.push(newFriend);
+
+  user.friends = x;
+  return user;
 }
 
 function setUsersToPremium(users) {
@@ -67,6 +133,14 @@ function setUsersToPremium(users) {
   // each user object has the property 'isPremium'
   // set each user's isPremium property to true
   // return the users array
+  var x;
+
+  for (let i = 0; i < users.length; i++) {
+    x = users[i];
+    x.isPremium = true;
+    users[i] = x;
+  }
+  return users;
 }
 
 function sumUserPostLikes(user) {
@@ -75,6 +149,19 @@ function sumUserPostLikes(user) {
   // each post object has an integer property called 'likes'
   // sum together the likes from all the post objects
   // return the sum
+  var x;
+  var y; 
+  var z; 
+  let sum = 0; 
+
+  x = user.posts;   // setting x to the property posts; the property posts is an array
+
+  for (let i=0; i < x.length; i++) {
+    y = x[i];     //setting y to equal each object in the array posts 
+    z= y.likes;   //setting z to equal the value of the property called likes in object y
+    sum = sum + z; 
+  }
+  return sum;
 }
 
 function addCalculateDiscountPriceMethod(storeItem) {
@@ -86,6 +173,14 @@ function addCalculateDiscountPriceMethod(storeItem) {
   // price -> 20
   // discountPercentage -> .2
   // discountPrice = 20 - (20 * .2)
+
+  var discountedPrice; 
+
+  storeItem.calculateDiscountPrice = function() {
+    discountedPrice = storeItem.price - storeItem.price * storeItem.discountPercentage;
+    return discountedPrice;
+  };
+  return storeItem;
 }
 
 // Do not modify code below this line.
