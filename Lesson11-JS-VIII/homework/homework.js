72900775888857df3177efd3e8964390de2af1cd
Alina Gilgur <Alina@Ostaps-MacBook-Air.local>
2018-04-06 14:21:51 -0500

    updated homework.js file

--- Lesson11-JS-VIII/homework/homework.js
+++ Lesson11-JS-VIII/homework/homework.js
@@ -1,16 +1,24 @@
 // Do not change any of the function names
-
 // solve these recursively
 
 function nFactorial(n) {
   // return the factorial for n
   // example:
   // the factorial of 3 is 6 (3 * 2 * 1)
+   if (n < 2) {
+     return n;
+   }
+   return n* nFactorial(n - 1);
 }
 
 function nFibonacci(n) {
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  if (n < 3){
+    return n;
+  }
+  let num = nFibonacci(n-2) + nFibonacci(n-1);
+  return num;
 }
 
 // Do not modify code below this line.
