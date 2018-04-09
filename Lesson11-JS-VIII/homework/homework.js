959cd2e3c1cd02ecc603f57c8a433fd4bd5a97d2
Alex Ostrander <alexanderostrander23@gmail.com>
2018-04-09 14:06:11 -0400

    Completed Lesson 11

--- Lesson11-JS-VIII/homework/homework.js
+++ Lesson11-JS-VIII/homework/homework.js
@@ -6,11 +6,34 @@ function nFactorial(n) {
   // return the factorial for n
   // example:
   // the factorial of 3 is 6 (3 * 2 * 1)
+  let numFactor = n;
+  let argNum = n;
+  if (argNum < 0) {
+  	return argNum;
+  }else if (argNum === 0) {
+  	return 1;
+  }else {
+  	while (argNum !== 1) {
+  		numFactor = numFactor * (argNum - 1);
+  		argNum -= 1;
+  	}
+  }
+  return numFactor;
 }
 
 function nFibonacci(n) {
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+
+  let preFirst = 0; 
+  let preSecond = 1; 
+  let next = 1;
+  for(let i = 2; i <= n + 1; i++) {
+    next = preFirst + preSecond;
+    preFirst = preSecond;
+    preSecond = next;
+  };
+  return next;
 }
 
 // Do not modify code below this line.
