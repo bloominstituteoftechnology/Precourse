012f6d0346511cb418bde59ee7c1d6c6a47bcb89
Samuel Kirkpatrick <kirksamalpha@gmail.com>
2018-04-10 10:44:54 -0500

    Added homework from Lesson 7

--- Lesson7-JS-IV/homework/homework.js
+++ Lesson7-JS-IV/homework/homework.js
@@ -1,43 +1,135 @@
 // Do not change any of the function names
 
-function makeCat(name, age) {
-  // create a new object with a name property with the value set to the name argument
-  // add an age property to the object with the value set to the age argument
-  // add a method called meow that returns the string 'Meow!'
-  // return the object
-}
 
-function addProperty(object, property) {
-  // add the property to the object with a value of null
+const makeCat = {
+  name:'Sam',
+  age:'50',
+  meow: function (){
+    return 'Meow!';
+  }
+  };
+  makeCat.meow();
+     // create a new object with a name property with the value set to the name argument
+    // add an age property to the object with the value set to the age argument
+   // add a method called meow that returns the string 'Meow!'
   // return the object
-  // note: the property name is NOT 'property'.  The name is the value of the argument called property (a string)
-}
 
-function invokeMethod(object, method) {
-  // method is a string that contains the name of a method on the object
+
+ 
+   
+  const object = {
+    x: 1,
+    y: 2,
+  };
+  const updatedObject = {
+    x: 1,
+    y: 2,
+    z: null,
+  };
+  function addProperty() {
+  return updatedObject;
+  }
+  addProperty(object, 'z');
+     // add the property to the object with a value of null
+    // return the object
+   // note: the property name is NOT 'property'.
+  //The name is the value of the argument called property (a string)
+
+
+
+ 
+  invokeMethod(object, method),
+  function() {
+    const object = {
+      x: 0,
+      incrementX: function(){
+        this.x++;
+      }
+    };
+    invokeMethod(object, 'incrementX');
+  };
+   // method is a string that contains the name of a method on the object
   // invoke this method
-  // nothing needs to be returned
-}
+ // nothing needs to be returned
 
-function multiplyMysteryNumberByFive(mysteryNumberObject) {
-  // mysteryNumberObject has a property called mysteryNumber
-  // multiply the mysteryNumber property by 5 and return the product
-}
 
-function deleteProperty(object, property) {
-  // remove the property from the object
-  // return the object
-}
 
-function newUser(name, email, password) {
-  // create a new object with properties matching the arguments passed in.
+ const mysteryNumberObject = {
+  mysteryNumber: -5,
+  multiplyMysteryNumberByFive: function(){
+  return this.mysteryNumber * 5;
+  }
+  };
+mysteryNumberObject.multiplyMysteryNumberByFive();
+ // mysteryNumberObject has a property called mysteryNumber
+// multiply the mysteryNumber property by 5 and return the product
+
+
+
+const object = {
+  x: 1,
+  y: 2,
+  z: null,
+};
+  function deleteProperty () {
+    delete object.z;
+    return object;
+  }
+    deleteProperty();
+
+
+
+
+    const user = {
+      name: 'Ben',
+      email: 'ben@lambdaschool.com',
+      password: 'correcthorsebatterystaple',
+    };
+  
+    const user2 = {
+      name: 'Austen',
+      email: 'austen@lambdaschool.com',
+      password: 'password',
+   };
+    
+    function newUser(name, email, password) {
+     const newUser = user2;
+     return newUser;
+    }
+      newUser(user2.name, user2.email, user2.password);
+   
+   // create a new object with properties matching the arguments passed in.
   // return the new object
-}
 
-function hasEmail(user) {
-  // return true if the user has a value for the property 'email'
-  // otherwise return false
-}
+
+
+
+  user1 = {
+    username: 'SunJieMing',
+    email: 'ben@lambdaschool.com',
+  };
+  
+  user2 = {
+    username: 'Austen',
+    email: null,
+  };
+  
+  user3 = {
+    username: 'Ryan',
+  };
+  
+   function hasEmail() {
+     if (user3.email){
+      return true;
+    } else 
+      return false;
+   }
+     hasEmail(user3);
+  
+   // return true if the user has a value for the property 'email'
+   // otherwise return false
+
+
 
 function hasProperty(object, property) {
   // return true if the object has the value of the property argument
@@ -56,11 +148,20 @@ function updatePassword(user, newPassword) {
   // return the object
 }
 
+
 function addFriend(user, newFriend) {
+  const user = { 
+    friends: ['Ben', 'Austen', 'Ryan', 'Mike', 'Young'], 
+  }; 
+  console.log(user);
+         newFriend = user.push('Shay');
+ 
+     return user;
+   }  
   // user has a property called friends that is an array
   // add newFriend to the end of the friends array
   // return the user object
-}
+
 
 function setUsersToPremium(users) {
   // users is an array of user objects.
