df430afdde60891beed257df43bb7a79e511738d
rikilamadrid <riki.lamadrid@gmail.com>
2018-04-10 14:45:44 -0500

    hm5

--- Lesson05-JS-II/homework/homework.js
+++ Lesson05-JS-II/homework/homework.js
@@ -3,6 +3,13 @@
 function getBiggest(x, y) {
   // x and y are integers.  Return the larger integer
   // if they are the same return either one
+  if (x > y) {
+    return x;
+  } else if (x < y){
+    return y;
+  } else {
+    return x || y;
+  }
 }
 
 function greeting(language) {
@@ -11,16 +18,36 @@ function greeting(language) {
   // language: 'Mandarin' -> 'Ni Hao!'
   // language: 'Spanish' -> 'Hola!'
   // if language is undefined return 'Hello!'
+  switch (language) {
+    case 'German':
+      return 'Guten Tag!';
+    case 'Mandarin':
+      return 'Ni Hao!';
+    case 'Spanish':
+      return 'Hola!';
+    default:
+      return 'Hello!';
+  }
 }
 
 function isTenOrFive(num) {
   // return true if num is 10 or 5
   // otherwise return false
+  if(num === 10 || num === 5) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function isInRange(num) {
   // return true if num is less than 50 and greater than 20
   // otherwise return false
+  if (num < 50 && num > 20) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function isInteger(num) {
@@ -30,6 +57,11 @@ function isInteger(num) {
   // -10 -> true
   // otherwise return false
   // hint: you can solve this using Math.floor
+  if(Number.isInteger(num)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function fizzBuzz(num) {
@@ -37,6 +69,14 @@ function fizzBuzz(num) {
   // if num is divisible by 5 return 'buzz'
   // if num is divisible by 3 & 5 return 'fizzbuzz'
   // otherwise return num
+  if (num % 3 === 0 && num % 5 === 0) {
+    return 'fizzbuzz';
+  } else if (num % 3 === 0) {
+    return 'fizz';
+  } else if (num % 5 ===0) {
+    return 'buzz';
+  }
+  return num;
 }
 
 function isPrime(num) {
@@ -45,6 +85,20 @@ function isPrime(num) {
   // hint: a prime number is only evenly divisible by itself and 1
   // hint2: you can solve this using a for loop
   // note: 0 and 1 are NOT considered prime numbers
+  var sqrt = Math.sqrt(num);
+  
+  if (num <= 1){
+    return false;
+  } else if (num === 2) {
+    return true;
+  }
+
+  for (let i = 2; i <= sqrt; i++) {
+    if(num % i === 0) {
+      return false;
+    }
+    return num !== 1;
+  }
 }
 
 
