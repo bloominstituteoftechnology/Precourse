8263c7a2f0e0b0637d876a944e77a93916f22402
RobinNWilliams <WilliamsRob45@gmail.com>
2018-04-11 17:34:14 -0500

    passed!

--- Lesson6-JS-III/homework/homework.js
+++ Lesson6-JS-III/homework/homework.js
@@ -1,6 +1,6 @@
 // Do not change any of the function names
 
-const arr = ['Brynne','Lily', 'Whimsy'];
+
 function returnFirst(arr){ 
   return arr[0];
 }
@@ -8,7 +8,7 @@ function returnFirst(arr){
  
 
 
- let arr =['Brynne','Lily','Whimsy'];
+ 
   function returnLast(arr){ 
     return arr[arr.length -1];
   }
@@ -17,7 +17,7 @@ function returnFirst(arr){
   
 
 
-const arr = ['Brynnne', 'Lily', 'Whimsy'];
+
 function getArrayLength(arr) { 
   return arr.length;  
   }
@@ -30,104 +30,98 @@ function incrementByOne(arr) {
   // increase each integer by one
   // return the array
  for (let i = 0; i < arr.length; i++){
- arr[i] = arr[i] +1;
+    arr[i] += 1 ;
 }
  return arr;
 }
   
-  const arr = ['Lily','EV','Emmy'];
-  var item = 'Simka';
-  arr.push(item);
+ 
+  
 function addItemToArray(arr, item) {
+  arr.push(item);
   return arr; 
 }  
   
   // add the item to the end of the array
   // return the array
 
-  const arr =['Bloodhound','BT Coonhound','Great Dane'];
-  let item = 'Chihuahua';
-  arr.unshift(item);
+ 
+ 
+ 
 function addItemToFront(arr, item) {
+  arr.unshift(item);
   return arr;
 }
   // add the item to the front of the array
   // return the array
   // hint: use the array method .unshift
  
-
-
-  const words =['Hello', 'world'];
-  let newWords = words.concat(['it', 'is', 'a', 'beautiful', 'day.']);
-function wordsToSentence(words) { 
-  return newWords;
+  function wordsToSentence(words) { 
+  let newWords = words.join(' ');
+  return newWords; 
   // words is an array of strings
   // return a string that is all of the words concatenated together
   // spaces need to be between each word
   // example: ['Hello', 'world!'] -> 'Hello world!'
 }
 
-let arr =['Red', 'Blue', 'Green', 'Orange'];
+function contains(arr, item) {
+  i = 0, count = arr.length, matchFound = false;
 
-function contains(arr, item) { 
-  return arr.some(function(arrItem){
-    return item === arrItem;
-  });
-  
+ for(; i < count; i++) {
+     if (arr[i] === item) {
+         matchFound = true;
+     }
+ }
+ return matchFound;
 }
-contains(arr, 'Orange');
-contains(arr, 'Purple');
-  // check to see if item is inside of arr
-  // return true if it is, otherwise
-  
-   return false
 
 
-let numbers =[5, 7, 8, 10];
-let  total = 0;
+
 function addNumbers(numbers){
-  for (let i = 0; i < numbers.length; i++);
-  return total += numbers[i];
-   
-}
-  // numbers is an array of integers.
+  let total = 0;
+  for (let i = 0; i < numbers.length; i++){
+     total += numbers[i];
+  }
+     return total;
+  }// numbers is an array of integers.
   // add all of the integers and return the value
 
-
-
-
-let testScores = [100, 80, 90, 75];
-let sum = 0;
-
+  
 function averageTestScore(testScores) {
+  let sum = 0;
 	for (let i = 0; i < testScores.length; i++) {
-		sum += testScores[i];
+    sum += testScores[i];
+  }
 		return sum / testScores.length;
 	} // testScores is an array.  Iterate over testScores and compute the average.
 	// return the average
-}
 
 
-let numbers = [0, 5, 10, 9];
 function largestNumber(numbers) {
   return Math.max.apply(null, numbers);
   // numbers is an array of integers
   // return the largest integer
 }
 
-let product = 0;
 function multiplyArguments() {
-  console.log(arguments[3]);{ 
-  if (arguments[0]);
-  return 0;
+  let product = 1;
+  if (arguments.length === 0) {
+    return 0;
+   }
+  if (arguments.length === 1) {
+    return arguments[0];
   }
-  for(let i=0; i <arguments.length; i ++);{ product +=(i * product);
+for (let i = 0; i < arguments.length; i++) { 
+        product *=  arguments[i];
+  }
+  return product; 
 }
-  console.log(product);
+ 
   // use the arguments keyword to multiply all of the arguments together and return the product
   // if no arguments are passed in return 0
   // if one argument is passed in just return it
-}
+
 
 // Do not modify code below this line.
 // --------------------------------
