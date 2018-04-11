732349db33f1c43a1c677a89487cd2d78b7b716f
Jordan Rowland <36084892+Jordan-Rowland@users.noreply.github.com>
2018-04-10 19:09:20 -0700

    Homework lessons 4, 5, 6

--- Lesson04-JS-I/homework/homework.js
+++ Lesson04-JS-I/homework/homework.js
@@ -54,86 +54,86 @@ function divide(x, y) {
 function areEqual(x, y) {
   // return true if x and y are the same
   // otherwise return false
-  return x == y
+  return x == y;
 }
 
 function areSameLength(str1, str2) {
   // return true if the two strings have the same length
   // otherwise return false
-  return str1.length == str2.length
+  return str1.length == str2.length;
 }
 
 function lessThanNinety(num) {
   // return true if the function argument: num , is less than ninety
   // otherwise return false
-  return num < 90
+  return num < 90;
 }
 
 function greaterThanFifty(num) {
   // return true if num is greater than fifty
   // otherwise return false
-  return num > 50
+  return num > 50;
 }
 
 function getRemainder(x, y) {
   // return the remainder from dividing x by y
-  return x % y
+  return x % y;
 }
 
 function isEven(num) {
   // return true if num is even
   // otherwise return false
-  return num % 2 == 0
+  return num % 2 == 0;
 }
 
 function isOdd(num) {
   // return true if num is odd
   // otherwise return false
-  return num % 2 != 0
+  return num % 2 != 0;
 }
 
 function square(num) {
   // square num and return the new value
   // hint: NOT square root!
-  return num * num
+  return num * num;
 }
 
 function cube(num) {
   // cube num and return the new value
-  return num * num * num
+  return num * num * num;
 }
 
 function raiseToPower(num, exponent) {
   // raise num to whatever power is passed in as exponent
-  return Math.pow(num, exponent)
+  return Math.pow(num, exponent);
 }
 
 function roundNumber(num) {
   // round num and return it
-  return Math.round(num)
+  return Math.round(num);
 }
 
 function roundUp(num) {
   // round num up and return it
-  return Math.ceil(num)
+  return Math.ceil(num);
 }
 
 function addExclamationPoint(str) {
   // add an exclamation point to the end of str and return the new string
   // 'hello world' -> 'hello world!'
-  return (str + '!')
+  return (str + '!');
 }
 
 function combineNames(firstName, lastName) {
   // return firstName and lastName combined as one string and separated by a space.
   // 'Lambda', 'School' -> 'Lambda School'
-  return (firstName + ' ' + lastName)
+  return (firstName + ' ' + lastName);
 }
 
 function getGreeting(name) {
   // Take the name string and concatenate other strings onto it so it takes the following form:
   // 'Sam' -> 'Hello Sam!'
-  return ('Hello ' + name + '!')
+  return ('Hello ' + name + '!');
 }
 
 // The next three questions will have you implement math area formulas. 
@@ -141,12 +141,12 @@ function getGreeting(name) {
  
 function getRectangleArea(length, width) {
   // return the area of the rectangle by using length and width
-  return length * width
+  return length * width;
 }
 
 function getTriangleArea(base, height) {
   // return the area of the triangle by using base and height
-  return .5 * base * height
+  return .5 * base * height;
 }
 
 // Do not modify code below this line.
