2fcb51732b0cd7839a9b208dce848802e48a9d5e
Michael-Real <mreal126@protonmail.com>
2018-04-12 14:36:35 -0400

    Update homework.js

--- Lesson04-JS-I/homework/homework.js
+++ Lesson04-JS-I/homework/homework.js
@@ -1,22 +1,22 @@
 //In these first 6 questions, replace `null` with the answer
 
 //create a string variable, it can contain anything
-const newString = 'Mike' ;
+const newString = 'Mike';
 
 //create a number variable, it an be any number
-const newNum = 40 ;
+const newNum = 40;
 
 //create a boolean variable
-const newBool = true ;
+const newBool = true;
 
 //solve the following math problem
 const newSubtract = 10 - 5 === 5;
 
 //Solve the following math problem
-const newMultiply = 10 * 4 === 40 ;
+const newMultiply = 10 * 4 === 40;
 
 //Solve the following math problem:
-const newModulo = 21 % 5 === 1 ;
+const newModulo = 21 % 5 === 1;
 
 
 
@@ -27,7 +27,7 @@ const newModulo = 21 % 5 === 1 ;
 
 function returnString(str) {
   //simply return the string provided: str
-  return (str);
+  return str;
 }
 
 function add(x, y) {
@@ -58,16 +58,17 @@ function divide(x, y) {
 function areEqual(x, y) {
   // return true if x and y are the same
   // otherwise return false
-  if (x == y) {
+  if x == y {
     return true;
   }
+  
   return false;
 }
 
 function areSameLength(str1, str2) {
   // return true if the two strings have the same length
   // otherwise return false
-  if (str1.length() === str2.length()) {
+  if (str1.length === str2.length) {
     return true;
   }
   return false;
@@ -99,7 +100,7 @@ function getRemainder(x, y) {
 function isEven(num) {
   // return true if num is even
   // otherwise return false
-  if (num % 2 === 0) {
+  if(num % 2 === 0) {
     return true;
   }
   return false;
@@ -108,7 +109,7 @@ function isEven(num) {
 function isOdd(num) {
   // return true if num is odd
   // otherwise return false
-  if (num % 2 === 1) {
+  if(num % 2 === 1) {
     return true;
   }
   return false;
@@ -117,12 +118,14 @@ function isOdd(num) {
 function square(num) {
   // square num and return the new value
   // hint: NOT square root!
-  return Math.pow(num, 2);
+  const squared = Math.pow(num, 2);
+  return squared;
 }
 
 function cube(num) {
   // cube num and return the new value
-  return Math.pow(num, 3);
+  const cubed = Math.pow(num, 3);
+  return cubed;
 }
 
 function raiseToPower(num, exponent) {
@@ -143,20 +146,21 @@ function roundUp(num) {
 function addExclamationPoint(str) {
   // add an exclamation point to the end of str and return the new string
   // 'hello world' -> 'hello world!'
-  return (str + '!');
+  const ex = 'str' + '!';
+  return ex;
 }
 
 function combineNames(firstName, lastName) {
   // return firstName and lastName combined as one string and separated by a space.
   // 'Lambda', 'School' -> 'Lambda School'
-  const name = (firstName + ' ' + lastName);
+  const name = ('firstName' + ' ' + 'lastName');
   return name;
 }
 
 function getGreeting(name) {
   // Take the name string and concatenate other strings onto it so it takes the following form:
   // 'Sam' -> 'Hello Sam!'
-  const greeting = ('Hello ' + name + '!');
+  const greeting = 'Hello ' + 'name' + '!';
   return greeting;
 }
 
