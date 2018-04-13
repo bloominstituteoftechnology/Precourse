034e8993ad7310e85a9430ca2bb734952f609976
karlie0 <kamabrapa88@gmail.com>
2018-04-12 23:34:48 -0400

    finished lesson 8

--- Lesson08-JS-V/homework/homework.js
+++ Lesson08-JS-V/homework/homework.js
@@ -1,17 +1,33 @@
 // Do not change any of the function names
 
-function createUserClass() {
+function createUserClass(options) {
   // create an ES6 class or a constructor function called User
   // it should accept an options object with username, name, email, and password properties
   // in the constructor set the username, name, email, and password properties
   // the constructor should have a method 'sayHi' on its prototype that returns the string 'Hello, my name is {{name}}'
   // {{name}} should be the name set on each instance
   // return the class
+  function User(options){
+    this.username = options.username;
+    this.name = options.name;
+    this.email = options.email;
+    this.password = options.password;
+
+  }
+
+  User.prototype.sayHi = function(){
+    return 'Hello, my name is '+ this.name;
+  };
+
+  return User;
 }
 
 function addPrototypeMethod(Constructor) {
   // add a method to the constructor's prototype
   // the method should be called 'sayHi' and should return the string 'Hello World!'
+  Constructor.prototype.sayHi = function(){
+    return 'Hello World!';
+  };
 }
 
 function addReverseString() {
@@ -19,6 +35,15 @@ function addReverseString() {
   // name this method reverse
   // hint:
   // you will need to use 'this' inside of reverse
+
+  String.prototype.reverse = function() {
+    let newString = '';
+    for(let i=0; i < this.length; i++ ){
+      newString = newString + this[this.length-1-i];
+    }
+    return newString;
+  };
+
 }
 
 // Do not modify code below this line.
