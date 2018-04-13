bb8546304f349f452f8fcbafd6b7a2994dce17ce
cmok4290 <cmok4290@gmail.com>
2018-04-12 20:10:56 -0400

    added file, Lesson07 homework.js

--- Lesson07-JS-IV/homework/homework.js
+++ Lesson07-JS-IV/homework/homework.js
@@ -5,61 +5,94 @@ function makeCat(name, age) {
   // add an age property to the object with the value set to the age argument
   // add a method called meow that returns the string 'Meow!'
   // return the object
+  const catObj = {
+    name: name,
+    age: age,
+    meow: () => {return 'Meow!';}};
+  return catObj;
 }
 
 function addProperty(object, property) {
   // add the property to the object with a value of null
   // return the object
   // note: the property name is NOT 'property'.  The name is the value of the argument called property (a string)
+  const addProp = property;
+  object[addProp] = null;
+  return object;
 }
 
 function invokeMethod(object, method) {
   // method is a string that contains the name of a method on the object
   // invoke this method
   // nothing needs to be returned
+  const addMethod = method;
+  object[addMethod]();
 }
 
 function multiplyMysteryNumberByFive(mysteryNumberObject) {
   // mysteryNumberObject has a property called mysteryNumber
   // multiply the mysteryNumber property by 5 and return the product
+  return mysteryNumberObject.mysteryNumber * 5;
 }
 
 function deleteProperty(object, property) {
   // remove the property from the object
   // return the object
+  
+  delete object[property];
+  return object;
 }
 
 function newUser(name, email, password) {
   // create a new object with properties matching the arguments passed in.
   // return the new object
+  const userObj = {name: name, email: email, password: password};
+  return userObj;
 }
 
 function hasEmail(user) {
   // return true if the user has a value for the property 'email'
   // otherwise return false
+  if (user.email)
+    return true;
+  return false;
 }
 
 function hasProperty(object, property) {
   // return true if the object has the value of the property argument
   // property is a string
   // otherwise return false
+  for (let i in object) {
+    if (i.toString() === property)
+      return true;
+  }
+  return false;
 }
 
 function verifyPassword(user, password) {
   // check to see if the provided password matches the password property on the user object
   // return true if they match
   // otherwise return false
+  if (user.password === password)
+    return true;
+  return false;
 }
 
 function updatePassword(user, newPassword) {
   // replace the existing password on the user object with the value of newPassword
   // return the object
+  user.password = newPassword;
+  return user;
 }
 
 function addFriend(user, newFriend) {
   // user has a property called friends that is an array
   // add newFriend to the end of the friends array
   // return the user object
+  for (let key in user) {
+    user[key].push(newFriend);
+  }
+  return user;
 }
 
 function setUsersToPremium(users) {
@@ -67,6 +100,10 @@ function setUsersToPremium(users) {
   // each user object has the property 'isPremium'
   // set each user's isPremium property to true
   // return the users array
+  for (let i = 0; i < users.length; i++) {
+    users[i].isPremium = true;
+  }
+  return users;
 }
 
 function sumUserPostLikes(user) {
@@ -75,6 +112,11 @@ function sumUserPostLikes(user) {
   // each post object has an integer property called 'likes'
   // sum together the likes from all the post objects
   // return the sum
+  let numLikes = 0;
+  for (let i in user.posts) {
+    numLikes += user.posts[i].likes;
+  }
+  return numLikes;
 }
 
 function addCalculateDiscountPriceMethod(storeItem) {
@@ -86,6 +128,9 @@ function addCalculateDiscountPriceMethod(storeItem) {
   // price -> 20
   // discountPercentage -> .2
   // discountPrice = 20 - (20 * .2)
+  storeItem.calculateDiscountPrice = () => {
+  return storeItem.price - (storeItem.price * storeItem.discountPercentage);};
+  return storeItem;
 }
 
 // Do not modify code below this line.
