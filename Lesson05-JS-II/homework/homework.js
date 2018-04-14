6c5539de8ec14bd0b46ba0323afa05909666825f
sean chavez <seanchavez@seanchavezsMBP.home>
2018-04-14 15:11:04 -0700

    modified homework.js, added feynman .txt

--- Lesson05-JS-II/homework/homework.js
+++ Lesson05-JS-II/homework/homework.js
@@ -3,9 +3,23 @@
 function getBiggest(x, y) {
   // x and y are integers.  Return the larger integer
   // if they are the same return either one
+  if (x === y || x > y) {
+    return x;
+  } else {
+    return y;
+  }
 }
 
 function greeting(language) {
+  if (language === 'German') {
+    return 'Guten Tag!';
+  } else if (language === 'Mandarin') {
+    return 'Ni Hao!';
+  } else if (language === 'Spanish') {
+    return 'Hola!';
+  } else {
+    return 'Hello!';
+  }
   // return a greeting for three different languages:
   // language: 'German' -> 'Guten Tag!'
   // language: 'Mandarin' -> 'Ni Hao!'
@@ -14,16 +28,20 @@ function greeting(language) {
 }
 
 function isTenOrFive(num) {
+  return num === 10 || num === 5;
   // return true if num is 10 or 5
   // otherwise return false
 }
 
 function isInRange(num) {
+  return num > 20 && num < 50;
   // return true if num is less than 50 and greater than 20
   // otherwise return false
 }
 
-function isInteger(num) {
+function isInteger(num) {  
+  return Number.isInteger(num);
+
   // return true if num is an integer
   // 0.8 -> false
   // 1 -> true
@@ -33,6 +51,18 @@ function isInteger(num) {
 }
 
 function fizzBuzz(num) {
+let fizzBuzz = '';
+if (num % 3 === 0) {
+  fizzBuzz += 'fizz';
+}
+if (num % 5 === 0) {
+  fizzBuzz += 'buzz';
+}
+if (fizzBuzz) {
+  return fizzBuzz;
+} else {
+  return num;
+}
   // if num is divisible by 3 return 'fizz'
   // if num is divisible by 5 return 'buzz'
   // if num is divisible by 3 & 5 return 'fizzbuzz'
@@ -40,6 +70,12 @@ function fizzBuzz(num) {
 }
 
 function isPrime(num) {
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return num > 1;
   // return true if num is prime.
   // otherwise return false
   // hint: a prime number is only evenly divisible by itself and 1
