af6d71696d1421140024526f712def31f1a9643c
sean chavez <seanchavez@seanchavezsMBP.home>
2018-04-15 10:48:06 -0700

    modified homework.js, added feynman.txt

--- Lesson08-JS-V/homework/homework.js
+++ Lesson08-JS-V/homework/homework.js
@@ -1,6 +1,16 @@
 // Do not change any of the function names
 
 function createUserClass() {
+  function User(options) {
+    this.username = options.username;
+    this.name = options.name;
+    this.email = options.email;
+    this.password = options.password;
+    this.sayHi = function(name) {
+      return 'Hello my name is ' + name;
+    };
+  }
+  return User;
   // create an ES6 class or a constructor function called User
   // it should accept an options object with username, name, email, and password properties
   // in the constructor set the username, name, email, and password properties
@@ -10,11 +20,21 @@ function createUserClass() {
 }
 
 function addPrototypeMethod(Constructor) {
+  Constructor.prototype.sayHi = function() {
+    return 'Hello World!';
+  };
   // add a method to the constructor's prototype
   // the method should be called 'sayHi' and should return the string 'Hello World!'
 }
 
 function addReverseString() {
+  String.prototype.reverse = function() {
+    let revString = '';
+    for (let i = this.length - 1; i >= 0; i--) {
+      revString += this[i];
+    }
+    return revString;
+  };
   // add a method to the string constructor's prototype that returns a reversed copy of the string
   // name this method reverse
   // hint:
