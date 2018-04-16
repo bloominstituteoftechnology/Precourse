c564244d16c6bc96a36724f7541d14a4053d3ff7
Samuel Kirkpatrick <kirksamalpha@gmail.com>
2018-04-16 02:03:28 -0500

    Uploaded Lesson8

--- Lesson8-JS-V/homework/homework.js
+++ Lesson8-JS-V/homework/homework.js
@@ -1,25 +1,50 @@
 // Do not change any of the function names
 
-function createUserClass() {
-  // create an ES6 class or a constructor function called User
-  // it should accept an options object with username, name, email, and password properties
-  // in the constructor set the username, name, email, and password properties
-  // the constructor should have a method 'sayHi' on its prototype that returns the string 'Hello, my name is {{name}}'
-  // {{name}} should be the name set on each instance
-  // return the class
-}
 
-function addPrototypeMethod(Constructor) {
-  // add a method to the constructor's prototype
-  // the method should be called 'sayHi' and should return the string 'Hello World!'
-}
+const User = function (username, name, email, password){
+  this.username = username;
+  this.name = name;
+  this.email = email;
+  this.password = password;
+};
+const user = new User('SunJieMing', 'Ben', 'ben@lambdaschool.com', 'LS Rocks!');
+console.log(user);
 
-function addReverseString() {
-  // add a method to the string constructor's prototype that returns a reversed copy of the string
+
+
+const User = function (username, name, email, password){
+  this.username = username;
+  this.name = name;
+  this.email = email;
+  this.password = password;
+  this.intro = 'Hello World!';
+};
+User.prototype.sayHi = function(){
+  this.intro = 'Hello World!';
+};
+const user = new User('SunJieMing', 'Ben', 'ben@lambdaschool.com', 'LS Rocks!', 'Hello World!');
+console.log(user);
+ //add a method to the constructor's prototype
+//the method should be called 'sayHi' and should return the string 'Hello World!'
+
+
+
+const User = function (username, name, email, password){
+  this.username = username;
+  this.name = name;
+  this.email = email;
+  this.password = password;
+  this.introRev = '!dlroW olleH';
+};
+  User.prototype.reverse = function(){
+  this.introRev = '!dlroW olleH';
+};
+const user = new User('SunJieMing', 'Ben', 'ben@lambdaschool.com', 'LS Rocks!', '!dlroW olleH');
+    console.log(user);
+   // add a method to the string constructor's prototype that returns a reversed copy of the string    
   // name this method reverse
-  // hint:
-  // you will need to use 'this' inside of reverse
-}
+ // hint:
+// you will need to use 'this' inside of reverse
 
 // Do not modify code below this line.
 // --------------------------------
