21bb4d6eb06a745a29ca3bce08944c98acd4c718
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-04-16 14:25:57 -0400

    Passed test

--- Lesson05-JS-II/homework/homework.js
+++ Lesson05-JS-II/homework/homework.js
@@ -3,6 +3,8 @@
 function getBiggest(x, y) {
   // x and y are integers.  Return the larger integer
   // if they are the same return either one
+  if (x > y) return x;
+  else return y;
 }
 
 function greeting(language) {
@@ -11,16 +13,37 @@ function greeting(language) {
   // language: 'Mandarin' -> 'Ni Hao!'
   // language: 'Spanish' -> 'Hola!'
   // if language is undefined return 'Hello!'
+  switch(language){
+    case 'German':
+    return 'Guten Tag!';
+
+    case 'Mandarin':
+    return 'Ni Hao!';
+
+    case 'Spanish':
+    return 'Hola!';
+
+    default:
+    return 'Hello!';
+  }
 }
 
 function isTenOrFive(num) {
   // return true if num is 10 or 5
   // otherwise return false
+  if (num === 5 || num === 10) return true;
+  else return false;
 }
 
 function isInRange(num) {
   // return true if num is less than 50 and greater than 20
   // otherwise return false
+  if (num < 50 && num > 20) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 function isInteger(num) {
@@ -30,6 +53,7 @@ function isInteger(num) {
   // -10 -> true
   // otherwise return false
   // hint: you can solve this using Math.floor
+  return Math.floor(num) === num;
 }
 
 function fizzBuzz(num) {
@@ -37,6 +61,10 @@ function fizzBuzz(num) {
   // if num is divisible by 5 return 'buzz'
   // if num is divisible by 3 & 5 return 'fizzbuzz'
   // otherwise return num
+  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+  else if (num % 3 === 0) return 'fizz';
+  else if (num % 5 === 0) return 'buzz';
+  else return num;
 }
 
 function isPrime(num) {
@@ -45,6 +73,12 @@ function isPrime(num) {
   // hint: a prime number is only evenly divisible by itself and 1
   // hint2: you can solve this using a for loop
   // note: 0 and 1 are NOT considered prime numbers
+  if (num <= 1) return false;
+
+  for (let i=2; i < num; i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
 }
 
 
