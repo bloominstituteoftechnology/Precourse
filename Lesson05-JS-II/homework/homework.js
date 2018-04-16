8db3485318cece8ba55d67a0b180cabd66a34c04
Michael-Real <mreal126@protonmail.com>
2018-04-16 13:08:39 -0400

    Update homework.js

--- Lesson05-JS-II/homework/homework.js
+++ Lesson05-JS-II/homework/homework.js
@@ -4,11 +4,11 @@ function getBiggest(x, y) {
   // x and y are integers.  Return the larger integer
   // if they are the same return either one
   if (x > y) {
-    return x};
-    else if (y > x) {
-    return y};
-    else if (x == y) {
-    return x || y}
+    return x}
+  else if (y > x) {
+  return y}
+  else if (x == y) {
+  return x || y}
 }
 
 function greeting(language) {
@@ -17,12 +17,12 @@ function greeting(language) {
   // language: 'Mandarin' -> 'Ni Hao!'
   // language: 'Spanish' -> 'Hola!'
   // if language is undefined return 'Hello!'
-  if language = German {
-    return 'Guten Tag!'};
-    else if language = Mandarin {
-    return 'Ni Hao!'};
-    else if language = Spanish {
-    return 'Hola!'};
+  if language = 'German' {
+    return 'Guten Tag!'}
+    else if language = 'Mandarin' {
+    return 'Ni Hao!'}
+    else if language = 'Spanish' {
+    return 'Hola!'}
     else if language = undefined {
     return 'Hello!'}
 }
@@ -31,15 +31,15 @@ function isTenOrFive(num) {
   // return true if num is 10 or 5
   // otherwise return false
   if (num == 10 || num == 5) {
-    return true};
+    return true}
     else {return false}
 }
 
 function isInRange(num) {
   // return true if num is less than 50 and greater than 20
   // otherwise return false
-  if num < 50 && num > 20 {
-    return true};
+  if (num < 50 && num > 20) {
+    return true}
     else {
     return false}
 }
@@ -52,7 +52,7 @@ function isInteger(num) {
   // otherwise return false
   // hint: you can solve this using Math.floor
   if (num == Math.floor(num)) {
-    return true};
+    return true}
     else {
     return false}
 }
@@ -63,11 +63,11 @@ function fizzBuzz(num) {
   // if num is divisible by 3 & 5 return 'fizzbuzz'
   // otherwise return num
   if (num % 3 == 0) {
-    return 'fizz'};
+    return 'fizz'}
     else if (num % 5 == 0) {
-    return 'buzz'};
+    return 'buzz'}
     else if (num % 3 == 0 && num % 5 == 0) {
-    return 'fizzbuzz'};
+    return 'fizzbuzz'}
     else {
     return num}
 }
@@ -80,9 +80,9 @@ function isPrime(num) {
   // note: 0 and 1 are NOT considered prime numbers
   for (i = 2 ; i <= num / 2 ; i++) {
     if (n % i == 0) 
-      return false};
+      return false}
       else {
-      return true};
+      return true}
 }
 
 
