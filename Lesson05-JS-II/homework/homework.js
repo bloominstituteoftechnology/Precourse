4b44d74d62afaa39e4f0d3e5936ae81b3547cf50
Michael-Real <mreal126@protonmail.com>
2018-04-16 14:34:15 -0400

    Update homework.js

--- Lesson05-JS-II/homework/homework.js
+++ Lesson05-JS-II/homework/homework.js
@@ -4,11 +4,11 @@ function getBiggest(x, y) {
   // x and y are integers.  Return the larger integer
   // if they are the same return either one
   if (x > y) {
-  return x}
+  return x};
   else if (y > x) {
-  return y}
+  return y};
   else if (x == y) {
-  return x || y}
+  return x || y};
 }
 
 function greeting(language) {
@@ -26,7 +26,7 @@ function greeting(language) {
     return 'Hello!';
   }
   else if (!(language in languages)){
-    return 'Hello!'
+    return 'Hello!';
   }
   else {
     return languages[language];
@@ -37,17 +37,17 @@ function isTenOrFive(num) {
   // return true if num is 10 or 5
   // otherwise return false
   if (num == 10 || num == 5) {
-    return true}
-    else {return false}
+    return true};
+    else {return false};
 }
 
 function isInRange(num) {
   // return true if num is less than 50 and greater than 20
   // otherwise return false
   if (num < 50 && num > 20) {
-    return true}
+    return true};
     else {
-    return false}
+    return false};
 }
 
 function isInteger(num) {
@@ -58,9 +58,9 @@ function isInteger(num) {
   // otherwise return false
   // hint: you can solve this using Math.floor
   if (num == Math.floor(num)) {
-    return true}
+    return true};
     else {
-    return false}
+    return false};
 }
 
 function fizzBuzz(num) {
@@ -69,12 +69,12 @@ function fizzBuzz(num) {
   // if num is divisible by 3 & 5 return 'fizzbuzz'
   // otherwise return num
   if (num % 3 === 0 && num % 5 === 0) {
-    return 'fizzbuzz'}
+    return 'fizzbuzz'};
   else if (num % 5 === 0) {
-    return 'buzz'}
+    return 'buzz'};
   else if (num % 3 === 0) {
-    return 'fizz'}
-  else {return num}
+    return 'fizz'};
+  else {return num};
 }
 
 function isPrime(num) {
@@ -83,10 +83,11 @@ function isPrime(num) {
   // hint: a prime number is only evenly divisible by itself and 1
   // hint2: you can solve this using a for loop
   // note: 0 and 1 are NOT considered prime numbers
-  for (i = 2 ; i <= num / 2 ; i++) {
-    if (num % i === 0) 
-      return false}
-    return true
+  if (num === 0 || num === 1) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;          
+  }
+  return  true;
 }
 
 // Do not modify code below this line.
