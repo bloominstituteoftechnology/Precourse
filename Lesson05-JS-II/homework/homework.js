16bb8d0be2b3d3472004a085e320385168b601eb
Michael-Real <mreal126@protonmail.com>
2018-04-16 15:03:50 -0400

    Update homework.js

--- Lesson05-JS-II/homework/homework.js
+++ Lesson05-JS-II/homework/homework.js
@@ -4,11 +4,11 @@ function getBiggest(x, y) {
   // x and y are integers.  Return the larger integer
   // if they are the same return either one
   if (x > y) {
-    return x}
-  else if (y > x) {
-    return y}
-  else if (x == y) {
-    return x || y}
+    return x;
+  } else if (y > x) {
+    return y;
+  } else if (x === y) 
+    return x || y;
 }
 
 function greeting(language) {
@@ -37,17 +37,20 @@ function isTenOrFive(num) {
   // return true if num is 10 or 5
   // otherwise return false
   if (num == 10 || num == 5) {
-    return true}
-    else {return false}
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function isInRange(num) {
   // return true if num is less than 50 and greater than 20
   // otherwise return false
   if (num < 50 && num > 20) {
-    return true}
-    else {
-    return false}
+    return true;
+  } else {
+    return false;
+}
 }
 
 function isInteger(num) {
@@ -58,9 +61,10 @@ function isInteger(num) {
   // otherwise return false
   // hint: you can solve this using Math.floor
   if (num == Math.floor(num)) {
-    return true}
-    else {
-    return false}
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function fizzBuzz(num) {
@@ -69,12 +73,13 @@ function fizzBuzz(num) {
   // if num is divisible by 3 & 5 return 'fizzbuzz'
   // otherwise return num
   if (num % 3 === 0 && num % 5 === 0) {
-    return 'fizzbuzz'}
-  else if (num % 5 === 0) {
-    return 'buzz'}
-  else if (num % 3 === 0) {
-    return 'fizz'}
-  else {return num}
+    return 'fizzbuzz';
+  } else if (num % 5 === 0) {
+    return 'buzz';
+  } else if (num % 3 === 0) {
+    return 'fizz';
+  } else {return num;
+  }
 }
 
 function isPrime(num) {
