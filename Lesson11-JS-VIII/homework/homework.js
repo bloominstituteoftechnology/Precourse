a58ddd6dce226fa58651be22b23c51f96eaf95e2
RobinNWilliams <WilliamsRob45@gmail.com>
2018-04-17 15:24:47 -0500

    passed!

--- Lesson11-JS-VIII/homework/homework.js
+++ Lesson11-JS-VIII/homework/homework.js
@@ -3,15 +3,30 @@
 // solve these recursively
 
 function nFactorial(n) {
+     if (n < 0) {
+          return -1;
+    }  
+    else if (n === 0) { 
+        return 1;
+    }
+      else {
+          return (n * nFactorial(n - 1));
+      }
   // return the factorial for n
   // example:
   // the factorial of 3 is 6 (3 * 2 * 1)
 }
 
 function nFibonacci(n) {
+  let fib = [1,2];
+  for (let i = 2; i < n; i++) {
+    fib[i] = fib[i - 1] + fib [i - 2];
+  }
+  return fib [n - 1]
+  }
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
-}
+
 
 // Do not modify code below this line.
 // --------------------------------
--- Lesson9-JS-VI/homework/homework.js
+++ Lesson9-JS-VI/homework/homework.js
@@ -1,25 +1,44 @@
 // Do not change any of the function names
 
-function invokeCallback(cb) {
+function invokeCallback(cb) { 
+  cb();
+  
+
   // invoke cb
 }
 
 function sumArray(numbers, cb) {
+ let sum = 0; 
+ for (let i = 0; i < numbers.length; i++) {
+   sum += numbers[i];
+ }
+  cb(sum);
   // sum up all of the integers in the numbers array
   // pass the result to cb
   // no return is necessary
 }
 
 function forEach(arr, cb) {
+  arr.forEach(function(value) {
+    cb(value);
+
+  });
+ 
   // iterate over arr and pass its values to cb one by one
   // hint: you will be invoking cb multiple times (once for each value in the array)
 }
 
-function map(arr, cb) {
+ function map(arr, cb) {
+  const newArray = arr.map(cb);
+  return newArray;
+ }
+  
+
+
   // create a new array
   // iterate over each value in arr, pass it to cb, then place the value returned from cb into the new arr
   // the new array should be the same length as the array argument
-}
+
 
 // Do not modify code below this line.
 // --------------------------------
