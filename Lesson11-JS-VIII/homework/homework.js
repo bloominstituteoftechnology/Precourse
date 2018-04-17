e667f600a261f311951e09666cdfd2a839d87662
Adrian Adames <adrianadames@gmail.com>
2018-04-17 17:01:52 -0400

    Submitted homework for Lesson 11

--- Lesson11-JS-VIII/homework/homework.js
+++ Lesson11-JS-VIII/homework/homework.js
@@ -6,11 +6,48 @@ function nFactorial(n) {
   // return the factorial for n
   // example:
   // the factorial of 3 is 6 (3 * 2 * 1)
+
+  var factSum;
+  
+  if (n === undefined) {
+    return factSum = 0;
+  }
+
+  if (n === 0) {
+    return factSum = 0;
+  }
+  
+  if (n < 1) {
+    return factSum = 0;
+  }
+  
+  if (n === 1) {
+    return factSum = 1; 
+  }
+  
+  factSum = n * nFactorial(n-1) ;
+
+  return factSum; 
+  
 }
 
 function nFibonacci(n) {
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  function find(nthNumber, current, previous) {
+    if (n === 1) {
+      return 1;
+    }
+    else if (n === 2) {
+      return 2; 
+    }
+    else if (nthNumber === n) {
+      return current;
+    } else {
+      return find(nthNumber+1, current + previous, current);
+    }
+  }
+  return find(3,3,2);
 }
 
 // Do not modify code below this line.
