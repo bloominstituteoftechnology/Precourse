e06161a852aae4748e44dfb22254226b862662e9
Jordan Rowland <36084892+Jordan-Rowland@users.noreply.github.com>
2018-04-16 17:23:33 -0700

    Homework for JSVI

--- Lesson09-JS-VI/homework/homework.js
+++ Lesson09-JS-VI/homework/homework.js
@@ -2,23 +2,39 @@
 
 function invokeCallback(cb) {
   // invoke cb
+  return cb();
 }
 
 function sumArray(numbers, cb) {
   // sum up all of the integers in the numbers array
   // pass the result to cb
   // no return is necessary
+  let a = 0;
+  for (let i of numbers){
+    a += i;
+  }
+  cb(a);
 }
 
 function forEach(arr, cb) {
   // iterate over arr and pass its values to cb one by one
   // hint: you will be invoking cb multiple times (once for each value in the array)
+  for (let i of arr){
+    cb(i);
+  }
 }
 
 function map(arr, cb) {
   // create a new array
-  // iterate over each value in arr, pass it to cb, then place the value returned from cb into the new arr
+  // iterate over each value in arr, pass it to cb, then place the 
+  // value returned from cb into the new arr
   // the new array should be the same length as the array argument
+  let newArr = [];
+  for (let i of arr){
+    let a = cb(i);
+    newArr.push(a);
+  }
+  return newArr;
 }
 
 // Do not modify code below this line.
