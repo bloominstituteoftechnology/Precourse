4f9a01bbb07bc3688ae9d8cab153c171bb5aceb5
Courtney Buratto <Courtney@MBPdeCoyBuratto.PK5001Z>
2018-04-17 20:29:32 -0700

    added file, feynman.JSIV.md

---
+++ test.js
@@ -0,0 +1,10 @@
+
+> lambdaschoolprecourse@1.0.0 test /Users/Courtney/LambdaSchool/Precourse
+> eslint Lesson09-JS-VI/homework/homework.js Lesson08-JS-V/homework/homework.js Lesson07-JS-IV/homework/homework.js Lesson06-JS-III/homework/homework.js  Lesson05-JS-II/homework/homework.js Lesson04-JS-I/homework/homework.js && jest "JSIV"
+
+
+/Users/Courtney/LambdaSchool/Precourse/Lesson07-JS-IV/homework/homework.js
+  31:3  error  'call' is not defined  no-undef
+
+✖ 1 problem (1 error, 0 warnings)
+
