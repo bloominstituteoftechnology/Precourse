b46e8f4d3f0e4d043bc86e76c5ea3380f5f4f475
Alex Ager <alexager5028@Alexs-MacBook-Air.local>
2018-04-19 09:30:09 -0600

    I completed the closures homework with all tests passing.

--- Lesson10-JS-VII/homework/homework.js
+++ Lesson10-JS-VII/homework/homework.js
@@ -1,13 +1,20 @@
 // Do not change any of the function names
 
+
 function counter() {
   // Return a function that when invoked increments and returns a counter variable.
   // Example: const newCounter = counter();
   // newCounter(); // 1
   // newCounter(); // 2
+        
+    let c = 0;
+    return function(){
+      return c += 1;
+    };
+    
 }
 
-function cacheFunction(cb) {
+function cacheFunction(anonymous) {
   // use closure to create a cache for the cb function
   // the function that you return should accept a single argument and invoke cb with that argument
   // when the function you return is invoked with an argument it should save that argument and its result
@@ -18,7 +25,22 @@ function cacheFunction(cb) {
   // if the function you return is invoked with 5 it would pass 5 to cb(5) and return 25
   // if the function you return is invoked again with 5 it will look on an object in the closure scope
   // and return 25 directly and will not invoke cb again
+
+let cache = {};
+
+return function(argumentForCall){
+  if (argumentForCall in cache){   //if the argument is in the cache
+    return cache[argumentForCall]; //return that argument
+  }
+  else {                           //if not..
+return cache[argumentForCall] = anonymous(argumentForCall); //save the result for future lookups
+    }                           
+  };
 }
+
+
+
+
 
 // Do not modify code below this line.
 // --------------------------------
