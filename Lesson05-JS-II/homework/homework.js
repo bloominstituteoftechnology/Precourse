03c6aa9d97cbe0d1f90a0f71ad025d277ef0d4fd
Fred Sheehan <fred@fredsheehan.com>
2018-04-21 01:06:34 +0100

    Homework Lesson 5 Completed - Test Passed

--- Lesson05-JS-II/homework/homework.js
+++ Lesson05-JS-II/homework/homework.js
@@ -1,50 +1,97 @@
 // Do not change any of the function names
 
 function getBiggest(x, y) {
-  // x and y are integers.  Return the larger integer
-  // if they are the same return either one
+    // x and y are integers.  Return the larger integer
+    // if they are the same return either one
+    if (x > y) {
+        return x;
+    } else if (y > x) {
+        return y;
+    } else return x;
 }
 
 function greeting(language) {
-  // return a greeting for three different languages:
-  // language: 'German' -> 'Guten Tag!'
-  // language: 'Mandarin' -> 'Ni Hao!'
-  // language: 'Spanish' -> 'Hola!'
-  // if language is undefined return 'Hello!'
+    // return a greeting for three different languages:
+    // language: 'German' -> 'Guten Tag!'
+    // language: 'Mandarin' -> 'Ni Hao!'
+    // language: 'Spanish' -> 'Hola!'
+    // if language is undefined return 'Hello!'
+    if (language === 'German') {
+        return 'Guten Tag!';
+    } else if (language === 'Mandarin') {
+        return 'Ni Hao!';
+    } else if (language === 'Spanish') {
+        return 'Hola!';
+    } else return 'Hello!';
 }
 
 function isTenOrFive(num) {
-  // return true if num is 10 or 5
-  // otherwise return false
+    // return true if num is 10 or 5
+    // otherwise return false
+    if (num === 10 || num === 5) {
+        return true;
+    } else return false;
 }
 
 function isInRange(num) {
-  // return true if num is less than 50 and greater than 20
-  // otherwise return false
+    // return true if num is less than 50 and greater than 20
+    // otherwise return false
+    if (num < 50 && num > 20) {
+        return true;
+    } else return false;
 }
 
 function isInteger(num) {
-  // return true if num is an integer
-  // 0.8 -> false
-  // 1 -> true
-  // -10 -> true
-  // otherwise return false
-  // hint: you can solve this using Math.floor
+    // return true if num is an integer
+    // 0.8 -> false
+    // 1 -> true
+    // -10 -> true
+    // otherwise return false
+    // hint: you can solve this using Math.floor
+    return Number.isInteger(num);
 }
 
 function fizzBuzz(num) {
-  // if num is divisible by 3 return 'fizz'
-  // if num is divisible by 5 return 'buzz'
-  // if num is divisible by 3 & 5 return 'fizzbuzz'
-  // otherwise return num
+    // if num is divisible by 3 return 'fizz'
+    // if num is divisible by 5 return 'buzz'
+    // if num is divisible by 3 & 5 return 'fizzbuzz'
+    // otherwise return num
+    if (num % 3 === 0 && num % 5 === 0) {
+        return 'fizzbuzz';
+    } else if (num % 5 === 0) {
+        return 'buzz';
+    } else if (num % 3 === 0) {
+        return 'fizz';
+    } else return num;
 }
 
 function isPrime(num) {
-  // return true if num is prime.
-  // otherwise return false
-  // hint: a prime number is only evenly divisible by itself and 1
-  // hint2: you can solve this using a for loop
-  // note: 0 and 1 are NOT considered prime numbers
+    // return true if num is prime.
+    // otherwise return false
+    // hint: a prime number is only evenly divisible by itself and 1
+    // hint2: you can solve this using a for loop
+    // note: 0 and 1 are NOT considered prime numbers
+
+    // If num is less than 2 or not an integer then it cannot be prime.
+    if (num < 2) {
+        return false;
+    }
+
+    if (num !== Math.round(num)) {
+        return false;
+    }
+
+    // Now we will assume that num is prime, and then try to prove that it is not.
+    let prime = true;
+
+    // Now check every whole number from 2 to the square root of num. If any of these divides num exactly it is not prime.
+    for (var i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            prime = false;
+        }
+    }
+    // Finally return whether n is prime or not.
+    return prime;
 }
 
 
@@ -53,11 +100,11 @@ function isPrime(num) {
 // --------------------------------
 
 module.exports = {
-  getBiggest,
-  greeting,
-  isTenOrFive,
-  isInRange,
-  isInteger,
-  fizzBuzz,
-  isPrime,
+    getBiggest,
+    greeting,
+    isTenOrFive,
+    isInRange,
+    isInteger,
+    fizzBuzz,
+    isPrime,
 };
