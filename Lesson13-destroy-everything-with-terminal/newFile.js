e858880e2c775ffd20cb70ce7a115aba54719589
Madison Mosley <maddielou12@gmail.com>
2018-04-25 13:05:28 -0500

    "Created new js file"

---
+++ Lesson13-destroy-everything-with-terminal/newFile.js
@@ -0,0 +1,19 @@
+ Volume in drive C is Windows
+ Volume Serial Number is 1AA7-57CB
+
+ Directory of C:\Users\MaddieTilt\Documents
+
+04/24/2018  05:07 AM    <DIR>          .
+04/24/2018  05:07 AM    <DIR>          ..
+10/22/2017  03:09 PM    <DIR>          Custom Office Templates
+11/12/2017  07:26 PM    <DIR>          CyberLink
+04/24/2018  04:27 AM    <DIR>          GitHub
+04/17/2018  06:50 AM    <DIR>          LambdaSchool
+04/24/2018  04:49 AM               777 Lesson13-destroy-everything-with-terminal
+04/15/2018  05:37 AM    <DIR>          New Folder
+04/15/2018  06:52 AM    <DIR>          New Fyle
+04/17/2018  06:35 AM               566 NewFile.js
+04/24/2018  05:07 AM                 0 NewFiles.js
+04/24/2018  05:04 AM    <DIR>          Precourse
+               3 File(s)          1,343 bytes
+               9 Dir(s)  924,563,951,616 bytes free
