fccbd6b7bdd7381b5777d2b5ee73cfce3e461b81
Fred Sheehan <fred@fredsheehan.com>
2018-04-25 20:15:13 +0100

    Lesson 8 finished, thank goodness for references like MDN

--- Lesson08-JS-V/homework/homework.js
+++ Lesson08-JS-V/homework/homework.js
@@ -1,31 +1,50 @@
 // Do not change any of the function names
 
 function createUserClass() {
-  // create an ES6 class or a constructor function called User
-  // it should accept an options object with username, name, email, and password properties
-  // in the constructor set the username, name, email, and password properties
-  // the constructor should have a method 'sayHi' on its prototype that returns the string 'Hello, my name is {{name}}'
-  // {{name}} should be the name set on each instance
-  // return the class
+    // create an ES6 class or a constructor function called User
+    // it should accept an options object with username, name, email, and password properties
+    // in the constructor set the username, name, email, and password properties
+    // the constructor should have a method 'sayHi' on its prototype that returns the string 'Hello, my name is {{name}}'
+    // {{name}} should be the name set on each instance
+    // return the class
+
+    function User(options) {
+        this.username = options.username;
+        this.password = options.password;
+        this.email = options.email;
+        this.name = options.name;
+    }
+    User.prototype.sayHi = function() {
+        return 'Hello, my name is ' + this.name;
+    };
+    return User;
 }
 
 function addPrototypeMethod(Constructor) {
-  // add a method to the constructor's prototype
-  // the method should be called 'sayHi' and should return the string 'Hello World!'
+    // add a method to the constructor's prototype
+    // the method should be called 'sayHi' and should return the string 'Hello World!'
+
+    Constructor.prototype.sayHi = function() {
+        return 'Hello World!';
+    };
 }
 
 function addReverseString() {
-  // add a method to the string constructor's prototype that returns a reversed copy of the string
-  // name this method reverse
-  // hint:
-  // you will need to use 'this' inside of reverse
+    // add a method to the string constructor's prototype that returns a reversed copy of the string
+    // name this method reverse
+    // hint: you will need to use 'this' inside of reverse
+
+    String.prototype.reverse = function() {
+        return this.split('').reverse().join('');
+    };
 }
 
-// Do not modify code below this line.
-// --------------------------------
+
+// Do not modify code below this line
+// ----------------------------------
 
 module.exports = {
-  createUserClass,
-  addPrototypeMethod,
-  addReverseString,
+    createUserClass,
+    addPrototypeMethod,
+    addReverseString,
 };
