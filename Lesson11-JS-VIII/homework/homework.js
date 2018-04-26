246277ae48813e48971c89302190867b043cc76c
Jacob Louie <jlouie9@gmail.com>
2018-04-26 17:40:07 -0400

    Finished HW11-JS-VIII

--- Lesson11-JS-VIII/homework/homework.js
+++ Lesson11-JS-VIII/homework/homework.js
@@ -6,11 +6,26 @@ function nFactorial(n) {
   // return the factorial for n
   // example:
   // the factorial of 3 is 6 (3 * 2 * 1)
+
+  let counter = 1;
+  innerRecursion(n);
+
+  function innerRecursion(i) {
+   if (i < 1) return;
+   counter = counter * i;
+   innerRecursion(i-1);
+  }
+  return counter;
 }
 
 function nFibonacci(n) {
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  if (n <= 2) {
+    return n;
+  } else {
+    return nFibonacci(n-1) + nFibonacci(n-2);
+  }
 }
 
 // Do not modify code below this line.
