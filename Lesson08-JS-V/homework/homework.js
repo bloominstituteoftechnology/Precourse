9ce06e26e20335fbf0049322e6492ff8bc777d8b
meganfontenot <33911508+meganfontenot@users.noreply.github.com>
2018-04-27 14:38:47 -0500

    Lesson08Homework

--- Lesson08-JS-V/homework/homework.js
+++ Lesson08-JS-V/homework/homework.js
@@ -1,34 +1,26 @@
 // Do not change any of the function names
 
-function createUserClass() {
-  class User {
-    constructor(options) {
-      this.username = options.username;
-      this.name = options.name;
-      this.email = options.email;
-      this.password = options.password;
-    }
-    sayHi() {
-      return `Hello, my name is ${this.name}`;
-    }
+function createUserClass (username, name, email, password) {
+  this.username = username;
+  this.name = name;
+  this.email = email;
+  this.password = password;
+  this.sayHi = function() {
+    return 'Hello, my name is {{name}}';
+  };
 }
-return User;
 
 function addPrototypeMethod(Constructor) {
-  Constructor.prototype.sayHello = function() {
-    return 'Hello World!'
+  Constructor.prototype.sayHi = function() {
+    return 'Hello World!';
   };
 }
 
 function addReverseString() {
   String.prototype.reverse = function() {
-   let reversestring = '';
-   for (var i = this.length - 1; i >= 0; i--) {
-     reversestring += this[i]
-   }
-   return reversestring;
+    return this.split('').reverse().join('');
+  };
 }
-
 // Do not modify code below this line.
 // --------------------------------
 
