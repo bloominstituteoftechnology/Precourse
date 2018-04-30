dce7f1cdef9dd8b631e780d10fd2a4887e0b5dfe
John Cuerdon <john.cuerdon@gmail.com>
2018-04-30 12:11:43 -0700

    Lesson09-JS homework cleaned up

--- Lesson09-JS-VI/homework/homework.js
+++ Lesson09-JS-VI/homework/homework.js
@@ -1,46 +1,34 @@
-// Do not change any of the function names
-
-function invokeCallback(cb) {
+function invokeCallback(cb) { // Do not change any of the function names
   // invoke cb
   cb();
 }
 
+
 function sumArray(numbers, cb) {
-  let sum = 0;  
+  let result = 0;  
   for (let i = 0; i < numbers.length; i++) { 
-    sum += numbers[i];  // sum up all of the integers in the numbers array  
-    
-  }
-  cb(sum);// pass the result to cb
-  // no return is necessary
+    result += numbers[i];  // sum up all of the integers in the numbers array     
+  }  // no return is necessary
+  cb(result);// but pass the result to cb
 }
   
-  
 
-
-function forEach(arr, cb) {
-  // iterate over arr and pass its values to cb one by one
-  for (let i = 0; i < arr.length; i++) {
-    // hint: you will be invoking cb multiple times 
-    //(once for each value in the array)
-    cb(arr[i]); 
-
+function forEach(arr, cb) {    // hint: you will be invoking cb multiple times 
+                              //(once for each value in the array)
+  for (let i = 0; i < arr.length; i++) {  // iterate over arr 
+    cb(arr[i]); // and pass its values to cb one by one
   }  
   return cb;
 }
 
+
 function map(arr, cb) {
-  // create a new array
-  const mappedArray = [];
-  // iterate over each value in arr, 
-  //pass it to cb, then place the value
-  // returned from cb into the new arr
-  // the new array should be the same length as the array argument
-  for (let i = 0; i < arr.length; i++) {
-    let result = cb(arr[i]);
-    mappedArray[i] = result;    
+  const mappedArray = [];  // create a new array
+  for (let i = 0; i < arr.length; i++) {  // iterate over each value in arr, 
+    let result = cb(arr[i]);  //pass each value to cb, 
+    mappedArray[i] = result;   // then place the value returned from cb into the new arr 
   }
-  return mappedArray;
+  return mappedArray;  // the new array should be the same length as the array argument
 }
 
 // Do not modify code below this line.
