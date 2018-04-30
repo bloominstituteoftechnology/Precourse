ec5a996856ea8d3229c7131bcd750dc80fd0f6c2
Samuel Kirkpatrick <kirksamalpha@gmail.com>
2018-04-30 08:58:48 -0500

    commit changes Lesson8

--- Lesson8-JS-V/homework/homework.js
+++ Lesson8-JS-V/homework/homework.js
@@ -1,50 +1,61 @@
 // Do not change any of the function names
 
 
-const User = function (username, name, email, password){
-  this.username = username;
-  this.name = name;
-  this.email = email;
-  this.password = password;
-};
-const user = new User('SunJieMing', 'Ben', 'ben@lambdaschool.com', 'LS Rocks!');
-  console.log(user);
+function createUserClass(){
+
+  const User = function (username, name, email, password){
+    this.username = username;
+    this.name = name;
+    this.email = email;
+    this.password = password;
+  };
+  const user = new User('SunJieMing', 'Ben', 'ben@lambdaschool.com', 'LS Rocks!'); 
+    return user;
+  }
+  createUserClass();
 
 
 
-const User = function (username, name, email, password){
-  this.username = username;
-  this.name = name;
-  this.email = email;
-  this.password = password;
-  this.intro = 'Hello World!';
-};
-User.prototype.sayHi = function(){
-  this.intro = 'Hello World!';
-};
-const user = new User('SunJieMing', 'Ben', 'ben@lambdaschool.com', 'LS Rocks!', 'Hello World!');
-console.log(user);
- //add a method to the constructor's prototype
-//the method should be called 'sayHi' and should return the string 'Hello World!'
+  function addPrototypeMethod(){
+    const User = function (username, name, email, password, intro){
+      this.username = username;
+      this.name = name;
+      this.email = email;
+      this.password = password;
+      this.intro = 'Hello World!';
+    };
+    User.prototype.sayHi = function(){
+      this.intro = 'Hello World!';
+    };
+    const user = new User('SunJieMing', 'Ben', 'ben@lambdaschool.com', 'LS Rocks!', 'Hello World!');
+    return user.intro;
+    }
+    addPrototypeMethod();
+     //add a method to the constructor's prototype
+    //the method should be called 'sayHi' and should return the string 'Hello World!'
 
 
 
-const User = function (username, name, email, password){
-  this.username = username;
-  this.name = name;
-  this.email = email;
-  this.password = password;
-  this.introRev = '!dlroW olleH';
-};
-  User.prototype.reverse = function(){
-  this.introRev = '!dlroW olleH';
-};
-const user = new User('SunJieMing', 'Ben', 'ben@lambdaschool.com', 'LS Rocks!', '!dlroW olleH');
-    console.log(user);
-   // add a method to the string constructor's prototype that returns a reversed copy of the string    
-  // name this method reverse
- // hint:
-// you will need to use 'this' inside of reverse
+    function addReverseString(){
+      const User = function (username, name, email, password){
+        this.username = username;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.introRev = '!dlroW olleH';
+      };
+        User.prototype.reverse = function(){
+        this.introRev = '!dlroW olleH';
+      };
+      const user = new User('SunJieMing', 'Ben', 'ben@lambdaschool.com', 'LS Rocks!', '!dlroW olleH');
+       return user.introRev;
+         
+      } 
+       addReverseString();
+         // add a method to the string constructor's prototype that returns a reversed copy of the string    
+        // name this method reverse
+       // hint:
+      // you will need to use 'this' inside of reverse
 
 // Do not modify code below this line.
 // --------------------------------
