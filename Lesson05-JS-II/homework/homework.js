38a7c66b42c118a5fa31c55424638b2f20871250
Ismail-Opatola <opatolamails@gmail.com>
2018-05-01 21:48:02 +0100

    j

--- Lesson05-JS-II/homework/homework.js
+++ Lesson05-JS-II/homework/homework.js
@@ -26,7 +26,7 @@ function greeting(language) {
     }
     else if (language === 'Spanish') {
      return 'Hola!';
-  }  else if (language === 0) { 
+  }  else if (undefined) { 
     return 'Hello!';
   }
 }
