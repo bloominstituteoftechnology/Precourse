6980322d24a36822e3af7ef11ccfc256c04c5e7e
Ismail-Opatola <opatolamails@gmail.com>
2018-05-01 11:16:38 +0100

    Lesson 4 - Homework Completed

--- Lesson04-JS-I/homework/homework.js
+++ Lesson04-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 //In these first 6 questions, replace `null` with the answer
 
 //create a string variable, it can contain anything
-const newString = 'Kansas' ;
+const newString = 'Nevada' ;
 
 //create a number variable, it can be any number
 const newNum = 25;
