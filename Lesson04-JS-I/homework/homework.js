eabc74333ff4dc77effd5818fe6f2546288cf9b0
Ismail-Opatola <opatolamails@gmail.com>
2018-05-01 03:08:16 +0100

    d

--- Lesson04-JS-I/homework/homework.js
+++ Lesson04-JS-I/homework/homework.js
@@ -74,10 +74,10 @@ function areSameLength(str1, str2) {
   // return true if the two strings have the same length
   // otherwise return false
   // code here
-  if (str1.lenght === str2.lenght) {
+  if (str1.length === str2.length) {
     return true;
   }
-  return false;
+  return (str1.length !== str2.length); false;
 }
 
 function lessThanNinety(num) {
