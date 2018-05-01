1a60d3321b9a6efbba2f6ac6711a8b222c49b816
Ismail-Opatola <opatolamails@gmail.com>
2018-05-01 10:45:41 +0100

    dfgh

--- Lesson04-JS-I/homework/homework.js
+++ Lesson04-JS-I/homework/homework.js
@@ -180,7 +180,7 @@ function getGreeting(name) {
   // Take the name string and concatenate other strings onto it so it takes the following form:
   // 'Sam' -> 'Hello Sam!'
   // code here
-  return alert('Hello, '+ name +'!');
+  return name.concate('Hello, '+ name +'!');
 }
 
 // The next three questions will have you implement math area formulas. 
