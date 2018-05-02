504af1a8ea59bb1e6e49bfdec7e43fe2e43c6f93
Ismail-Opatola <opatolamails@gmail.com>
2018-05-02 06:17:13 +0100

    Homework 5 completed

--- Lesson05-JS-II/homework/homework.js
+++ Lesson05-JS-II/homework/homework.js
@@ -69,11 +69,11 @@ function fizzBuzz(num) {
   // if num is divisible by 5 return 'buzz'
   // if num is divisible by 3 & 5 return 'fizzbuzz'
   // otherwise return num
-  if (num / 3) {
+  if (num % 3 === 0) {
     return 'fizz';
-  } else if (num / 5) {
+  } else if (num % 5 === 0) {
     return 'buzz';
-  } else if (num/3 && num/5) {
+  } else if (num % 3 === 0 && num % 5 === 0) {
     return 'fizzbuzz';
   } else return num;
 }
