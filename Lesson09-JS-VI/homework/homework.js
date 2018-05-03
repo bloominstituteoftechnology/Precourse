9827b57c3d00bfdfdd5f772c058a981850107483
CLN <cln@Sags-MacBook-Pro.local>
2018-05-02 21:05:01 -0400

    added hw9, homework.js

--- Lesson09-JS-VI/homework/homework.js
+++ Lesson09-JS-VI/homework/homework.js
@@ -1,6 +1,7 @@
 // Do not change any of the function names
 
 function invokeCallback(cb) {
+  return cb();
   // invoke cb
 }
 
@@ -8,16 +9,28 @@ function sumArray(numbers, cb) {
   // sum up all of the integers in the numbers array
   // pass the result to cb
   // no return is necessary
+  const sum = numbers.reduce(function(acc, item){
+    return acc + item;
+  });
+  cb(sum);
 }
 
 function forEach(arr, cb) {
   // iterate over arr and pass its values to cb one by one
   // hint: you will be invoking cb multiple times (once for each value in the array)
+  arr.forEach(function(item, index){
+    cb (item);
+  });
+
 }
 
 function map(arr, cb) {
   // create a new array
+  const newArr = arr.map(cb);
+return newArr;
+  
   // iterate over each value in arr, pass it to cb, then place the value returned from cb into the new arr
+ //cb(newArr);
   // the new array should be the same length as the array argument
 }
 
