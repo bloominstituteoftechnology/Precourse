c1a4dd225d467443c21a6b55b2962e780add2118
Ismail-Opatola <opatolamails@gmail.com>
2018-05-03 13:08:30 +0100

    kk

--- Lesson06-JS-III/homework/homework.js
+++ Lesson06-JS-III/homework/homework.js
@@ -19,7 +19,7 @@ function incrementByOne(arr) {
   // arr is an array of integers  
   // increase each integer by one
   // return the array
-  let sum = arr + 1++;
+  let sum = (arr + 1++);
   return sum;
 }
 
