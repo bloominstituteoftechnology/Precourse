0fd85cf98020229d07b9972fe028dd4e93384d04
unknown <may.alex43@gmail.com>
2018-05-03 20:33:28 -0400

    finished Lesson05 about if statements

--- Lesson05-JS-II/homework/homework.js
+++ Lesson05-JS-II/homework/homework.js
@@ -3,6 +3,9 @@
 function getBiggest(x, y) {
   // x and y are integers.  Return the larger integer
   // if they are the same return either one
+if (x>y) return x;
+else if (x<y) return y;
+else return x||y;
 }
 
 function greeting(language) {
@@ -11,17 +14,23 @@ function greeting(language) {
   // language: 'Mandarin' -> 'Ni Hao!'
   // language: 'Spanish' -> 'Hola!'
   // if language is undefined return 'Hello!'
-}
+if (language==='German') return 'Guten Tag!';
+if(language==='Mandarin') return 'Ni Hao!';
+if(language==='Spanish') return 'Hola!';
+else return 'Hello!';}
 
 function isTenOrFive(num) {
   // return true if num is 10 or 5
   // otherwise return false
-}
+if(num===10||num===5) return true;
+if(!(num===10||num===5)) return false;}
 
 function isInRange(num) {
   // return true if num is less than 50 and greater than 20
   // otherwise return false
-}
+if(num<50 && num>20)
+return true;
+else return false;}
 
 function isInteger(num) {
   // return true if num is an integer
@@ -30,14 +39,22 @@ function isInteger(num) {
   // -10 -> true
   // otherwise return false
   // hint: you can solve this using Math.floor
-}
+if (Math.floor(num)===num) return true;
+if (Math.floor(num)===0.8) return false;
+if (Math.floor(num)===1) return true;
+if (Math.floor(num)===-10) return true;
+else return false;}
 
 function fizzBuzz(num) {
   // if num is divisible by 3 return 'fizz'
   // if num is divisible by 5 return 'buzz'
   // if num is divisible by 3 & 5 return 'fizzbuzz'
   // otherwise return num
-}
+if(num%3===0 && num%5===0) return 'fizzbuzz';
+if(num%3===0) return 'fizz';
+if(num%5===0) return 'buzz';
+if(num%3===0 && num%5===0) return 'fizzbuzz';
+else return num;}
 
 function isPrime(num) {
   // return true if num is prime.
@@ -45,6 +62,10 @@ function isPrime(num) {
   // hint: a prime number is only evenly divisible by itself and 1
   // hint2: you can solve this using a for loop
   // note: 0 and 1 are NOT considered prime numbers
+if(num===1|| num===0){return false;}
+for (let i=2; i<num; i++) 
+if(num%i===0){return false;}
+return true;
 }
 
 
@@ -60,4 +81,4 @@ module.exports = {
   isInteger,
   fizzBuzz,
   isPrime,
-};
+ };
