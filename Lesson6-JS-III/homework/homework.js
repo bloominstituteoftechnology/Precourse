e367d7a37e11843ea7c38d5dfc0e82b9ffc95e63
farooqsiraj <36709663+farooqsiraj@users.noreply.github.com>
2018-05-04 13:18:11 -0400

    updated homeworkk.js
    completed earlier never uploaded on Github

--- Lesson6-JS-III/homework/homework.js
+++ Lesson6-JS-III/homework/homework.js
@@ -2,31 +2,44 @@
 
 function returnFirst(arr) {
   // return the first item from the array
+  return arr[0];
 }
 
 function returnLast(arr) {
   // return the last item of the array
+  return arr[arr.length -1];
 }
 
 function getArrayLength(arr) {
   // return the length of the array
+  return arr.length;
 }
 
 function incrementByOne(arr) {
   // arr is an array of integers  
   // increase each integer by one
   // return the array
+  for(let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i] + 1;
+    
+  } return arr;
 }
 
 function addItemToArray(arr, item) {
   // add the item to the end of the array
   // return the array
+   arr.push(item); 
+
+  return arr;
+
 }
 
 function addItemToFront(arr, item) {
   // add the item to the front of the array
   // return the array
   // hint: use the array method .unshift
+   arr.unshift(item);
+  return arr;
 }
 
 function wordsToSentence(words) {
@@ -34,6 +47,14 @@ function wordsToSentence(words) {
   // return a string that is all of the words concatenated together
   // spaces need to be between each word
   // example: ['Hello', 'world!'] -> 'Hello world!'
+  let first = words[0];
+  let last = words[words.length -1];
+
+  words.foreach(function(item, index, array){
+    return (item);
+  });
+ 
+
 }
 
 function contains(arr, item) {
