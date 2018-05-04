dd65224bd49eee4259095890b8b7b2ceb30228fe
Ismail-Opatola <opatolamails@gmail.com>
2018-05-04 23:40:06 +0100

    c

--- Lesson07-JS-IV/homework/homework.js
+++ Lesson07-JS-IV/homework/homework.js
@@ -19,9 +19,7 @@ function addProperty(object, property) {
   // add the property to the object with a value of null
   // return the object
   // note: the property name is NOT 'property'.  The name is the value of the argument called property (a string)
-  object = {
-    property:null,
-  }; 
+  object.property = null; 
   return object;
 }
 
