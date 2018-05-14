4e186edf1e8525b52b7b2a0745c27783232ae153
swag2 <brendonkootz@gmail.com>
2018-05-14 11:18:59 -0500

    finished precourse

--- Lesson10-JS-VII/homework/homework.js
+++ Lesson10-JS-VII/homework/homework.js
@@ -24,9 +24,40 @@ function cacheFunction(cb) {
   // if the function you return is invoked with 5 it would pass 5 to cb(5) and return 25
   // if the function you return is invoked again with 5 it will look on an object in the closure scope
   // and return 25 directly and will not invoke cb again
-
-
+ 	let cache = {};
+	return function(num){
+		if(num in cache){return cache[num];} //9
+		
+		cache[num] = num * num;
+		return cb(num);
+	}
+	
 }
+
+
+/*
+	"cb was being called 4 times, so I got rid of cb after the 'if' and 
+	 directly assigned it to what cb was doing annddd wallaaa it worked"
+	
+ 	let cache = {};
+	return function(num){
+		if(num in cache){return cache[num];} //9
+		else {
+			cache[num] = cb(num);
+			return cb(num);
+		}
+	}
+		// down to 4 times
+*/
+
+
+
+
+
+
+
+
+
 
 // Do not modify code below this line.
 // --------------------------------
