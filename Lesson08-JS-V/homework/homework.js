d0cda5770e54aca21332febbd85cb34baa71502d
Carolina Utojo <carolinautojo@Carolinas-iMac.702com.net>
2018-05-18 12:00:40 -0500

    homework 8

--- Lesson08-JS-V/homework/homework.js
+++ Lesson08-JS-V/homework/homework.js
@@ -7,11 +7,24 @@ function createUserClass() {
   // the constructor should have a method 'sayHi' on its prototype that returns the string 'Hello, my name is {{name}}'
   // {{name}} should be the name set on each instance
   // return the class
+  var user = function (options) {
+    this.username = options.username;
+    this.name = options.name;
+    this.email = options.email;
+    this.password =  options.password;
+  };
+  user.prototype.sayHi = function (){
+    return 'Hello, my name is ' + this.name;
+  };
+  return user;
 }
 
 function addPrototypeMethod(Constructor) {
   // add a method to the constructor's prototype
   // the method should be called 'sayHi' and should return the string 'Hello World!'
+  Constructor.prototype.sayHi = function (){
+    return 'Hello World!';
+  };
 }
 
 function addReverseString() {
@@ -19,6 +32,18 @@ function addReverseString() {
   // name this method reverse
   // hint:
   // you will need to use 'this' inside of reverse
+  var revString = function(str)
+  {
+    this.str = str;
+    this.rev_str = '';
+  };
+  revString.prototype.reverse = function (){
+    for ( var i = this.str.length - 1 ; i >= 0 ; i--) {
+      this.rev_str += this.str[i];
+    } 
+    return this.rev_str;
+  };
+  return revString;
 }
 
 // Do not modify code below this line.
