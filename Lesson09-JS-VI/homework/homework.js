d7845c5c1d2f80cc6efbc2f32f1b96792db37906
Kevin Sooter <kevin.m.sooter@gmail.com>
2018-05-31 14:09:25 -0500

    Completed Lesson 9

--- Lesson09-JS-VI/homework/homework.js
+++ Lesson09-JS-VI/homework/homework.js
@@ -1,24 +1,38 @@
+// 1. Feynman Writing Prompts - Write out explanations of the following concepts like you are explaining it to a 12 year old.  Doing this will help you quickly discover any holes in your understanding.  Ask your questions on Slack.
+
+// 	* Callback Functions - A callback function is a function passed into another function as an argument, which is then invoked inside the outer function to complete some kind of routine or action.
+
 // Do not change any of the function names
 
 function invokeCallback(cb) {
   // invoke cb
+  cb();
 }
 
 function sumArray(numbers, cb) {
   // sum up all of the integers in the numbers array
   // pass the result to cb
   // no return is necessary
+  const sum = numbers.reduce((acc, val) => acc + val, 0);
+  cb(sum);
 }
 
 function forEach(arr, cb) {
   // iterate over arr and pass its values to cb one by one
   // hint: you will be invoking cb multiple times (once for each value in the array)
+  arr.forEach(value => {
+    cb(value);
+  });
 }
 
 function map(arr, cb) {
   // create a new array
   // iterate over each value in arr, pass it to cb, then place the value returned from cb into the new arr
   // the new array should be the same length as the array argument
+  const array = arr.map(value => {
+    return cb(value);
+  });
+  return array;
 }
 
 // Do not modify code below this line.
@@ -28,5 +42,5 @@ module.exports = {
   invokeCallback,
   sumArray,
   forEach,
-  map,
+  map
 };
