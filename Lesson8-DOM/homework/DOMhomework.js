70bd8858689b2a565bbf4635e598d7bc3c2d2a59
Christopher Espiritu <christopheredrian@gmail.com>
2018-06-01 22:29:55 +0800

    Added lesson 8

--- Lesson8-DOM/homework/DOMhomework.js
+++ Lesson8-DOM/homework/DOMhomework.js
@@ -3,26 +3,33 @@
            add your name to the end of the current innerHTML*/
 
 //Uncomment and add your code:
-// let created = document.querySelector(/* Your Code here */);
+let created = document.querySelector('span#createdBy');
+created.innerHTML += " Christopher Espiritu";
 
-function Todo(){
+function Todo(description){
 
 /* 
   STEP 2: Create a class constructor called 'Todo' this function should take one argument, the description of the todo.
     add two values to the class: this.description which should be set equal to the description passed to the class, and 
     this.complete which should be set to false. 
 */
+  this.description = description;
+  this.complete = false;
 
 }
 
 /* STEP 3: Add a completeTodo method to the prototype of Todo. It will not take any arguemnts. Inside the function set the
            Todo's complete to true*/
-
+           
+Todo.prototype.completeTodo = function(){
+  this.complete = true;
+};
 /* STEP 4: initiate an array called 'toDoItems'. In this array you should have one new object of the class Todo. */
 let toDoItems;
+toDoItems = [];
+toDoItems.push(new Todo('Finish precourse'));
 
-
-function buildTodo() {
+function buildTodo(todo, index) {
 /*
   STEP 5: This function, buildTodo, will take an object of class Todo as it's first argument and 
           a numerical index as it's second.
@@ -39,14 +46,28 @@ function buildTodo() {
             7.) Append child todoText to todoShell
             8.) return todoShell
 */
+    let todoShell = document.createElement('div');
+    todoShell.className = "todoShell";
+    let todoText = document.createElement('span');
+    todoText.innerHTML = todo.description;
+    todoText.id = index;
+    todoText.onclick = completeTodo;
+    if(todo.complete){
+      todoText.className = "completeText";
+    }
+    todoShell.appendChild(todoText);
+    return todoShell;
 }
 
-function buildTodos() {
+function buildTodos(todos) {
   /* 
   STEP 6: This function will build and return an array of todo element. It will take an array of objects of the Todo class as it's only argument.
           Using the map method on the array passed in, use the 'buildTodo' function as the callback passed to map. 
           Return the new mapped array.
   */
+ return todos.map((todo, index) => {
+   return buildTodo(todo, index);
+ });
 }
 
 function displayTodos() {
@@ -62,6 +83,13 @@ function displayTodos() {
 
             You can now load your html file in your broswer and see your work so far.
   */
+ let todoContainer = document.querySelector('#todoContainer');
+ todoContainer.innerHTML = "";
+ let todos = buildTodos(toDoItems);
+  todos.forEach(t => {
+    todoContainer.appendChild(t);
+  });
+  
 }
 
 
@@ -82,7 +110,11 @@ function addTodo() {
   */
 
   //UNCOMMENT THE NEXT LINE
-  // let newTodo = document.querySelector('#todoInput');
+  let newTodo = document.querySelector('#todoInput');
+  toDoItems.push(new Todo(newTodo.value));
+  newTodo.value = "";
+  displayTodos();
+  
 }
 
 /* 
@@ -93,8 +125,9 @@ function addTodo() {
 */
 
 //UNCOMMENT THE NEXT LINE
-// let button;
-
+let button;
+button = document.getElementById('addButton');
+button.onclick = addTodo;
 
 
 function completeTodo(event) {
@@ -111,7 +144,9 @@ function completeTodo(event) {
             3.) In the buildTodo function add a 'click' event listener to the 'todoText' element, and pass this function as the callback.
   */
   //UNCOMMENT THE NEXT LINE
-  // let index = event.target.id;
+  let index = event.target.id;
+  toDoItems[index].completeTodo(); // 1
+  displayTodos(); // 2
 }
 
 /* STEP 11: Make sure ALL tests pass */
@@ -132,6 +167,7 @@ function completeTodo(event) {
 
 
 // Call displayTodos here <-----
+displayTodos();
 // ---------------------------- DO NOT CHANGE ANY CODE BELOW THIS LINE ----------------------------- //
 if (typeof module !== 'undefined') {
   module.exports = {
