b794db21615c6fb65d53404f510332c35f234c8c
aellis77 <wool.neu@gmail.com>
2018-06-04 00:28:47 -0400

    v1.0 Lesson10 homework

--- Lesson10-JS-VII/homework/homework.js
+++ Lesson10-JS-VII/homework/homework.js
@@ -5,6 +5,8 @@ function counter() {
   // Example: const newCounter = counter();
   // newCounter(); // 1
   // newCounter(); // 2
+  let count=0;
+  return (function() { return (count = count + 1);} );
 }
 
 function cacheFunction(cb) {
@@ -18,6 +20,27 @@ function cacheFunction(cb) {
   // if the function you return is invoked with 5 it would pass 5 to cb(5) and return 25
   // if the function you return is invoked again with 5 it will look on an object in the closure scope
   // and return 25 directly and will not invoke cb again
+  
+  // need a lookback structure of some sort - keying input with output and function.
+  
+  // create object
+  const lookBackObj = {
+    inputVar : -99,
+    inputFunct : function (){},
+    output : 123,
+    countCb : 0  };
+
+  return function(arg) {
+    if ((arg === lookBackObj.inputVar) && (cb === lookBackObj.inputFunct)) {
+      return lookBackObj.output;
+    } else {
+      lookBackObj.output = cb(arg);
+      lookBackObj.inputVar = arg;
+      lookBackObj.inputFunct = cb;
+      lookBackObj.countCb = lookBackObj.countCb + 1;
+      return lookBackObj.output;
+    }
+  };
 }
 
 // Do not modify code below this line.
