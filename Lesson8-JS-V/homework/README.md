21d122af4e83bb49a94ff2957f912a9d62fd0903
TheGameFreak720 <luis.alvarez@commithub.com>
2018-06-05 23:02:26 -0400

    finished lesson 8

--- Lesson8-JS-V/homework/README.md
+++ Lesson8-JS-V/homework/README.md
@@ -4,8 +4,8 @@
 ---
 1. Feynman Writing Prompts - Write out explanations of the following concepts like you are explaining it to a 12 year old.  Doing this will help you quickly discover any holes in your understanding.  Ask your questions on Slack.
 		
-	* `prototype`
-	* Constructors
+	* `prototype` = prototypes are a way to consume less memory space and create methods that can be used by various objects when needed.
+	* Constructors = a template we can use to initialize an object with predefined conditions set before. This makes it easier when coding instead of writing the same thing we can use a contructor.
 
 2. From the top level of your `Precourse` folder, run `npm test JSV.test.js` to run the automated tests. You will fill out the functions in `homework.js` to make the tests pass.
 
--- Lesson8-JS-V/homework/homework.js
+++ Lesson8-JS-V/homework/homework.js
@@ -7,11 +7,26 @@ function createUserClass() {
   // the constructor should have a method 'sayHi' on its prototype that returns the string 'Hello, my name is {{name}}'
   // {{name}} should be the name set on each instance
   // return the class
+    var User = class {
+        constructor (user) {
+            this.username = user.username;
+            this.name = user.name;
+            this.email = user.email;
+            this.password = user.password;
+            this.sayHi = function () {
+                return 'Hello, my name is ' + this.name;
+            };
+        }
+    };
+    return User;
 }
 
 function addPrototypeMethod(Constructor) {
   // add a method to the constructor's prototype
   // the method should be called 'sayHi' and should return the string 'Hello World!'
+    Constructor.prototype.sayHi = function(){
+        return 'Hello World!';
+    };
 }
 
 function addReverseString() {
@@ -19,6 +34,9 @@ function addReverseString() {
   // name this method reverse
   // hint:
   // you will need to use 'this' inside of reverse
+    String.prototype.reverse = function() {
+        return this.split('').reverse().join('');
+    };
 }
 
 // Do not modify code below this line.
