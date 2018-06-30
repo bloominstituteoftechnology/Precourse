081308be6bd793f4bce839f51a9b404479830a60
Lorenzo Evans <lorenzo.evans94@gmail.com>
2018-06-30 18:55:27 -0400

    Updated homework files.

--- Lesson9-JS-VI/homework/homework.js
+++ Lesson9-JS-VI/homework/homework.js
@@ -10,17 +10,30 @@ function sumArray(numbers, cb) {
   // sum up all of the integers in the numbers array
   // pass the result to cb
   // no return is necessary
+
+  let arrSum = numbers.reduce((total, amount) => total + amount);
+
+  cb(arrSum) ;
+
 }
 
 function forEach(arr, cb) {
   // iterate over arr and pass its values to cb one by one
   // hint: you will be invoking cb multiple times (once for each value in the array)
+
+  arr.forEach(cb(function(){for (let i = 0; i <= arr.length; i++){return arr ;}})) ;
+
+  
 }
 
 function map(arr, cb) {
   // create a new array
   // iterate over each value in arr, pass it to cb, then place the value returned from cb into the new arr
   // the new array should be the same length as the array argument
+
+  let newArr = [] ;
+
+  arr.map(cb())
 }
 
 // Do not modify code below this line.
