c285c0f389fd7652b4d6bcf6ede68a730148f720
Alex Martin <atonymartin20@yahoo.com>
2018-07-01 20:52:37 -0500

    Lessons 5, 6, & 7 completed

--- Lesson5-JS-II/homework/homework.js
+++ Lesson5-JS-II/homework/homework.js
@@ -3,6 +3,10 @@
 function getBiggest(x, y) {
   // x and y are integers.  Return the larger integer
   // if they are the same return either one
+  if(x >= y) {
+    return x;
+  }
+  return y;
 }
 
 function greeting(language) {
@@ -11,16 +15,34 @@ function greeting(language) {
   // language: 'Mandarin' -> 'Ni Hao!'
   // language: 'Spanish' -> 'Hola!'
   // if language is undefined return 'Hello!'
+  if(language === 'German') {
+    return 'Guten Tag!';
+  }
+  else if (language === 'Mandarin') {
+    return 'Ni Hao!';
+  }
+  else if (language === 'Spanish') {
+    return 'Hola!';
+  }
+  return 'Hello!';
 }
 
 function isTenOrFive(num) {
   // return true if num is 10 or 5
   // otherwise return false
+  if(num === 5 || num === 10) {
+    return true;
+  }
+  return false;
 }
 
 function isInRange(num) {
   // return true if num is less than 50 and greater than 20
   // otherwise return false
+  if(num < 50 && num > 20) {
+    return true;
+  } 
+  return false;
 }
 
 function isInteger(num) {
@@ -30,13 +52,31 @@ function isInteger(num) {
   // -10 -> true
   // otherwise return false
   // hint: you can solve this using Math.floor
+  if(num % 1 === 0) {
+    return true;
+  }
+  return false;
 }
+// OR if(num === Math.floor(num)) {
+//   return true;
+//  }
+//   return false;
 
 function fizzBuzz(num) {
   // if num is divisible by 3 return 'fizz'
   // if num is divisible by 5 return 'buzz'
   // if num is divisible by 3 & 5 return 'fizzbuzz'
   // otherwise return num
+  if(num % 3 === 0 && num % 5 === 0) {
+    return 'fizzbuzz';
+  }
+  else if(num % 3 === 0) {
+    return 'fizz';
+  }
+  else if(num % 5 === 0) {
+    return 'buzz';
+  }
+  return num;
 }
 
 function isPrime(num) {
@@ -45,6 +85,16 @@ function isPrime(num) {
   // hint: a prime number is only evenly divisible by itself and 1
   // hint2: you can solve this using a for loop
   // note: 0 and 1 are NOT considered prime numbers
+  if(num <= 1) {
+    return false;
+  }
+
+  for(let i = 2; i < num; i++) {
+    if(num % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 
--- Lesson7-JS-IV/homework/homework.js
+++ Lesson7-JS-IV/homework/homework.js
@@ -5,23 +5,35 @@ function makeCat(name, age) {
   // add an age property to the object with the value set to the age argument
   // add a method called meow that returns the string 'Meow!'
   // return the object
+  const NewCat = {
+    name: name,
+    age: age,
+    meow: function() {
+      return 'Meow!';
+    },
+  };
+  return NewCat;
 }
 
 function addProperty(object, property) {
   // add the property to the object with a value of null
   // return the object
   // note: the property name is NOT 'property'.  The name is the value of the argument called property (a string)
+  object[property] = null;
+  return object;
 }
 
 function invokeMethod(object, method) {
   // method is a string that contains the name of a method on the object
   // invoke this method
   // nothing needs to be returned
+  object[method]();
 }
 
 function multiplyMysteryNumberByFive(mysteryNumberObject) {
   // mysteryNumberObject has a property called mysteryNumber
   // multiply the mysteryNumber property by 5 and return the product
+  return mysteryNumberObject.mysteryNumber * 5; 
 }
 
 function deleteProperty(object, property) {
