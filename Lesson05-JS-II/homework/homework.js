79778622f5412c7bfcb16d87cf6ff0aa48f7fa41
amadison17 <angelmadison2015@gmail.com>
2018-07-11 19:50:14 -0400

    lesson5

--- Lesson05-JS-II/homework/homework.js
+++ Lesson05-JS-II/homework/homework.js
@@ -62,13 +62,13 @@ function fizzBuzz(num) {
   // if num is divisible by 5 return 'buzz'
   // if num is divisible by 3 & 5 return 'fizzbuzz'
   // otherwise return num
-  if(num % 3 === 0){
-    return 'fizz';
-  }else if(num % 5 === 0){
-    return 'buzz';
-  }else if(num % 3 === 0 && num % 5 === 0){
-    return 'fizzbuzz';
-  }else{
+if(num % 3 === 0 && num % 5 === 0) {
+  return 'fizzbuzz';
+  } else if (num % 3 === 0) {
+  return 'fizz';
+  } else if (num % 5 === 0) {
+  return 'buzz';
+  } else {
     return num;
   }
 }
@@ -79,11 +79,12 @@ function isPrime(num) {
   // hint: a prime number is only evenly divisible by itself and 1
   // hint2: you can solve this using a for loop
   // note: 0 and 1 are NOT considered prime numbers
-  for(let i = 2; i < num; i++){
-    if(num % i === 0){
-      return false;
+  let i = 1; 
+  if(num % i === 0){
+    return false;
     }
-  }  return true;
+  
+  return true;
 }
 
 // Do not modify code below this line.
