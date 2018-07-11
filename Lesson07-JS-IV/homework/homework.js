8b54b11e8eb946511431b61e07f89091446f09d2
amadison17 <angelmadison2015@gmail.com>
2018-07-11 19:54:00 -0400

    lesson7

--- Lesson07-JS-IV/homework/homework.js
+++ Lesson07-JS-IV/homework/homework.js
@@ -9,42 +9,44 @@ function makeCat(name, age) {
   cat.name = name;
   cat.age = age;
   cat.meow = function(){
-    return 'Meow!';  
-
+    return 'Meow!';
+  };
   return cat;
-  }
+  
+}
+  
 function addProperty(object, property) {
   // add the property to the object with a value of null
   // return the object
   // note: the property name is NOT 'property'.  The name is the value of the argument called property (a string)
-  object[property] = null;
-  return object;
-  }
+object[property] = null;
+return object;
+}
 
 function invokeMethod(object, method) {
   // method is a string that contains the name of a method on the object
   // invoke this method
   // nothing needs to be returned
-  object[method]();
+object[method]();
 }
 
 function multiplyMysteryNumberByFive(mysteryNumberObject) {
   // mysteryNumberObject has a property called mysteryNumber
   // multiply the mysteryNumber property by 5 and return the product
-  return mysteryNumberObject.mysteryNumber * 5;
+return mysteryNumberObject.mysteryNumber * 5;
 }
 
 function deleteProperty(object, property) {
   // remove the property from the object
   // return the object
-  delete object[property];
-  return object;
+delete object[property];
+return object;
 }
 
 function newUser(name, email, password) {
   // create a new object with properties matching the arguments passed in.
   // return the new object
-  var user = {}
+  var user = {};
   user.name = name;
   user.email = email;
   user.password = password;
@@ -55,10 +57,10 @@ function newUser(name, email, password) {
 function hasEmail(user) {
   // return true if the user has a value for the property 'email'
   // otherwise return false
-  if (user.email.length) {
-    return true;
-  } else { 
-    return false;
+if (user.email.length) {
+  return true;
+} else {
+  return false;
   }
 }
 
@@ -66,19 +68,15 @@ function hasProperty(object, property) {
   // return true if the object has the value of the property argument
   // property is a string
   // otherwise return false
-  if (object.hasOwnProperty(property) && object[property].length) {
-    return true;
-  } else {
-    return false;
-  }
+
 }
 
 function verifyPassword(user, password) {
   // check to see if the provided password matches the password property on the user object
   // return true if they match
   // otherwise return false
-  if (user.password == password) {
-    return true;
+if (user.password == password) {
+  return true;
   } else {
     return false;
   }
@@ -87,16 +85,16 @@ function verifyPassword(user, password) {
 function updatePassword(user, newPassword) {
   // replace the existing password on the user object with the value of newPassword
   // return the object
-  user.password = newPassword;
-  return user;
+user.password = newPassword;
+return user;
 }
 
 function addFriend(user, newFriend) {
   // user has a property called friends that is an array
   // add newFriend to the end of the friends array
   // return the user object
-  user.friends.push(newFriend);
-  return user;
+user.friends.push(newFriend);
+return user;
 }
 
 function setUsersToPremium(users) {
@@ -104,10 +102,9 @@ function setUsersToPremium(users) {
   // each user object has the property 'isPremium'
   // set each user's isPremium property to true
   // return the users array
-  for(var i = 0; i < users.length; i++) {
-    users[i].isPremium = true;
-  }
-
+for (var i = 0; i < users.length; i++) {
+  users[i].isPremium = true;
+  } 
   return users;
 }
 
@@ -117,14 +114,13 @@ function sumUserPostLikes(user) {
   // each post object has an integer property called 'likes'
   // sum together the likes from all the post objects
   // return the sum
-  var total_likes = 0;
-  for (var i = 0; i < user.posts.length; i++){
-    total_likes += user.posts[i].likes;
+var total = 0;
+for (var i = 0; i < user.posts.length; i++){
+  total += user.posts[i].likes;
   }
-
-  return total_likes;
+  return total;
 }
-
+ 
 function addCalculateDiscountPriceMethod(storeItem) {
   // add a method to the storeItem object called 'calculateDiscountPrice'
   // this method should multiply the storeItem's 'price' and 'discountPercentage' to get the discount
@@ -134,10 +130,10 @@ function addCalculateDiscountPriceMethod(storeItem) {
   // price -> 20
   // discountPercentage -> .2
   // discountPrice = 20 - (20 * .2)
-  storeItem.calculateDiscountPrice = function(){
-    var discountPrice = this.price - (this.price * this.discountPercentage);
-    return discountPrice;
-  }
+storeItem.calculateDiscountPrice = function(){
+  var discountPrice = this.price - (this.price * this.discountPercentage);
+  return discountPrice;
+  };
   return storeItem;
 }
 
@@ -159,4 +155,4 @@ module.exports = {
   setUsersToPremium,
   sumUserPostLikes,
   addCalculateDiscountPriceMethod,
-}
+};
