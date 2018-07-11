41260ff7872140fc98fb4496ba3e32e2a1aa97a2
Daniel Lim <danheelim@gmail.com>
2018-07-10 21:59:59 -0700

    homework

--- Lesson04-JS-I/homework/homework.js
+++ Lesson04-JS-I/homework/homework.js
@@ -5,8 +5,8 @@ const newString = 'nothing' ;
 
 //create a number variable, it an be any number
 const newNum = 4 ;
-const sub = 5
-const multi = 4
+const sub = 5;
+const multi = 4;
 
 //create a boolean variable
 const newBool = true ;
@@ -15,7 +15,6 @@ const newBool = true ;
 const newSubtract = 10 - sub === 5;
 
 //Solve the following math problem
-const 
 const newMultiply = 10 * multi === 40 ;
 
 //Solve the following math problem:
@@ -30,12 +29,12 @@ const newModulo = 21 % 5 === null ;
 
 function returnString(str) {
   //simply return the string provided: str
-  console.log("hello this is problem 1");
+  return str;
 }
 
 function add(x, y) {
   const z = x + y;
-  console.log(z)
+  return z;
 
   // x and y are numbers
   // add x and y together and return the value
@@ -46,73 +45,73 @@ function subtract(x, y) {
   // subtract y from x and return the value
   return x + y;
   // code here
-};
+}
 
 function multiply(x, y) {
   return x*y;
   // multiply x by y and return the value
   // code here
-};
+}
 
 function divide(x, y) {
   // divide x by y and return the value
   // code here
   return x/y;
-};
+}
 
 function areEqual(x, y) {
   // return true if x and y are the same
   // otherwise return false
   // code here
   if(x == y){
-    return "true";
+    return 'true';
   }
   else{
-    return "false";
+    return 'false';
   }
-};
+}
 
 function areSameLength(str1, str2) {
   // return true if the two strings have the same length
   // otherwise return false
   // code here
   if (length.str1 == length.str2){
-    return "true";
+    return 'true';
   }
   else {
-    return "false";
+    return 'false';
   }
-};
+}
 
 function lessThanNinety(num) {
   // return true if the function argument: num , is less than ninety
   // otherwise return false
   // code here
   if( num < 90 ){
-    return "true"
+    return 'true';
   }
   else {
-    return "false"
+    return 'false';
   }
-};
+}
 
 function greaterThanFifty(num) {
   // return true if num is greater than fifty
   // otherwise return false
   // code here
   if (num > 50){
-    return "true";
+    return 'true';
   }
   else {
-    return "false";
+    return 'true';
   }
-};
+}
 
 function getRemainder(x, y) {
   // return the remainder from dividing x by y
   // code here
-  console.log(x % y ** 2);
-  }
+  return x % y;
+  
 }
 
 function isEven(num) {
@@ -120,24 +119,24 @@ function isEven(num) {
   // otherwise return false
   // code here
   if(num / 2 == 0 ) {
-    return "true";
+    return 'true';
   }
   else {
-    return "false";
+    return 'false';
   }
-};
+}
 
 function isOdd(num) {
   // return true if num is odd
   // otherwise return false
   // code here
   if (num / 2 != 0){
-    return "true"
+    return 'true';
   }
   else {
-    return "false";
+    return 'false';
   }
-};
+}
 
 function square(num) {
   Math.pow(num, 2);
@@ -175,21 +174,21 @@ function addExclamationPoint(str) {
   // add an exclamation point to the end of str and return the new string
   // 'hello world' -> 'hello world!'
   // code here
-  return str + "!";
-};
+  return str + '!';
+}
 
 function combineNames(firstName, lastName) {
   // return firstName and lastName combined as one string and separated by a space.
   // 'Lambda', 'School' -> 'Lambda School'
   // code here
-  return firstName + " " + LastName;
-};
+  return firstName + ' ' + lastName;
+}
 
 function getGreeting(name) {
   // Take the name string and concatenate other strings onto it so it takes the following form:
   // 'Sam' -> 'Hello Sam!'
   // code here
-  return "HELLO" + " " + name + "!"; 
+  return 'HELLO' + ' ' + name + '!'; 
 }
 
 // The next three questions will have you implement math area formulas. 
@@ -198,7 +197,7 @@ function getGreeting(name) {
 function getRectangleArea(length, width) {
   // return the area of the rectangle by using length and width
   // code here
-  return Length * width;
+  return length * width;
 }
 
 function getTriangleArea(base, height) {
