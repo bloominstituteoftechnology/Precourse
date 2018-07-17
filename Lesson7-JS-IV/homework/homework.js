37b8a9c35133248d9be767131da0b1a384161de3
unknown <tsaihuangsd@gmail.com>
2018-07-16 23:09:56 -0700

    add homework for Lesson 7

--- Lesson7-JS-IV/homework/homework.js
+++ Lesson7-JS-IV/homework/homework.js
@@ -1,82 +1,175 @@
 // Do not change any of the function names
-
+/*let newObject = {
+    name: 'kitty',
+    age: 3,
+    mysteryNumber: 7,
+    meow: function(){
+      return 'Meow!';
+    }
+}*/
+//return newObject;
 function makeCat(name, age) {
   // create a new object with a name property with the value set to the name argument
   // add an age property to the object with the value set to the age argument
   // add a method called meow that returns the string 'Meow!'
   // return the object
+  let newObject = {
+    name: name,
+    age: age,
+    meow: function(){
+      return 'Meow!';
+    },
+  };
+  return newObject;
 }
-
+//makeCat('kitty',3);
 function addProperty(object, property) {
   // add the property to the object with a value of null
   // return the object
   // note: the property name is NOT 'property'.  The name is the value of the argument called property (a string)
+  object[property] = null;
+  return object;
 }
-
+//addProperty(newObject,'newProp');
 function invokeMethod(object, method) {
   // method is a string that contains the name of a method on the object
   // invoke this method
   // nothing needs to be returned
+  let methodName = method;
+  object[methodName]();
 }
-
+//invokeMethod(newObject,'meow');
 function multiplyMysteryNumberByFive(mysteryNumberObject) {
   // mysteryNumberObject has a property called mysteryNumber
   // multiply the mysteryNumber property by 5 and return the product
+  return mysteryNumberObject.mysteryNumber*5;
 }
-
+//multiplyMysteryNumberByFive(newObject);
 function deleteProperty(object, property) {
   // remove the property from the object
   // return the object
+    delete object[property];
+  return object;
 }
-
+//console.log(newObject.mysteryNumber);
+//deleteProperty(newObject, 'mysteryNumber');
 function newUser(name, email, password) {
   // create a new object with properties matching the arguments passed in.
   // return the new object
+  let tempObject = {
+    name: name,
+    email: email,
+    password: password,
+  };
+/*  for (let key in tempObject){
+        console.log(key);
+        console.log(tempObject[key]);
+    }*/
+  return tempObject;
 }
+//newUser('Terrell','tsaihuangsd','sdfgs');
 
+/*let users = {
+    name: 'Terrell',
+    email: 'sdf',
+    friend: ['Brandon', 'Ashley'],
+    password: 'you suck',
+  }*/
 function hasEmail(user) {
   // return true if the user has a value for the property 'email'
   // otherwise return false
+  return user.email !== null && user.email !== undefined;
 }
-
+//hasEmail(users);
 function hasProperty(object, property) {
   // return true if the object has the value of the property argument
   // property is a string
   // otherwise return false
+  let hasProp = false;
+  for (let key in object) {
+    if (key === property){
+      hasProp = true;
+    }
+  }
+  return hasProp;
 }
-
+//hasProperty(users,'sdfg')
 function verifyPassword(user, password) {
   // check to see if the provided password matches the password property on the user object
   // return true if they match
   // otherwise return false
+  if (user['password'] === password){
+      return true;
+  } else {
+      return false;   
+  }
 }
-
+//verifyPassword(users, 'you suck');
 function updatePassword(user, newPassword) {
   // replace the existing password on the user object with the value of newPassword
   // return the object
+  user.password = newPassword;
+  return user;
 }
-
+//updatePassword(users,'you stink');
 function addFriend(user, newFriend) {
   // user has a property called friends that is an array
   // add newFriend to the end of the friends array
   // return the user object
+  user.friends.push(newFriend);
+  return user;
 }
-
+//addFriend(users,'Kai');
+/*let Terrell = {
+  name: 'Terrell',
+  isPremium: false,
+};
+let Brandon = {
+  name: 'Brandon',
+  isPremium: false,
+};
+let Ashley = {
+  name: 'Ashley',
+  isPremium: false,
+};
+let userArray = [Terrell,Brandon,Ashley];
+*/  
 function setUsersToPremium(users) {
   // users is an array of user objects.
   // each user object has the property 'isPremium'
   // set each user's isPremium property to true
   // return the users array
+  for (let i=0;i<users.length;i++) {
+    users[i].isPremium = true;
+  }
+  return users;
 }
+//setUsersToPremium(userArray);
 
+/*let post1 = {likes:4,}
+let post2 = {likes:2,}
+let post3 = {likes:3,}
+let tsai ={
+  posts: [post1, post2, post3],
+}*/
 function sumUserPostLikes(user) {
   // user has an array property called 'posts'
   // posts is an array of post objects
   // each post object has an integer property called 'likes'
   // sum together the likes from all the post objects
   // return the sum
+  let totalLikes = 0;
+  for (let i=0;i<user.posts.length;i++) {
+    totalLikes = totalLikes + user.posts[i].likes;
+  }
+  return totalLikes;
 }
+//sumUserPostLikes(tsai);
 
+/*let coke = {
+  price: 10,
+  discountPercentage: .6,
+}*/
 function addCalculateDiscountPriceMethod(storeItem) {
   // add a method to the storeItem object called 'calculateDiscountPrice'
   // this method should multiply the storeItem's 'price' and 'discountPercentage' to get the discount
@@ -86,10 +179,14 @@ function addCalculateDiscountPriceMethod(storeItem) {
   // price -> 20
   // discountPercentage -> .2
   // discountPrice = 20 - (20 * .2)
+  storeItem['calculateDiscountPrice'] = function() {
+    return (storeItem.price - (storeItem.price * storeItem.discountPercentage));
+  };
+  return storeItem;
 }
-
+// addCalculateDiscountPriceMethod(coke);
 // Do not modify code below this line.
-// --------------------------------
+// -------------------------------
 
 module.exports = {
   makeCat,
