6731de15bb1786df3119dc65778ccf17ba1ab19f
daniela-idara <38259246+daniela-idara@users.noreply.github.com>
2018-11-17 08:29:05 -0600

    update - FINAL
    Test Suites: 1 passed, 1 total
    Tests:       28 passed, 28 total
    Snapshots:   0 total
    Time:        1.997s, estimated 2s

--- Lesson04-JS-I/homework/homework.js
+++ Lesson04-JS-I/homework/homework.js
@@ -47,14 +47,14 @@ function divide(x, y) {
 }
 
 function areEqual(x, y) {
-  if (x == y) {
+  if (x === y) {
     return true;
   }
     return false;
 }
 
 function areSameLength(str1, str2) {
-  if (str1 == str2) {
+  if (str1.length === str2.length) {
     return true;
   }
     return false;
@@ -75,7 +75,7 @@ function greaterThanFifty(num) {
 }
 
 function getRemainder(x, y) {
-  return (x % y)
+  return (x % y);
 }
 
 function isEven(num) {
@@ -91,64 +91,49 @@ function isOdd(num) {
     }
       return false;
 }
+
 function square(num) {
-    if (num )
-  // square num and return the new value
-  // hint: NOT square root!
-  // code here
+  return Math.pow(num, 2);
 }
 
 function cube(num) {
-  // cube num and return the new value
-  // code here
+  return Math.pow(num, 3);
 }
 
 function raiseToPower(num, exponent) {
-  // raise num to whatever power is passed in as exponent
-  // code here
+  return Math.pow(num, exponent);
 }
 
 function roundNumber(num) {
-  // round num and return it
-  // code here
+  return Math.round(num);
 }
 
 function roundUp(num) {
-  // round num up and return it
-  // code here
+  return Math.ceil(num);
 }
 
 function addExclamationPoint(str) {
-  // add an exclamation point to the end of str and return the new string
-  // 'hello world' -> 'hello world!'
-  // code here
+  return (str + '!');
 }
 
 function combineNames(firstName, lastName) {
-  // return firstName and lastName combined as one string and separated by a space.
-  // 'Lambda', 'School' -> 'Lambda School'
-  // code here
+  return (firstName + ' ' + lastName);
 }
 
 function getGreeting(name) {
-  // Take the name string and concatenate other strings onto it so it takes the following form:
-  // 'Sam' -> 'Hello Sam!'
-  // code here
+  return ('Hello ' + name + '!');
 }
 
 // The next three questions will have you implement math area formulas. 
 // If you can't remember these area formulas then head over to Google.
  
 function getRectangleArea(length, width) {
-  // return the area of the rectangle by using length and width
-  // code here
+  return (length * width);
 }
 
 function getTriangleArea(base, height) {
-  // return the area of the triangle by using base and height
-  // code here
+  return (base * height) / 2 ;
 }
-
 
 // Do not modify code below this line.
 // --------------------------------
