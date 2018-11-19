37925af3c689a1757d0eabf484d625aad7778d9a
daniela-idara <purpleelephant1987@gmail.com>
2018-11-19 17:12:13 -0600

    update errors

--- Lesson05-JS-II/homework/homework.js
+++ Lesson05-JS-II/homework/homework.js
@@ -56,11 +56,9 @@ function fizzBuzz(num) {
 }
 
 function isPrime(num) {
-  // return true if num is prime.
-  // otherwise return false
-  // hint: a prime number is only evenly divisible by itself and 1
-  // hint2: you can solve this using a for loop
-  // note: 0 and 1 are NOT considered prime numbers
+  for(var i = 2; i < num; i++)
+    if(num % i === 0) return false;
+  return num !== 1 && num !== 0;
 }
 
 
