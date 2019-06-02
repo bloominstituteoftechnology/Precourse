a309d0d9b54ad1cd443ae0e3f14285d57ef83715
Dev-Nebe <cp.nebe@gmail.com>
2019-06-02 01:56:39 +0100

    Homework with solutions; Two tests (wordsToSentence and multiplyArguments) failed. They will be fixed as soon as possible.

--- Lesson06-JS-III/homework/homework.js
+++ Lesson06-JS-III/homework/homework.js
@@ -2,65 +2,95 @@
 
 function returnFirst(arr) {
   // return the first item from the array
+  return arr[0];
 }
 
 function returnLast(arr) {
   // return the last item of the array
+  return arr[arr.length - 1];
 }
 
 function getArrayLength(arr) {
   // return the length of the array
+  return arr.length;
 }
 
 function incrementByOne(arr) {
   // arr is an array of integers  
   // increase each integer by one
   // return the array
+  return arr.map(x => x + 1);
 }
 
 function addItemToArray(arr, item) {
   // add the item to the end of the array
   // return the array
+  arr.push(item);
+  return arr;
 }
 
 function addItemToFront(arr, item) {
   // add the item to the front of the array
   // return the array
   // hint: use the array method .unshift
+  arr.unshift(item);
+  return arr;
 }
 
-function wordsToSentence(words) {
+/*function wordsToSentence(words) {
   // words is an array of strings
   // return a string that is all of the words concatenated together
   // spaces need to be between each word
   // example: ['Hello', 'world!'] -> 'Hello world!'
-}
+  if (words.length === 1){
+    return words;}
+  else {
+    let lastWord = words.pop;
+    words.map(x => x.concat(' '));
+    words.concat(lastWord[0]);
+  return words.reduce((x, z) => x.concat(z));}
+
+}*/
 
 function contains(arr, item) {
   // check to see if item is inside of arr
   // return true if it is, otherwise return false
+  for (let i=0; i < arr.length; i++){
+    if (arr[i] === item){
+      return true;
+    }
+  } return false;
 }
 
 function addNumbers(numbers) {
   // numbers is an array of integers.
   // add all of the integers and return the value
+  return numbers.reduce((x, y) => x + y);
 }
 
 function averageTestScore(testScores) {
   // testScores is an array.  Iterate over testScores and compute the average.
   // return the average
+  return testScores.reduce((x, y) => x + y)/testScores.length;
 }
 
 function largestNumber(numbers) {
   // numbers is an array of integers
   // return the largest integer
+  return Math.max(...numbers);
+  // there's a solution for this using the reduce() functionality
 }
 
-function multiplyArguments() {
+/*function multiplyArguments() {
   // use the arguments keyword to multiply all of the arguments together and return the product
   // if no arguments are passed in return 0
   // if one argument is passed in just return it
-}
+  if (arguments === null){return 0;}
+  else if (arguments.length == 1) {return arguments;}
+  else{
+    return Array(arguments).reduce((x, y) => x * y);
+  }
+}*/
 
 // Do not modify code below this line.
 // --------------------------------
